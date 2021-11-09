71b323ef25de03374394706390d40c23fd5e1bf8
Rafael Medeiros <62731714+RafaelMedeirosGomes@users.noreply.github.com>
2021-11-09 17:39:45 -0300

    feat(back-end): :sparkles: sequelize configurado

---
+++ back-end/src/database/models/sale.js
@@ -0,0 +1,22 @@
+/**
+ * @param {import('sequelize').Sequelize} sequelize 
+ * @param {import('sequelize').DataTypes} DataTypes 
+ * @return 
+ */
+ module.exports = (sequelize, DataTypes) => {  
+  const Sale = sequelize.define('Sale', {
+    id: { type: DataTypes.INTEGER, allowNull: false, autoIncrement: true, primaryKey: true },
+    userId: DataTypes.INTEGER,
+    sellerId: DataTypes.INTEGER,
+    totalPrice: DataTypes.STRING,
+    deliveryAddress: DataTypes.STRING,
+    deliveryNumber: DataTypes.STRING,
+    saleDate: DataTypes.DATE,
+    status: DataTypes.STRING
+  }, { timestamps: false });
+  Sale.associate = (models) => {
+    Sale.belongsTo(models.User, { as: 'user' });
+    Sale.belongsTo(models.User, { as: 'seller' });
+  };  
+  return Sale;
+};

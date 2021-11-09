1145e596f576128e3518590e4fd3b3aa3381313d
Gustavo Sousa <gustavohl98@gmail.com>
2021-11-09 18:34:09 -0300

    fix: quantity field in product-sales migration

--- back-end/src/database/migrations/20211109202152-create-product-sales.js
+++ back-end/src/database/migrations/20211109202152-create-product-sales.js
@@ -1,4 +1,3 @@
-
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('productsSales', {
@@ -21,10 +20,10 @@ module.exports = {
         onDelete: 'CASCADE',
         references: {
           model: 'sales',
-          key: 'id'
+          key: 'id',
         },
-      quantity: Sequelize.INTEGER,  
       },
+      quantity: Sequelize.INTEGER,
     });
   },
 

e06318d4fe0e5c81db08130ab9e498a615ca71b5
Marcelo Maurício <mmauricio.design@gmail.com>
2021-11-09 17:34:50 -0300

    Feat 0: created a model association SaleProduct with Sale and Product

---
+++ back-end/src/database/models/SaleProduct.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, _DataTypes) => {
+  const SaleProduct = sequelize.define('PostsCategories', {},
+    { timestamps: false });
+
+  SaleProduct.associate = (models) => {
+    models.Sale.belongsToMany(models.Product, {
+      as: 'products',
+      through: SaleProduct,
+      foreignKey: 'sale_id',
+      otherKey: 'product_id',
+    });
+    models.Product.belongsToMany(models.Sale, {
+      as: 'sales',
+      through: SaleProduct,
+      foreignKey: 'product_id',
+      otherKey: 'sale_id',
+    });
+  };
+
+  return SaleProduct;
+};

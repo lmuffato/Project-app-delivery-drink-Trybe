9991c621003e26a9ef3f5625b919551fc6fc92ce
Parker9309 <juan.tentiomaru9309@gmail.com>
2021-11-09 18:24:10 -0300

    feat: salesProduct migration created

---
+++ back-end/src/database/migrations/20211109212126-salesProducts.js
@@ -0,0 +1,26 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("salesProducts", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      product_id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      quantity: {
+        type: Sequelize.INTEGER,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("salesProducts");
+  },
+};

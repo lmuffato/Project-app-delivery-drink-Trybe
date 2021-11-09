3dab245452d30e8048bb0ad087b4713dc35e817b
Parker9309 <juan.tentiomaru9309@gmail.com>
2021-11-09 18:34:55 -0300

    feat: products migration created

---
+++ back-end/src/database/migrations/20211109212559-products.js
@@ -0,0 +1,29 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("products", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      price: {
+        allowNull: false,
+        type: Sequelize.DECIMAL,
+      },
+      url_image: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("products");
+  },
+};

50deeda60412218fe7462aca19838b5cee93c11c
Robertson <robertson.nunes@gmail.com>
2021-11-09 19:43:37 -0300

    feat: fixed linter issues

--- front-end/src/pages/Register.js
+++ front-end/src/pages/Register.js
@@ -1,8 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
-const Register = () => {
-  return (
-    <div>
+const Register = () => (
+  <div>
     <h1>Cadastro</h1>
     <form>
       <label htmlFor="inputName">
@@ -10,31 +9,32 @@ const Register = () => {
         <input
           data-testid="common_register__input-name"
           name="inputName"
-          />
+        />
       </label>
       <label htmlFor="inputEmail">
-          Email
-          <input
-            id="input"
-            data-testid="common_register__input-email"
-            name="inputEmail"
-          />
-        </label>
-        <label htmlFor="inputPassword">
-          Senha
-          <input
-            id="input"
-            data-testid="common_register__input-password"
-            name="inputPassword"
-          />
-        </label>
-        <button
-          type="button"
-          data-testid="common_register__button-register"
-        >
-          Cadastrar
-        </button>
+        Email
+        <input
+          id="input"
+          data-testid="common_register__input-email"
+          name="inputEmail"
+        />
+      </label>
+      <label htmlFor="inputPassword">
+        Senha
+        <input
+          id="input"
+          data-testid="common_register__input-password"
+          name="inputPassword"
+        />
+      </label>
+      <button
+        type="button"
+        data-testid="common_register__button-register"
+      >
+        Cadastrar
+      </button>
     </form>
-    </div>
-  )
-}
+  </div>
+);
+
+export default Register;

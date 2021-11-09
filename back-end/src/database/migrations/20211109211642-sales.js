4a092185e380b1475efcf4750e293eda49fc334a
lucas-paz <lucas.hpaz@outlook.com>
2021-11-09 18:23:15 -0300

    fix: Table name

--- back-end/src/database/migrations/20211109211642-sales.js
+++ back-end/src/database/migrations/20211109211642-sales.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Users', {
+    return queryInterface.createTable('Sales', {
       id: {
         allowNull: false,
         autoIncrement: true,

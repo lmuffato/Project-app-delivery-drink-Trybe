2a05a772136c4f6ed345085416e000b12aaaa4bc
Parker9309 <juan.tentiomaru9309@gmail.com>
2021-11-09 18:10:42 -0300

    feat: sales migration created

---
+++ back-end/src/database/migrations/20211109202711-sales.js
@@ -0,0 +1,49 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("sales", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "sales",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+        foreignKey: true,
+      },
+      seller_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "sales",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+        foreignKey: true,
+      },
+      total_price: {
+        type: Sequelize.DECIMAL,
+      },
+      delivery_address: {
+        type: Sequelize.STRING,
+      },
+      delivery_number: {
+        type: Sequelize.STRING,
+      },
+      status: {
+        type: Sequelize.STRING,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("sales");
+  },
+};

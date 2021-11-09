688bf69db311733fe22063ec7064efe110e95b77
Marcelo Maurício <mmauricio.design@gmail.com>
2021-11-09 18:47:45 -0300

    Feat 0: create a migrattion about Products

---
+++ back-end/src/database/migrations/20211109214504-create-products.js
@@ -0,0 +1,28 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      price: {
+        allowNull: false,
+        type: Sequelize.DECIMAL,
+        unique: true,
+      },
+      urlImage: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Products');
+  }
+};

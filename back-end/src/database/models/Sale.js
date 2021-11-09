74e60bc38b9ac1083c4a6088659ba7c2ed11893f
Marcelo Maurício <mmauricio.design@gmail.com>
2021-11-09 17:07:31 -0300

    Feat 0: created Sale model for the database

---
+++ back-end/src/database/models/Sale.js
@@ -0,0 +1,23 @@
+module.exports = (sequelize, DataTypes) => {
+  const Sale = sequelize.define('User', {
+    id: { type: DataTypes.INTEGER, primaryKey: true },
+    total_price: DataTypes.DECIMAL,
+    delivery_address: DataTypes.STRING,
+    delivery_number: DataTypes.STRING,
+    sale_date: DataTypes.DATE,
+    status: DataTypes.VARCHAR,
+    user_id: { type: DataTypes.INTEGER, foreignKey: true },
+  },
+  {
+    timestamps: false,
+    tableName: 'Sales',
+    underscored: true,
+  });
+
+  Sale.associate = (models) => {
+    Sale.belongsTo(models.User,
+      { foreignKey: 'user_id', as: 'user' });
+  };
+
+  return Sale;
+};

6a02e72abddfc28d63ed2c98f1f8978e3582da2a
Robertson <robertson.nunes@gmail.com>
2021-11-09 18:29:43 -0300

    feat: Implementa inputs

---
+++ front-end/src/pages/Register.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+
+const Register = () => {
+  return (
+    <div>
+    <h1>Cadastro</h1>
+    <form>
+      <label htmlFor="inputName">
+        Nome
+        <input
+          data-testid="common_register__input-name"
+          name="inputName"
+          />
+      </label>
+      <label htmlFor="inputEmail">
+          Email
+          <input
+            id="input"
+            data-testid="common_register__input-email"
+            name="inputEmail"
+          />
+        </label>
+        <label htmlFor="inputPassword">
+          Senha
+          <input
+            id="input"
+            data-testid="common_register__input-password"
+            name="inputPassword"
+          />
+        </label>
+    </form>
+    </div>
+  )
+}

ac434b4665392f8e35ed144f771ed0b551a89fe1
biazidioti <bzferreira.bio@gmail.com>
2021-11-10 09:45:06 -0300

    Config User model and migration

--- back-end/src/database/models/user.js
+++ back-end/src/database/models/user.js
@@ -1,26 +1,25 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  User.init({
+  const User = sequelize.define('User', 
+  {
     name: DataTypes.STRING,
-    password: DataTypes.STRING,
-    email: DataTypes.STRING,
+    password: {
+      type: DataTypes.STRING,
+      validate: {
+        len: [6, 32]
+      }
+    },
+    email: { 
+      type: DataTypes.STRING,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
     role: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'User',
+  }, 
+  {
+    timestamps: false,
+    tableName: 'Users',
   });
   return User;
 };

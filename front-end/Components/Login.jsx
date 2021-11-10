c6cd8437eb47351cb56d39d5621f87912187c025
Parker9309 <juan.tentiomaru9309@gmail.com>
2021-11-10 16:44:52 -0300

    fix: lint4

--- front-end/Components/Login.jsx
+++ front-end/Components/Login.jsx
@@ -3,16 +3,16 @@ import React from 'react';
 export default function Login() {
   return (
       <div>
-        <form>
-          <input type="email" placeholder="email@trybeer.com.br" required>
-            Login
-          </input>
-          <input type="password" placeholder="*********">
-            Senha
-          </input>
-          <button type="button">LOGIN</button>
-          <button type="button">Ainda não tenho conta</button>
-        </form>
+      <form>
+        <input type="email" placeholder="email@trybeer.com.br" required>
+          Login
+        </input>
+        <input type="password" placeholder="*********">
+          Senha
+        </input>
+        <button type="button">LOGIN</button>
+        <button type="button">Ainda não tenho conta</button>
+      </form>
       </div>
   );
 }

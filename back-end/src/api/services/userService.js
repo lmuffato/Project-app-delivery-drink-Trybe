a819937b9ca47b23d5288096d19217c4581bffbb
Gustavo Sousa <gustavohl98@gmail.com>
2021-11-10 16:53:04 -0300

    Implements login route with jwt

--- back-end/src/api/services/userService.js
+++ back-end/src/api/services/userService.js
@@ -1,14 +1,31 @@
+const md5 = require('md5');
+const jwt = require('jsonwebtoken');
 const { User } = require('../../database/models');
 const AppError = require('../utils/AppError');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const { JWT_SECRET } = process.env;
 
 const login = async ({ email, password }) => {
   if (!email || !password) {
     throw new AppError(400, 'Invalid data');
   }
 
-  const user = await User.findOne({ where: { email }});
+  const user = await User.findOne({ where: { email } });
 
-  return user;
+  const passwordMd5 = md5(password);
+
+  if (!user || user.dataValues.password !== passwordMd5) {
+    throw new AppError(404, 'User not nound or incorrect password');
+  }
+
+  const token = jwt.sign({
+    id: user.dataValues.id,
+    email: user.dataValues.email,
+  }, JWT_SECRET);
+
+  return token;
 };
 
 module.exports = {
--- back-end/src/api/utils/AppError.js
+++ back-end/src/api/utils/AppError.js
@@ -1,5 +1,6 @@
 class AppError extends Error {
   constructor(code, message) {
+    super();
     this.code = code;
     this.message = message;
   }

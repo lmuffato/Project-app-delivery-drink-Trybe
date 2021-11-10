d4f9d5cd54474607a7b957f451a705024c5102e4
Parker9309 <juan.tentiomaru9309@gmail.com>
2021-11-10 15:38:46 -0300

    fix: lint and function estructure

--- front-end/Components/Login.jsx
+++ front-end/Components/Login.jsx
@@ -1,22 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class login extends Component {
-  render() {
+export default function Login() {
     return (
       <div>
         <form>
-          <input type='email' placeholder='email@trybeer.com.br' required>
-            Login
+          <input type="email" placeholder="email@trybeer.com.br" required>
+          Login
           </input>
-          <input type='password' placeholder='*********'>
-            Senha
+          <input type="password" placeholder="*********">
+          Senha
           </input>
-          <button type='button'>LOGIN</button>
-          <button type='button'>Ainda não tenho conta</button>
+          <button type="button">LOGIN</button>
+          <button type="button">Ainda não tenho conta</button>
         </form>
       </div>
     );
-  }
 }
-
-export default login;

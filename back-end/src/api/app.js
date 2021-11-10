db79ec54fdc694fb9208b34b780e573c980c1426
Rodrigo Rech <rodrigorech92@gmail.com>
2021-11-10 10:33:01 -0300

    Feat: app.use(bodyparser)

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const userControllers = require('../controllers/userControllers')
 
 const app = express();
+app.use(bodyParser.json());
 
 app.post('/login', userControllers.login)
 

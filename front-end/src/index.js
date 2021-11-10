5c090aaee2318a57b138251c48cdde81f303535f
Joao Nasc <oi@joaonasc.dev>
2021-11-10 15:26:10 -0300

    feat: coloca BrowserRouter em volta do App

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root'),
 );
 

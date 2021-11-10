2741538f96f6865d33187c49659abf173bed1bb5
biazidioti <bzferreira.bio@gmail.com>
2021-11-10 18:03:07 -0300

    Feat: login page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import './App.css';
 // import rockGlass from './images/rockGlass.svg';
 import { Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={ <div>Xablau</div> } />
+      <Route path="/" element={ <Login /> } />
     </Routes>
   );
 }

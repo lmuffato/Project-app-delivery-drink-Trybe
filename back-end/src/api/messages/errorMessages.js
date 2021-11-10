7d1b268134d36a6b32e8eacd3fea9beca3913396
iago <iago.pferreiravr@gmail.com>
2021-11-10 14:28:54 -0300

    feat(backend-login): Adicionando arquivo de mensagens de erro

---
+++ back-end/src/api/messages/errorMessages.js
@@ -0,0 +1,12 @@
+module.exports = {
+  ALL_FILDES_FILLED: 'All fields must be filled.',
+  EMAIL_ALREADY_REGISTRED: 'Email already registered.',
+  INCORRECT_USERNAME_OR_PASSWORD: 'Incorrect username or password.',
+  INVALID_ENTRIES: 'Invalid entries. Try again.',
+  NO_REGISTRED_TASKS: 'There are no registered tasks.',
+  NO_REGISTRED_USERS: 'There are no registered users',
+  NON_EXISTENTE_USER: 'Non-existent user.',
+  ONLY_ADMINS_REGISTER: 'Only admins can register new roles',
+  SERVER_ERROR: 'Server error.',
+  TASK_NOT_FOUND: 'Task not found.',
+};

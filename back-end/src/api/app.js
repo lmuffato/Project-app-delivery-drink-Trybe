9c266b506ee67e338aeda3838bbb744ba1142380
iago <iago.pferreiravr@gmail.com>
2021-11-10 18:28:38 -0300

    feat(backend-products): Criando a rota /products
    Ainda não está implementada a verificação de login

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { userController } = require('./controllers');
+const cors = require('cors');
+const { userController, productsController } = require('./controllers');
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 
 app.post('/login', userController.login);
+
+app.get('/products', productsController.findAllProducts);
 
 module.exports = app;

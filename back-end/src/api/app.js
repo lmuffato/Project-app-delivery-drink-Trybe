3cdf59b1b11a4ab49b70a201f23f7358eac2cdca
iago <iago.pferreiravr@gmail.com>
2021-11-10 15:20:42 -0300

    feat(backend-login): Cria a rota /login

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { userController } = require('./controllers');
 
 const app = express();
 
 app.use(bodyParser.json());
 
 app.get('/coffee', (_req, res) => res.status(418).end());
+
+app.post('/login', userController.login);
 
 module.exports = app;

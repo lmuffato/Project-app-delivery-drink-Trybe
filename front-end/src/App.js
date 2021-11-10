6ef3d3e18cc87d4cd57cd643f790214bd320f965
Marcus Hüsemann <mchusemann@gmail.com>
2021-11-10 15:43:10 -0300

    feat: cria rotas para pagina de login

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import './App.css';
-import rockGlass from './images/rockGlass.svg';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <div className="App">
-      <span className="logo">TRYBE</span>
-      <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
-        Glass
-      </object>
-    </div>
+    <Routes>
+      <Route exact path="/">
+        {/* Source:  https://gist.github.com/mjackson/b5748add2795ce7448a366ae8f8ae3bb */}
+        <Route path="/" element={ <Navigate replace to="/login" /> } />
+      </Route>
+      <Route path="/login" element={ <Login /> } />
+    </Routes>
   );
 }
 

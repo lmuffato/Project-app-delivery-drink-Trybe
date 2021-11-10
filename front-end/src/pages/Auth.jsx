d0c5ceb9dee53127cef695605c424e051f195e06
Joao Nasc <oi@joaonasc.dev>
2021-11-10 15:48:00 -0300

    feat: cria formulário para login

--- front-end/src/pages/Auth.jsx
+++ front-end/src/pages/Auth.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import imgManHoldingBeer from '../images/man-holding-beer.png';
 
 export default function Auth() {
   return (
-    <h1>Auth</h1>
+    <>
+      <img src={ imgManHoldingBeer } alt="Homem sorrindo e segurando caneca de cerveja" />
+      <img src="./logo-compact.svg" alt="tchau problema" />
+      <form>
+        <label htmlFor="email">
+          E-mail:
+          <input id="email" type="email" placeholder="Digite o seu e-mail" />
+        </label>
+        <label htmlFor="password">
+          Senha:
+          <input id="password" type="email" placeholder="Digite a sua senha" />
+        </label>
+        <button type="submit">Entrar</button>
+        <Link to="/register">Ainda não tenho conta</Link>
+      </form>
+    </>
   );
 }

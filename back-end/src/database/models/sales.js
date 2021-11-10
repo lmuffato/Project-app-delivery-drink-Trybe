5bc736cb587e4e2aa1e64c79a18d9cd9daf8cff4
Bruno Mendes <mendessbruno@gmail.com>
2021-11-10 16:25:12 -0300

    feat: implementado sequelize para criar o banco de dados

---
+++ back-end/src/database/models/sales.js
@@ -0,0 +1,22 @@
+module.exports = (sequelize, DataTypes) => {
+  const sales = sequelize.define('sales', {
+    seller_id: DataTypes.INTEGER,
+    total_price: DataTypes.DECIMAL,
+    delivery_address: DataTypes.STRING,
+    delivery_number: DataTypes.STRING,
+    sale_date: DataTypes.DATE,
+    status: DataTypes.STRING
+  }, {
+    timestamps: false,
+    tableName: 'sales',
+  });
+
+  sales.associate = (models) => {
+    sales.belongsTo(models.users,
+      { foreignKey: 'user_id', as: 'user' });
+    sales.belongsTo(models.users,
+      { foreignKey: 'seller_id', as: 'seller' });
+  };
+
+  return BlogPosts;
+};
---
+++ back-end/src/database/models/users.js
@@ -0,0 +1,20 @@
+module.exports = (sequelize, DataTypes) => {
+  const users = sequelize.define("users", {
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    role: DataTypes.STRING
+  }, {
+    timestamps: false,
+    tableName: 'users',
+  });
+
+  users.associate = (models) => {
+    users.hasMany(models.sales,
+      { foreignKey: 'user_id', as: 'sales' });
+    users.hasMany(models.sales,
+      { foreignKey: 'seller_id', as: 'sales' });
+  };
+
+  return users;
+};

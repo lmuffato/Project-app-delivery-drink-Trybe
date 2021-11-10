054391a3925a6502e51f6801dd13e15e62dd2cf4
Joao Nasc <oi@joaonasc.dev>
2021-11-10 20:06:45 -0300

    feat: valida campos

--- front-end/src/pages/Auth.jsx
+++ front-end/src/pages/Auth.jsx
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import imgManHoldingBeer from '../images/man-holding-beer.png';
 import useInputs from '../hooks/useInputs';
+import loginSchema from '../schemas/login';
 
 export default function Auth() {
-  const [, setInputs] = useInputs();
+  const [values, setInputs] = useInputs({ email: '', password: '' });
+  const [schemaStatus, setSchemaStatus] = useState({ valid: false, error: '' });
   const history = useHistory();
+
+  useEffect(() => {
+    const { error } = loginSchema.validate(values);
+    setSchemaStatus({ valid: error === undefined, error: error ? error.message : '' });
+  }, [values]);
 
   function logIn(event) {
     event.preventDefault();
-    history.push('/home');
+    try {
+      if (!schemaStatus.valid) throw new Error(schemaStatus.error);
+      history.push('/home');
+    } catch (error) {
+      console.error(error.message);
+    }
   }
 
   return (
@@ -30,7 +42,7 @@ export default function Auth() {
           Senha:
           <input
             id="password"
-            type="email"
+            type="password"
             placeholder="Digite a sua senha"
             onChange={ setInputs }
           />

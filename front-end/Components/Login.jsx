ed4c04b319f6d702ce2009e47b72b63c8593f5aa
Parker9309 <juan.tentiomaru9309@gmail.com>
2021-11-10 17:51:01 -0300

    fix: json and lint

--- front-end/Components/Login.jsx
+++ front-end/Components/Login.jsx
@@ -3,13 +3,15 @@ import React from 'react';
 export default function Login() {
   return (
     <div>
-        <form>
-         <label for="login">Login</label> 
-        <input type="email" id="email" placeholder="email@trybeer.com.br" required>
-        </input>
-         <label for="senha">Senha</label> 
-        <input type="password" id="senha" placeholder="*********">
-        </input>
+      <form>
+        <label htmlFor="login">
+          Login
+          <input type="email" id="email" placeholder="email@trybeer.com.br" required />
+        </label>
+        <label htmlFor="senha">
+          Senha
+          <input type="password" id="senha" placeholder="*********" />
+        </label>
         <button type="button">LOGIN</button>
         <button type="button">Ainda não tenho conta</button>
       </form>

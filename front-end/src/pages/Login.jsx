00c24fb28732c5a25de67fb292d15a84d02e3353
Marcus Hüsemann <mchusemann@gmail.com>
2021-11-10 17:14:36 -0300

    feat: cria validacoes e logica de ativaçao do botao de login

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,10 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const initialState = () => (
   { email: '', password: '', buttonStatus: true, errorMessage: '' }
 );
+
+const minPassLength = 6;
+
+const isValidEmail = (email) => /\w+@\w+\.\w+/gi.test(email);
+const isValidPassword = (password) => password.length >= minPassLength;
 function Login() {
   const [values, setValues] = useState(initialState);
+  // const [error, setErrors] = useState(null);
+
+  useEffect(() => {
+    setValues({
+      ...values,
+      buttonStatus: isValidEmail(values.email) && isValidPassword(values.password),
+    });
+  }, [values.email, values.password]);
 
   const onChange = (event) => {
     const { value, name } = event.target;
@@ -44,6 +57,7 @@ function Login() {
       <button
         data-testid="common_login__button-login"
         type="submit"
+        disabled={ !values.buttonStatus }
       >
         LOGIN
       </button>

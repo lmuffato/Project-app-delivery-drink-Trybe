d5e5244d837bc6079790c4f772625a72ad75b957
Rodrigo Rech <rodrigorech92@gmail.com>
2021-11-10 16:27:47 -0300

    Update: [api] - res status updated to 404 when user is not found

--- back-end/src/services/userService.js
+++ back-end/src/services/userService.js
@@ -3,7 +3,7 @@ const createJWT = require('../auth/createJWT');
 
 const login = async (email, password) => {
   const user = await User.findOne({ where: { email, password } })
-  if (user === null) return { status: 401, message: { message: 'Incorrect username or password' } }
+  if (user === null) return { status: 404, message: { message: 'Incorrect username or password' } }
   const { password, ...userWithoutPassword } = user;
   const token = createJWT(userWithoutPassword);
   return { status: 200, message: { token } };

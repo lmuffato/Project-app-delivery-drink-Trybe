34eba244ea349704a64f0d4ea68f9b6dfd113b76
Rodrigo Rech <rodrigorech92@gmail.com>
2021-11-10 16:30:46 -0300

    Feat: [api] middlewares added to app.port(login) route

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const userControllers = require('../controllers/userControllers')
+const userControllers = require('../controllers/userControllers');
+const { checkEmail } = require('../middlewares/checkEmail');
+const { checkPassword } = require('../middlewares/checkPassword');
 
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/login', userControllers.login)
+app.post('/login', checkEmail, checkPassword, userControllers.login)
 
 module.exports = app;

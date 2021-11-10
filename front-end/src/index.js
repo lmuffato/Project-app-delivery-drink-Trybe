3c8b4ecbf07763ab205afc31284c17d9e2c60750
Hernanne Domiciano <78621976+hernannegp@users.noreply.github.com>
2021-11-10 18:39:35 -0300

    feat: initial config redux

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -5,10 +5,14 @@ import './index.css';
 import App from './App';
 // import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
+import store from '../src/redux/store';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
   <BrowserRouter>,   
+    <Provider store={store}>
       <App />
+    </Provider>,
   </BrowserRouter>,
   document.getElementById('root'),
 );

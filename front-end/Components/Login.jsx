3a09be6b19b58ee82724574cd50c06ec96ff4072
Parker9309 <juan.tentiomaru9309@gmail.com>
2021-11-10 16:20:29 -0300

    Fix: eslint

--- front-end/Components/Login.jsx
+++ front-end/Components/Login.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 
 export default function Login() {
     return (
-      <div>
-        <form>
-          <input type="email" placeholder="email@trybeer.com.br" required>
+    <div>
+      <form>
+        <input type="email" placeholder="email@trybeer.com.br" required>
           Login
-          </input>
+        </input>
           <input type="password" placeholder="*********">
           Senha
           </input>
-          <button type="button">LOGIN</button>
-          <button type="button">Ainda não tenho conta</button>
-        </form>
-      </div>
-    );
+        <button type="button">LOGIN</button>
+        <button type="button">Ainda não tenho conta</button>
+      </form>
+    </div>
+  );
 }

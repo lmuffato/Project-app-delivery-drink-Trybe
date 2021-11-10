6fe52dfae12f68b742d65bc4af151a9c1fd94534
Parker9309 <juan.tentiomaru9309@gmail.com>
2021-11-10 16:51:51 -0300

    fix: lint5 and sintax

--- front-end/Components/Login.jsx
+++ front-end/Components/Login.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 
 export default function Login() {
   return (
-      <div>
-      <form>
-        <input type="email" placeholder="email@trybeer.com.br" required>
-          Login
+    <div>
+        <form>
+         <label for="login">Login</label> 
+        <input type="email" id="email" placeholder="email@trybeer.com.br" required>
         </input>
-        <input type="password" placeholder="*********">
-          Senha
+         <label for="senha">Senha</label> 
+        <input type="password" id="senha" placeholder="*********">
         </input>
         <button type="button">LOGIN</button>
         <button type="button">Ainda não tenho conta</button>
       </form>
-      </div>
+    </div>
   );
 }

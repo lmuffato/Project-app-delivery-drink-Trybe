95a31ff7fab8b4845d6303eafbcdeb568b198c03
Lucas André <lucas.cancado89@gmail.com>
2021-11-10 19:40:19 -0300

    adiciona productCard

---
+++ back-end/src/services/productsService.js
@@ -0,0 +1,10 @@
+const { Product } = require('../database/models/index');
+
+const getProducts = async () => {
+  const products = await Product.findAll();
+  return { status: 200, data: products };
+};
+
+module.exports = {
+  getProducts,
+}
--- front-end/src/pages/CustomerProducts.jsx
+++ front-end/src/pages/CustomerProducts.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import NavBar from '../components/CustomerNavBar';
+import ProductCard from '../components/ProductCard';
 
 function ClientProducts() {
+  const [productList, setProductList] = useState([]);
+
+  async function getProducts() {
+    const productsReq = await axios.get('http://localhost:3001/products');
+    const products = productsReq.data;
+    setProductList(products);
+  }
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
   return (
     <div>
       <NavBar fixed="top" />
+      {
+        productList.map((prod) => <ProductCard key={ prod.name } product={ prod } />)
+      }
     </div>
   );
 }

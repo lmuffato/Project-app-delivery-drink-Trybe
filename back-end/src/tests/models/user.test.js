bada1a1e95117335257cc1156b0b05946546c1f0
Marcelo Maurício <mmauricio.design@gmail.com>
2021-11-10 16:45:58 -0300

    Feat 0: created a unitary test about User model

---
+++ back-end/src/tests/models/user.test.js
@@ -0,0 +1,17 @@
+const {
+  sequelize,
+  dataTypes,
+  checkModelName,
+  checkPropertyExists,
+} = require('sequelize-test-helpers');
+
+const UserModel = require('../../database/models/User');
+
+describe('O model de User', () => {
+  const User = UserModel(sequelize, dataTypes);
+  const user = new User();
+
+  describe('possui o nome "User"', () => {
+    checkModelName(User)('User');
+  });
+});

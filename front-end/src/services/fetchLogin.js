84d6fa79012b2c9b6c2f422712fca8cceae8fa0c
Rodrigo Rech <rodrigorech92@gmail.com>
2021-11-11 18:09:56 -0300

    Feat: [front] - login connection with backend

---
+++ front-end/src/services/fetchLogin.js
@@ -0,0 +1,17 @@
+const fetchLogin = async (email, password) => {
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    mode: 'cors',
+    body: JSON.stringify({ email, password }),
+  };
+  console.log(email, password);
+  const response = await fetch('http://localhost:3001/login', requestOptions)
+    .then((res) => res.json())
+    .then((data) => data);
+  return response;
+};
+
+export default fetchLogin;

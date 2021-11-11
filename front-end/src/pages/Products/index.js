87fa6880a65a91964446263ff066a8bc2f43ec55
Marcelo Maurício <mmauricio.design@gmail.com>
2021-11-11 18:54:14 -0300

    Feat 11: create page for products

---
+++ front-end/src/pages/Products/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function ProductsPage() {
+  const teste = 'test';
+
+  return (
+    <>
+      <h1>Página de Produtos</h1>
+      <p>{ teste }</p>
+    </>
+  );
+}
+
+export default ProductsPage;

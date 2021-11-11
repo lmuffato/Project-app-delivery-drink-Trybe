6843abe6cd20131020e9e9f2ed2ddc7d10668d24
Marcus Hüsemann <mchusemann@gmail.com>
2021-11-10 21:34:13 -0300

    feat: adiciona importaçao e uso do cors

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const { userController } = require('./controllers');
 
 const app = express();
-
+app.use(cors());
 app.use(bodyParser.json());
 
 app.get('/coffee', (_req, res) => res.status(418).end());

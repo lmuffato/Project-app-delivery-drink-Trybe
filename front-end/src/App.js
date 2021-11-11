1b94ea956ba6b8cda1f5a1851e6ef04f9f877a46
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-11-11 08:50:24 -0300

    feat: adiciona rota /admin/manage para a página AdminManagerUsers

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 // import rockGlass from './images/rockGlass.svg';
 import { Routes, Route } from 'react-router-dom';
+import AdminManagerUsers from './pages';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={ <div>Xablau</div> } />
+      <Route path="/admin/manage" exact element={ <AdminManagerUsers /> } />
     </Routes>
   );
 }

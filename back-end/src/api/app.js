d1538de6b04f4e57af81e0e117e2f0fcdff5e5e2
Gui-lira <gui.lira.martins@gmail.com>
2021-11-11 14:45:43 -0300

    endpoint da api pronto

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const { getSalesBySeller } = require('../controllers');
 
 const app = express();
 
+app.use(bodyParser.json());
+
 app.get('/coffee', (_req, res) => res.status(418).end());
+
+app.get('/sales', getSalesBySeller);
 
 module.exports = app;

912804d5105430e576cf2c28200e5702dad32782
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2021-11-11 17:17:38 -0300

    feat, form login

--- front-end/src/pages/Login.js
+++ front-end/src/pages/Login.js
@@ -1,12 +1,51 @@
 import React from 'react';
+// import axios from 'axios';
 
 export default function Login() {
   // const [email, setEmail] = useState('');
   // const [password, setPassword] = useState('');
+  // const [loginButton, setLoginButton] = useState(false);
+  // function loginBtn() {}
 
   return (
     <main>
-      <div> xablau </div>
+      <h1>Login</h1>
+      <form>
+        <input
+          type="email"
+          name="Login-email"
+          className="email-input"
+          placeholder="exemplo@email.com"
+          onChange={ console.log('validation') }
+          data-testid="common_login__input-email"
+        />
+        <input
+          type="password"
+          name="Login-password"
+          className="password-input"
+          placeholder="Digite a senha"
+          onChange={ console.log('validation') }
+          data-testid="common_login__input-password"
+        />
+        <button
+          type="submit"
+          className="button-input"
+          onClick={ console.log('validation') }
+          // disabled={ !loginButton }
+        >
+          Login
+        </button>
+      </form>
+      <button
+        type="button"
+        className="register-input"
+        data-testid="common_login__button-register"
+      >
+        Registre-se
+      </button>
+      <div>
+        <p>Aqui vai um erro</p>
+      </div>
     </main>
   );
 }

a7784684ecdec7be47dcf44cb1bb98d4e404b0e5
AndreBarroso <andre_barroso17@hotmail.com>
2021-11-11 15:59:47 -0300

    feat: cria rota gets de products

--- back-end/src/api/server.js
+++ back-end/src/api/server.js
@@ -2,6 +2,7 @@ const port = process.env.PORT || 3001;
 const app = require('./app');
 const usersControllers = require('../layers/users/usersControllers');
 const salesControllers = require('../layers/sales/salesControllers');
+const productsControllers = require('../layers/products/productsControllers');
 
 app.get('/test', (_req, res) => {
   return res.status(418).json({ message: "mensagem de test" });
@@ -18,6 +19,11 @@ http://localhost:3001/users
 app.use('/sales', salesControllers);
 /*
 http://localhost:3001/sales
+*/
+
+app.use('/products', productsControllers);
+/*
+http://localhost:3001/products
 */
 
 app.listen(port);

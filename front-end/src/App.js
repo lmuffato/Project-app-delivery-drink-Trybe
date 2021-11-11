f2914b08d7b1271b9cc0147a10174a189785f5c2
giovanni-maldonado <giovannitadeu08@gmail.com>
2021-11-11 19:29:29 -0300

    feat: rotas para login

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import Login from './pages/Login';
 
 function App() {
   return (
     <div className="App">
-      <span className="logo">TRYBE</span>
-      <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
-        Glass
-      </object>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" render={ () => <Redirect to="/login" /> } />
+          <Route path="/login" component={ Login } />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }

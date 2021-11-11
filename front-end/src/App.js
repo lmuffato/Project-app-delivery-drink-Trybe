a2b355f519f78e6ad6f2cc55c16fe34e6498ac49
Joao Nasc <oi@joaonasc.dev>
2021-11-10 21:16:13 -0300

    feat: adiciona imagem de fundo

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import Routes from './Routes';
+import background from './images/background.png';
 
 function App() {
   return (
-    <Routes />
+    <div className="App" style={ { background: `url('${background}') center / cover` } }>
+      <Routes />
+    </div>
   );
 }
 

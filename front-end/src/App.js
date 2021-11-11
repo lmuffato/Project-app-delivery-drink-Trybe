710521ffc2fe046e1586c4ffb1354cda5fe17fff
igson <igson660@gmail.com>
2021-11-10 22:50:44 -0500

    fix: mifica os inputs do component Login para somente acitares dados bem formatados

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
+// import { Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
+// import Register from './pages/Register';
 
 function App() {
-  return (<p> em construção </p>);
+  return (
+    <>
+      <Login />
+      {/* <Register />
+      <Switch>
+        <Route path="/" exact component={ Login } />
+        <Route path="/login" exact component={ Login } />
+        <Route path="/register" exact component={ Register } />
+      </Switch> */}
+    </>
+  );
 }
 
 export default App;

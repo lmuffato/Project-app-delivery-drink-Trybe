9aba6c3be672eefa92ca18e107aa1a58dded0aa8
Rodrigo Rech <rodrigorech92@gmail.com>
2021-11-11 19:06:20 -0300

    Feat: Route /customer/products created at app.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import Login from './pages/Login';
+import CustomerProducts from './pages/CustomerProducts'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
             <Redirect to="/login" />
           </Route>
           <Route exact path="/login" component={ Login } />
+          <Route exact path="/customer/products" component={ CustomerProducts } />
         </Switch>
       </BrowserRouter>
     </div>

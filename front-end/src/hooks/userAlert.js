6d5eb686f0ef9aea3f40509baed3bdae28b4478c
jonathan <jonathanfernandesjf98@gmail.com>
2021-11-11 20:04:18 -0300

    feat: criacao do hook personalizado para o componente alert

---
+++ front-end/src/hooks/userAlert.js
@@ -0,0 +1,44 @@
+
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import Element from '../components/alert';
+
+export default function useAlert(timeout) {
+  const [alertType, setAlertType] = useState('');
+  const [alertMessage, setAlertMessage] = useState('');
+  const [showAlert, setShowAlert] = useState(false);
+
+  useEffect(() => {
+    const defaultTimeout = 5000;
+    if (showAlert) {
+      setTimeout(() => {
+        setShowAlert(false);
+      }, timeout || defaultTimeout);
+    }
+  }, [showAlert, timeout]);
+
+  const Alert = ({ dataTestId }) => (
+    <Element
+      type={ alertType }
+      message={ alertMessage }
+      show={ showAlert }
+      dataTestId={ dataTestId }
+    />
+  );
+
+  Alert.propTypes = {
+    dataTestId: PropTypes.string,
+  };
+
+  Alert.defaultProps = {
+    dataTestId: '',
+  };
+
+  return {
+    Alert,
+    alertMessage: setAlertMessage,
+    alertType: setAlertType,
+    isVisible: showAlert,
+    showAlert: setShowAlert,
+  };
+}

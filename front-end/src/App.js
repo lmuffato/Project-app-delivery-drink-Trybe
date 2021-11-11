99e5c6f27969eb43ef4ad92cbc5fc0c7f3602eaf
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-11-11 14:58:15 -0300

    adiciona rota para página AdminManagerUsers

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import './App.css';
 // import rockGlass from './images/rockGlass.svg';
-import { Routes, Route } from 'react-router-dom';
-import AdminManagerUsers from './pages';
-import Login from './components/Login';
+import AllRoute from './routes/AllRoute';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={ <div>Xablau</div> } />
-      <Route path="/admin/manage" exact element={ <AdminManagerUsers /> } />
-      <Route path="/" element={ <Login /> } />
-    </Routes>
+    <AllRoute />
   );
 }
 

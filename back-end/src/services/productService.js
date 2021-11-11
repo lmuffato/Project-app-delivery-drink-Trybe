a4c6b3e22a70fb4dce613fbfa6bb81da2c2e7da7
Felipe Flores <felipefloresvendas@gmail.com>
2021-11-11 11:05:57 -0300

    :sparkles: feat: added post Sale, and products images

--- back-end/src/services/productService.js
+++ back-end/src/services/productService.js
@@ -1,4 +1,4 @@
-const { Product } = require('../database/models');
+const { Product, Sales } = require('../database/models');
 const errorMap = require('../utils/errorMap');
 
 const getAll = async () => {
@@ -12,4 +12,21 @@ const getAll = async () => {
   }
 };
 
-module.exports = { getAll };
+const postProducts = async (data) => {
+  const {
+    userId, sellerId, totalPrice, deliveryAddress, deliveryNumber, products } = data;
+    // products [{ idDoProduto: as61a65s, quantidade: 3 }]
+    // status = 'pendente'
+  try {
+    const result = await Sales.create({
+      userId, sellerId, totalPrice, deliveryAddress, deliveryNumber, status: 'pendente',
+    });
+    if (!result) return errorMap.NotFound;
+    
+    return { result };
+  } catch (error) {
+    return errorMap.internalError;
+  }
+};
+
+module.exports = { getAll, postProducts };

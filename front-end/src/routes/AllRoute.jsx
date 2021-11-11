2ac20b4f1b7fd0a0bc03961b11ba0b1c079eb3b4
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-11-11 14:53:10 -0300

    adiciona rota para página AdminManagerUsers

--- front-end/src/routes/AllRoute.jsx
+++ front-end/src/routes/AllRoute.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Login from '../components/Login';
+import AdminManagerUsers from '../pages';
 
 export default function AllRoute() {
   return (
     <Routes>
       <Route path="/" element={ <Login /> } />
       <Route path="/login" element={ <Login /> } />
+      <Route path="/admin/manage" element={ <AdminManagerUsers /> } />
     </Routes>
   );
 }

9ebb475ecf28e8c5042126b92dae7c2e570642d4
Marcelo Maurício <mmauricio.design@gmail.com>
2021-11-11 18:25:49 -0300

    Feat 11: created a endpoint the app for /products

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+
 const loginRouter = require('../router/login');
 const registrationRouter = require('../router/registration');
+const productRouter = require('../router/product');
 
 const app = express();
 
@@ -13,5 +15,7 @@ app.get('/coffee', (_req, res) => res.status(418).end());
 
 app.use('/login', loginRouter);
 app.use('/registration', registrationRouter);
+
+app.use('/products', productRouter);
 
 module.exports = app;

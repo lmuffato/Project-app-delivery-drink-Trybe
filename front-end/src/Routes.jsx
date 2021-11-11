c02930a0b55c2d18ccf839c6e9f466e2b28bdd9b
Joao Nasc <oi@joaonasc.dev>
2021-11-11 20:33:38 -0300

    feat: cria rota para produtos

--- front-end/src/Routes.jsx
+++ front-end/src/Routes.jsx
@@ -5,6 +5,7 @@ import Auth from './pages/Auth';
 export default function Routes() {
   return (
     <Switch>
+      <Route path="/customer/products" render={ () => <h1>Produtos</h1> } />
       <Route path="/login" component={ Auth } />
       <Route path="/" render={ () => <Redirect to="/login" /> } />
     </Switch>

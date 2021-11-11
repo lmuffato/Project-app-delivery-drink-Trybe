c749b5790e19d60bf02c68726977a17f2e70781e
Parker9309 <juan.tentiomaru9309@gmail.com>
2021-11-11 16:55:46 -0300

    feat: Register page created and already implemented on routes

--- front-end/src/Components/Routes.jsx
+++ front-end/src/Components/Routes.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Redirect, Switch, Route } from 'react-router-dom';
+import Register from './Register';
 import Login from './Login';
 
 export default function Rout() {
@@ -7,6 +8,7 @@ export default function Rout() {
     <Switch>
       <Route path="/login" component={ Login } />
       <Route path="/" render={ () => <Redirect to="/login" /> } />
+      <Route path="/register" component={ Register } />
     </Switch>
   );
 }

f32375fa98346a07587e0fbc48f251d28d9afa33
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-11-11 17:32:53 -0300

    modifica estado global de ManagerUsersProvider, adiciona uma variável para salvar todos os usuários e outra para salvar o usuário cadastrado.

--- front-end/src/provider/ManagerUsersProvider.js
+++ front-end/src/provider/ManagerUsersProvider.js
@@ -1,13 +1,21 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { element } from 'prop-types';
 import { ManagerUsersContext } from '../context';
+import isNotEmptyObject from '../utils/isNotEmptyObject';
 
 function ManagerUsersProvider({ children }) {
-  const [users, setUsers] = useState();
+  const [users, setUsers] = useState([]);
+  const [user, setUser] = useState({});
   const consumer = {
     users,
     setUsers,
+    user,
+    setUser,
   };
+
+  useEffect(() => {
+    if (isNotEmptyObject(user)) { setUsers((prev) => [...prev, user]); }
+  }, [user]);
 
   return (
     <ManagerUsersContext.Provider value={ { ...consumer } }>

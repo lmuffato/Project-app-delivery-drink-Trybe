2fa14822b971908880b455ba8f56c4c2c9adb901
Felipe Shcolnik <shcolnik@gmail.com.br>
2021-11-11 16:48:22 -0300

    fix esLint '/' unnecessary

--- back-end/src/api/services/productService.js
+++ back-end/src/api/services/productService.js
@@ -1,4 +1,4 @@
-const { Product } = require('../../database/models/');
+const { Product } = require('../../database/models');
 
 const getAll = async () => {
   const result = await Product.findAll();

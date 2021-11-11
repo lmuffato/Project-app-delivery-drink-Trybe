11b59d21eabc04310667d232bedbcc55847ef699
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-11-11 11:12:18 -0300

    refactor: refatora FormRegisterUser e adiciona validação do formulário

--- front-end/src/components/FormRegisterUser/index.js
+++ front-end/src/components/FormRegisterUser/index.js
@@ -1,73 +1,112 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useValidator } from 'react-joi';
+import {
+  initialData, schema, explicitCheck,
+} from '../../utils/validateFormRegisterConfigOptions';
 
 const FormRegisterUser = () => {
-  const INITIAL_STATE = {
-    name: '',
-    email: '',
-    password: '',
-    role: 'client',
-  };
+  const [enableButton, setEnableButton] = useState(true);
 
-  const [dataUser, setDataUser] = useState(INITIAL_STATE);
+  const {
+    state, setData,
+    setExplicitField,
+    validate,
+  } = useValidator({ initialData, schema, explicitCheck });
 
   const handleForm = ({ target: { value, name } }) => {
-    setDataUser((prevState) => ({
+    setData((prevState) => ({
       ...prevState,
       [name]: value,
     }));
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    validate();
+  };
+
+  useEffect(() => {
+    const enableOrDisable = state.$auto_invalid;
+    setEnableButton(enableOrDisable);
+  }, [state.$auto_invalid]);
+
   return (
-    <form>
-      <label htmlFor="name">
-        <input
-          type="text"
-          name="name"
-          id="name"
-          data-testid="admin_manage__input-name"
-          onChange={ handleForm }
-          value={ dataUser.name }
-        />
-      </label>
-      <label htmlFor="email">
-        <input
-          type="email"
-          name="email"
-          id="email"
-          data-testid="admin_manage__input-email"
-          onChange={ handleForm }
-          value={ dataUser.email }
-        />
-      </label>
-      <label htmlFor="password">
-        <input
-          type="password"
-          name="password"
-          id="password"
-          data-testid="admin_manage__input-password"
-          onChange={ handleForm }
-          value={ dataUser.password }
-        />
-      </label>
+    <>
+      <form onSubmit={ handleSubmit }>
+        <label htmlFor="name">
+          Nome
+          <input
+            type="text"
+            name="name"
+            id="name"
+            data-testid="admin_manage__input-name"
+            onChange={ handleForm }
+            onBlur={ () => setExplicitField('name', true) }
+            value={ state.$data.name }
+            placeholder="Mínimo 12 caracteres"
+          />
+        </label>
 
-      <select
-        data-testid="admin_manage__select-role"
-        onChange={ handleForm }
-        name="role"
-        value={ dataUser.role }
-      >
-        <option value="salesman">Vendedor</option>
-        <option value="client">Cliente</option>
-        <option value="administrator">Administrator</option>
-      </select>
+        <label htmlFor="email">
+          Email
+          <input
+            type="text"
+            name="email"
+            id="email"
+            data-testid="admin_manage__input-email"
+            onChange={ handleForm }
+            onBlur={ () => setExplicitField('email', true) }
+            value={ state.$data.email }
+            placeholder="example@email.com"
+          />
+        </label>
 
-      <button
-        type="submit"
-        data-testid="admin_manage__button-register"
-      >
-        cadastrar
-      </button>
-    </form>
+        <label htmlFor="password">
+          Senha
+          <input
+            type="password"
+            name="password"
+            id="password"
+            data-testid="admin_manage__input-password"
+            onChange={ handleForm }
+            onBlur={ () => setExplicitField('password', true) }
+            value={ state.$data.password }
+            placeholder="Mínimo 6 caracteres"
+          />
+        </label>
+
+        <label htmlFor="role">
+          Tipo
+          <select
+            id="role"
+            data-testid="admin_manage__select-role"
+            onChange={ handleForm }
+            name="role"
+            onBlur={ () => setExplicitField('role', true) }
+            value={ state.$data.role }
+          >
+            <option value="salesman">Vendedor</option>
+            <option value="client">Cliente</option>
+            <option value="administrator">Administrator</option>
+          </select>
+        </label>
+
+        <button
+          type="submit"
+          data-testid="admin_manage__button-register"
+          disabled={ enableButton }
+        >
+          cadastrar
+        </button>
+      </form>
+
+      <span>
+        {state.$errors.name.map((data) => data.$message).join(',')}
+        {state.$errors.email.map((data) => data.$message).join(',')}
+        {state.$errors.password.map((data) => data.$message).join(',')}
+        {state.$errors.role.map((data) => data.$message).join(',')}
+      </span>
+    </>
   );
 };
 

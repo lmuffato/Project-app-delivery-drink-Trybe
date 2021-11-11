9dbf52fb39e706f14527a2443e6ded9ef1d45d02
Luciano Amâncio <madruga665@hotmail.com>
2021-11-11 13:28:33 -0300

    recupera valores digitados nos inputs

--- front-end/src/components/InputField/InputField.jsx
+++ front-end/src/components/InputField/InputField.jsx
@@ -2,13 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './styles.module.css';
 
-export default function InputField({ labelName, type, name, id }) {
+export default function InputField({ labelName, type, name, id, value, onChange }) {
   return (
     <div className={ styles.inputContainer }>
       <label htmlFor={ id }>
         {labelName}
       </label>
-      <input className={ styles.input } type={ type } name={ name } id={ id } />
+      <input
+        className={ styles.input }
+        type={ type }
+        name={ name }
+        id={ id }
+        value={ value }
+        onChange={ onChange }
+      />
     </div>
   );
 }
--- front-end/src/components/LoginForm/LoginForm.jsx
+++ front-end/src/components/LoginForm/LoginForm.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '../Button';
 import InputField from '../InputField/InputField';
 import styles from './styles.module.css';
 
 export default function LoginForm() {
+  const [email, setEmail] = useState('');
+  const [password, setpassword] = useState('');
+
   return (
     <form className={ styles.loginFormContainer }>
       <InputField
@@ -11,12 +14,16 @@ export default function LoginForm() {
         type="email"
         name="loginInput"
         id="loginInput"
+        value={ email }
+        onChange={ (event) => setEmail(event.target.value) }
       />
       <InputField
         labelName="Senha"
         type="password"
         name="passwordInput"
         id="passwordInput"
+        value={ password }
+        onChange={ (event) => setpassword(event.target.value) }
       />
       <Button title="Login" typeButton="primary" />
       <Button title="Ainda não tem conta" typeButton="tertiary" />

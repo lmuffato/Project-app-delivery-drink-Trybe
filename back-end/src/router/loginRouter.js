de484b126e69870278437f4436aad1b57a85c6c2
Eduardo Costa Maciel <costaeduardomaciel@gmail.com>
2021-11-11 19:33:42 -0300

    Fix: Ajusta funçao services/loginUser, controlles/loginUser e router/loginRouter

--- back-end/src/router/loginRouter.js
+++ back-end/src/router/loginRouter.js
@@ -2,9 +2,10 @@ const express = require('express');
 const rescue = require('express-rescue');
 
 const loginController = require('../controllers/login');
+const validateLogin = require('../middlewares/validateLogin');
 
 const loginRouter = express.Router();
 
-loginRouter.post('/', rescue(loginController.loginUser));
+loginRouter.post('/', validateLogin, rescue(loginController.loginUser));
 
 module.exports = loginRouter;

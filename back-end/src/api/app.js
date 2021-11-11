65c831296c935ab1c56a5c409cd4fb8e784187db
Orlando Flores <orlando_flores@id.uff.br>
2021-11-11 19:57:23 -0300

    feat: add cors to app

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const loginRouter = require('../routers/loginRouter');
 const registerRouter = require('../routers/registerRouter');
 
 const app = express();
+
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
+}));
 app.use(bodyParser.json());
 
 app.use('/login', loginRouter);

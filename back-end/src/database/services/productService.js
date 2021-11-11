d9c35115dac50c353586f26fbf5505379388e597
Marcelo Maurício <mmauricio.design@gmail.com>
2021-11-11 18:20:51 -0300

    Fix 11: the Product service layer to the default code implementation to utilized in the project

--- back-end/src/database/services/productService.js
+++ back-end/src/database/services/productService.js
@@ -1,11 +1,12 @@
 const { Product } = require('../models');
+const { HTTP_CREATED, HTTP_CONFLICT, HTTP_OK_STATUS } = require('../../status');
 
-const getAll = async () => {
+async function getAll() {
   const products = await Product.findAll();
 
-  if (!products) return { status: 400, message: 'Products are empty' };
+  if (!products) return { status: HTTP_CONFLICT, message: 'Products are empty' };
 
-  return { status: 200, data: products };
+  return { data: products, code: HTTP_OK_STATUS,  };
 };
 
 module.exports = {

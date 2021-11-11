adac8a91cfd9da2973739c3175397885a739efc5
lmuffato <lmiremuffato@gmail.com>
2021-11-10 22:17:19 -0300

    estruturaçao do sequelize e acesso a tabela users

--- back-end/src/api/server.js
+++ back-end/src/api/server.js
@@ -1,5 +1,15 @@
 const port = process.env.PORT || 3001;
 const app = require('./app');
+const userController = require('../layers/users/userController');
+
+app.get('/test', (_req, res) => {
+  return res.status(418).json({ message: "mensagem de test" });
+});
+/*
+http://localhost:3001/test
+*/
+
+app.use('/users', userController);
 
 app.listen(port);
 console.log(`Api rodando na porta ${port}`);

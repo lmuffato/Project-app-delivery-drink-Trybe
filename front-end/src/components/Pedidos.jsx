a246f58f7cf162cdd5f1991128bb592f305fd2ae
biazidioti <bzferreira.bio@gmail.com>
2021-11-11 15:16:17 -0300

    All front routes

---
+++ front-end/src/components/Pedidos.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Pedidos() {
+  return (
+    <div>
+      <h1>Pedidos</h1>
+    </div>
+  );
+}
--- front-end/src/routes/AllRoute.jsx
+++ front-end/src/routes/AllRoute.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Login from '../components/Login';
+import Cadastro from '../components/Cadastro';
+import Pedidos from '../components/Pedidos';
+import Produtos from '../components/Produtos';
+import PedidoDetalhes from '../components/PedidoDetalhes';
+import CheckoutComprador from '../components/CheckoutComprador';
+import Vendas from '../components/Vendas';
+import VendaDetalhes from '../components/VendaDetalhes';
+import Admin from '../components/Admin';
+import NotFound from '../components/NotFound';
 
 export default function AllRoute() {
   return (
     <Routes>
-      <Route path="/" element={ <Login /> } />
       <Route path="/login" element={ <Login /> } />
+      <Route path="/" element={ <Navigate replace to="/login" /> } />
+      <Route path="/register" element={ Cadastro } />
+      <Route path="/customer/orders" element={ Pedidos } />
+      <Route path="/customer/products" element={ Produtos } />
+      <Route path="/customer/orders/:id" element={ PedidoDetalhes } />
+      <Route path="/customer/checkout" element={ CheckoutComprador } />
+      <Route path="/seller/orders" element={ Vendas } />
+      <Route path="/seller/orders/:id" element={ VendaDetalhes } />
+      <Route path="/admin/manage" element={ Admin } />
+      <Route element={ NotFound } />
     </Routes>
   );
 }

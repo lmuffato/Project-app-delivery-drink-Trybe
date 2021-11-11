066e3a4d57516fe799df4355a9fcafbd96a762f8
Luciano Amâncio <madruga665@hotmail.com>
2021-11-11 09:27:38 -0300

    add LoginPage na rota '/'

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
+import { Route, Routes } from 'react-router';
+import LoginPage from './pages/LoginPage';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
 
 function App() {
   return (
-    <div className="App">
-      <span className="logo">TRYBE</span>
-      <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
-        Glass
-      </object>
-    </div>
+    <Routes>
+      <Route exact path="/" element={ <LoginPage /> } />
+    </Routes>
   );
 }
 

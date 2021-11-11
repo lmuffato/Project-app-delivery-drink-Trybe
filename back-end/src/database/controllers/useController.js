2e76980f1892b69f572e329fc78604009705bb7d
Gabriel <g.pereira.a.m@gmail.com>
2021-11-11 16:42:37 -0300

    feat(Req 3 and  8) - :sparkle: created Service Login

--- back-end/src/database/controllers/useController.js
+++ back-end/src/database/controllers/useController.js
@@ -1,37 +1,31 @@
-const { HTTP_NOT_FOUND } = require('../../status');
+const { HTTP_NOT_FOUND, HTTP_BAD_REQUEST } = require('../../status');
 const useService = require('../services/useService');
 
 async function createUser(req, res) {
   try {
     const { name, email, password } = req.body;
-    const { isRegistered, code, error, data } = await useService.createUserService({ name, email, password, role });
+    const { isRegistered, code, error, data } = await useService.createUserService({ name, email, password });
 
     if (isRegistered) return res.status(code).json({ error });
 
     if (!isRegistered) return res.status(code).json(data);
   } catch (e) {
-    return res.status(HTTP_NOT_FOUND).json({ error: error.message });
+    return res.status(HTTP_NOT_FOUND).json({ error: e.message });
   }
 }
 
 async function login(req, res) {
   try {
     const { email, password } = req.body;
-    const userLogin = { password, email };
-    const isUserResgistered = await findUserByEmail(email);
+    const { notFound, invalidPassword, code, error, data } = await useService.loginService({ email, password });
 
-    if (!isUserResgistered) {
-      return res.status(409).json({ error: "User dont exists" });
-    }
+    if (notFound) return res.status(code).json({ error });
 
-    if (password !== isUserResgistered.password) {
-      return res.status(409).json({ error: "Invalid data" });
-    } else {
-      const token = await createToken(userLogin);
-      return res.status(200).json({ token });
-    }
-  } catch (error) {
-    return res.status(400).json({ error: error.message });
+    if (invalidPassword) return res.status(code).json({ error });
+
+    return res.status(code).json(data);
+  } catch (e) {
+    return res.status(HTTP_BAD_REQUEST).json({ error: e.message });
   }
 }
 
--- back-end/src/database/services/useService.js
+++ back-end/src/database/services/useService.js
@@ -1,5 +1,5 @@
 const { User } = require('../models');
-const { HTTP_CREATED, HTTP_CONFLICT } = require('../../status');
+const { HTTP_CREATED, HTTP_CONFLICT, HTTP_OK_STATUS } = require('../../status');
 const { createToken } = require('../../validations/auth/validateJWT');
 
 async function readByEmailService(email) {
@@ -7,25 +7,43 @@ async function readByEmailService(email) {
   return user;
 };
 
-async function createUserService({ name, email, password, role }) {
+async function createUserService({ name, email, password }) {
   const isUserResgistered = await readByEmailService(email);
+  const role = "customer";
 
   if (isUserResgistered) {
     return { isRegistered: true, code: HTTP_CONFLICT, error: "User already registered" };
-  }
+  };
 
-  await User.create({name, email, password, role });
+  await User.create({ name, email, password, role });
 
-  const userLogin = { name, email, role};
-
+  const userLogin = { name, email, role };
   const token = await createToken(userLogin);
-
   const data = { name, email, role, token };
 
-  return { data, code: HTTP_CREATED};
+  return { data, code: HTTP_CREATED };
+};
+
+async function loginService({ password, email }) {
+  const isUserResgistered = await readByEmailService(email);
+
+  if (!isUserResgistered) {
+    return { notFound: true, code: HTTP_CONFLICT, error: "User dont exists" };
+  };
+
+  if (password !== isUserResgistered.password) {
+    return { invalidPassword: true, code: HTTP_CONFLICT, error: "Invalid data" }
+  };
+
+  const { name, role } = await readByEmailService(email);
+  const userLogin = { name, role, email };
+  const token = await createToken(userLogin);
+  const data = { name, email, role, token };
+
+  return { data, code: HTTP_OK_STATUS };
 };
 
 module.exports = {
   createUserService,
-  readByEmailService,
+  loginService,
 };

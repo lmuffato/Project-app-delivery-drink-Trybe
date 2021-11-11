6cb6bc71a18903147e47aac4c02608aee97604fe
biazidioti <bzferreira.bio@gmail.com>
2021-11-11 15:32:16 -0300

    Renders JSX from routes!

--- front-end/src/routes/AllRoute.jsx
+++ front-end/src/routes/AllRoute.jsx
@@ -16,15 +16,15 @@ export default function AllRoute() {
     <Routes>
       <Route path="/login" element={ <Login /> } />
       <Route path="/" element={ <Navigate replace to="/login" /> } />
-      <Route path="/register" element={ Cadastro } />
-      <Route path="/customer/orders" element={ Pedidos } />
-      <Route path="/customer/products" element={ Produtos } />
-      <Route path="/customer/orders/:id" element={ PedidoDetalhes } />
-      <Route path="/customer/checkout" element={ CheckoutComprador } />
-      <Route path="/seller/orders" element={ Vendas } />
-      <Route path="/seller/orders/:id" element={ VendaDetalhes } />
-      <Route path="/admin/manage" element={ Admin } />
-      <Route element={ NotFound } />
+      <Route path="/register" element={ <Cadastro /> } />
+      <Route path="/customer/orders" element={ <Pedidos /> } />
+      <Route path="/customer/products" element={ <Produtos /> } />
+      <Route path="/customer/orders/:id" element={ <PedidoDetalhes /> } />
+      <Route path="/customer/checkout" element={ <CheckoutComprador /> } />
+      <Route path="/seller/orders" element={ <Vendas /> } />
+      <Route path="/seller/orders/:id" element={ <VendaDetalhes /> } />
+      <Route path="/admin/manage" element={ <Admin /> } />
+      <Route element={ <NotFound /> } />
     </Routes>
   );
 }

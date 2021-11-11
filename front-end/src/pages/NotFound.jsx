60f572db59e287661fd7dd68cd5888a00eafe383
luanlsr <luanfswd@gmail.com>
2021-11-11 19:44:34 -0300

    feat: :sparkles: notfound page criada

--- front-end/src/pages/NotFound.jsx
+++ front-end/src/pages/NotFound.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import notFoundImg from '../assets/images/john_travolta_notFound.gif';
+import '../styles/NotFound.css';
+import Button from '../components/atoms/Button';
 
 export default function NotFound() {
   return (
-    <div className='NotFound'>
+    <div className="notFound">
       <h1>PÁGINA NÃO ENCONTRADA</h1>
       <img src={ notFoundImg } alt="notfound" />
-      <Link to="/products">Voltar</Link>
+      <Link to="/products">
+        <Button className="btn-login bg-danger" text="VOLTAR" />
+      </Link>
     </div>
   );
 }

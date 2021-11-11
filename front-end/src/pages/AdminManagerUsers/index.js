1438108b4ef354c95ea214cc284e988a1bb3d14e
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-11-11 08:49:06 -0300

    feat: adiciona página AdminManagerUsers que será responsável por cadastrar novos clientes, vendedores ou administradores

---
+++ front-end/src/pages/AdminManagerUsers/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import FormRegisterUser from '../../components/FormRegisterUser';
+
+const AdminManagerUsers = () => (
+  <div>
+    <FormRegisterUser />
+  </div>
+);
+
+export default AdminManagerUsers;

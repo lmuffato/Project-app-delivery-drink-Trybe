8215f8b7258722c5f7323a6c21daa9c6bbca07b5
Micaela Alves <micaela.alves93@gmail.com>
2021-11-11 17:06:47 -0300

    feat - card para produtos

--- front-end/src/pages/ProdutosClientes.jsx
+++ front-end/src/pages/ProdutosClientes.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
+import CardProduto from '../components/CardProduto';
 
 function ProdutosClientes() {
   return (
-    <NavBar />
+    <>
+      <NavBar />
+      <CardProduto />
+    </>
   );
 }
 

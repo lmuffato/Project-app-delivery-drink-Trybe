f1b8caa18c8af6071fa4699466324c54420c7ddb
AndreBarroso <andre_barroso17@hotmail.com>
2021-11-11 14:19:40 -0300

    fix: restaura models

---
+++ back-end/src/database/models/sales.js
@@ -0,0 +1,27 @@
+// models/sales.js
+module.exports = (sequelize, DataTypes) => {
+  const sales = sequelize.define('sales', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    user_id: {type: DataTypes.INTEGER, foreignKey: true},
+    // seller_id: DataTypes.INTEGER,
+    total_price: { type: DataTypes.DECIMAL(9,2) },
+    delivery_address: { type: DataTypes.STRING },
+    delivery_number: { type: DataTypes.STRING },
+    status: { type: DataTypes.STRING },
+    
+  },
+  {
+    tableName: 'sales', // Tabela a ser acessada pelo banco de dados;
+    timestamps: true, // Quando este campo está habilitado, os campos createdAt e updatedAt são automaticamente preenchidos com suas respectivias datas.
+    createdAt: 'sale_date',
+    updatedAt: 'updated',
+    tableName: 'sales',
+  });
+
+  sales.associate = (models) => { // Define a associação entre um campo da tabela  BlogPots e a tabela Users.
+    sales.belongsTo(models.user, { // O campo indicado pertence a tabela User, acessada pelo models.User.
+      foreignKey: 'user_id', as: 'User' }); // Esse campo acessa a chave extrangeira userId conffigurada no migrate, e exibe esse campo com o nome "user".
+      //  foreignKey: ['user_id', 'seller_id']);  Usar esse trecho em substituição ao acima, se for usar a seller_id
+  };
+  return sales;
+};

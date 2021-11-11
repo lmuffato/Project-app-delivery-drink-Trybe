c7fe63d53cfaf75b7876ea706ce06c7dfa3b2fe8
Luciano Amâncio <madruga665@hotmail.com>
2021-11-11 11:25:45 -0300

    add rota login

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { Route, Routes } from 'react-router';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import './App.css';
 
 function App() {
   return (
     <Routes>
-      <Route exact path="/" element={ <LoginPage /> } />
+      <Route exact path="/login" element={ <LoginPage /> } />
+      <Route path="/" element={ <Navigate replace to="/login" /> } />
     </Routes>
   );
 }

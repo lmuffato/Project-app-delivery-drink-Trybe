672fb825ba63069041ce13414ab978eb2fb4223a
Joao Nasc <oi@joaonasc.dev>
2021-11-11 16:33:26 -0300

    feat: exibe alerta quando validação falha

--- front-end/src/pages/Auth.jsx
+++ front-end/src/pages/Auth.jsx
@@ -2,18 +2,21 @@ import React, { useEffect, useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import imgManHoldingBeer from '../images/man-holding-beer.png';
 import useInputs from '../hooks/useInputs';
+import useAlert from '../hooks/useAlert';
 import loginSchema from '../schemas/login';
 import styles from '../styles/pages/Auth.module.scss';
 
 export default function Auth() {
   const [values, setInputs] = useInputs({ email: '', password: '' });
   const [schemaStatus, setSchemaStatus] = useState({ valid: false, error: '' });
+  const { Alert, alertMessage, alertType, isVisible, showAlert } = useAlert();
   const history = useHistory();
 
   useEffect(() => {
+    showAlert(false);
     const { error } = loginSchema.validate(values);
     setSchemaStatus({ valid: error === undefined, error: error ? error.message : '' });
-  }, [values]);
+  }, [values, showAlert]);
 
   function logIn(event) {
     event.preventDefault();
@@ -21,12 +24,15 @@ export default function Auth() {
       if (!schemaStatus.valid) throw new Error(schemaStatus.error);
       history.push('/home');
     } catch (error) {
-      console.error(error.message);
+      alertType('danger');
+      alertMessage(error.message);
+      showAlert(true);
     }
   }
 
   return (
     <section className={ styles.auth }>
+      { isVisible && <Alert dataTestId="common_login__element-invalid-email" /> }
       <span className={ styles.manHoldingBeer }>
         <img
           src={ imgManHoldingBeer }
@@ -61,7 +67,6 @@ export default function Auth() {
           className="primary"
         >
           Entrar
-
         </button>
         <Link to="/register" data-testid="common_login__button-register">
           Ainda não tenho conta

fe6acbab94927de8b0ef7698a9e482afecc0576e
Lucas Lara <lucasml@live.com>
2021-11-11 14:48:24 -0300

    styled components checkpoint

---
+++ front-end/src/components/form.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles/form.css';
+
+export default function form() {
+  return (
+    <div classNam="login-box">
+      <form className="login-form">
+        <input />
+        <input />
+        <button type="button">LOGIN</button>
+        <button type="button">CADASTRE-SE</button>
+      </form>
+    </div>
+  );
+}
--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Form from '../components/form';
 
 export default function Login() {
   return (
     <div>
       <h1>Login</h1>
+      <Form />
     </div>
   );
 }

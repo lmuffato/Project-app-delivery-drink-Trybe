423f6430e7467778fbbfe01d16265a44680b0a59
Southier <patrick.southier@hotmail.com>
2021-11-11 15:57:53 -0300

    feat: :sparkles: cria página de cadastro de usuário utilizando o componente RegisterForm e o importa no arquivo de rotas

---
+++ front-end/src/pages/Register.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import RegisterForm from '../components/molecules/RegisterForm';
+import '../styles/login.css';
+
+function Register() {
+  return (
+    <div className="register_container">
+      <RegisterForm />
+    </div>
+  );
+}
+
+export default Register;
--- front-end/src/routes/routes.jsx
+++ front-end/src/routes/routes.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Login from '../pages/Login';
+import Register from '../pages/Register';
 import NotFound from '../pages/NotFound';
 
 export default function Routes() {
@@ -10,6 +11,7 @@ export default function Routes() {
         <Redirect to="/login" />
       </Route>
       <Route exact path="/login" component={ Login } />
+      <Route exact path="/register" component={ Register } />
       <div id="NavBar">
         <Route exact path="/products" component={ Login } />
       </div>

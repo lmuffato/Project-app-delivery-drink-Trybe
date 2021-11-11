a78c5b1875cb73c9eafc7a967da4ea356361e5d4
igson <igson660@gmail.com>
2021-11-10 23:09:27 -0500

    feat: implementaçao das rotas

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,19 +1,17 @@
 import React from 'react';
-// import { Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
-// import Register from './pages/Register';
+import Register from './pages/Register';
 
 function App() {
   return (
-    <>
-      <Login />
-      {/* <Register />
+    <BrowserRouter>
       <Switch>
         <Route path="/" exact component={ Login } />
         <Route path="/login" exact component={ Login } />
         <Route path="/register" exact component={ Register } />
-      </Switch> */}
-    </>
+      </Switch>
+    </BrowserRouter>
   );
 }
 

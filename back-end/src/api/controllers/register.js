419cf79efa5f5947e89b94a7afad3eb66635e50e
Eduardo Costa Maciel <costaeduardomaciel@gmail.com>
2021-11-12 19:06:33 -0300

    Fix: funçao register, adiciona o 'ruscue' a funçao

--- back-end/src/api/controllers/register.js
+++ back-end/src/api/controllers/register.js
@@ -1,12 +1,14 @@
+const rescue = require('express-rescue');
+
 const registerService = require('../services/register');
+const serviceUser = require('../services/register');
 
-const register = async (req, res) => {
+const register = rescue(async (req, res) => {
   const { name, email, password, role } = req.body;
-
   const data = await registerService.register(name, email, password, role);
 
   res.status(201).json(data);
-};
+});
 
 module.exports = {
   register,

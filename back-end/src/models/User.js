0f16ce3a0db1c27d2f2997031d0a53ff5f8a7bd5
Jodiel Briesemeister <jodielll29@gmail.com>
2021-11-12 16:21:24 -0300

    feat: add md5 on login

--- back-end/src/models/User.js
+++ back-end/src/models/User.js
@@ -1,12 +1,11 @@
 const { user } = require('../database/models');
 
-const login = async (email, password) => {
+const login = async ({ email, password }) => {
   const userLogin = await user.findOne({ where: { email, password } });
-  console.log(userLogin);
   return userLogin;
 };
 
-const createUser = async (name, email, password) => {
+const createUser = async ({ name, email, password }) => {
   const newUser = await user.create({ name, email, password });
   return newUser;
 };
--- back-end/src/services/User.js
+++ back-end/src/services/User.js
@@ -4,24 +4,31 @@ const { schemaCreatedUser, schemaLogin } = require('../validations/validations')
 
 const login = async (email, password) => {
   const { error } = schemaLogin.validate({ email, password });
+
   if (error) {
     return { err1: { message: error.message } };
   }
 
-  const user = await User.login(email, password);
+  const passwordEncrypted = md5(password);
+  const user = await User.login({ email, password: passwordEncrypted });
+
   if (user === null) {
     return { err2: { message: 'User not found' } };
   }
+
   return { user };
 };
 
 const createUser = async (name, email, password) => {
   const { error } = schemaCreatedUser.validate({ name, email, password });
+
   if (error) {
     return { err: { message: error.message } };
   }
+
   const passwordEncrypted = md5(password);
   const newUser = await User.createUser({ name, email, password: passwordEncrypted });
+
   return { newUser };
 };
 

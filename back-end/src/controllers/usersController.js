b70cb99ef3a541cd0a27552171bb135440ecc018
Lucas André <lucas.cancado89@gmail.com>
2021-11-12 18:14:43 -0300

    fix eslint

--- back-end/src/controllers/usersController.js
+++ back-end/src/controllers/usersController.js
@@ -5,7 +5,7 @@ const getUserbyEmail = async (req, res) => {
   const { status, message, data } = await User.getUserbyEmail(email);
   if (message) return res.status(status).json({ message });
   return res.status(status).json({ user: data });
-}
+};
 
 module.exports = {
   getUserbyEmail,

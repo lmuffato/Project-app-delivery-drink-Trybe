531167fd94e926963a035baebe44d1aae7ccea82
luanlsr <luanfswd@gmail.com>
2021-11-12 17:57:39 -0300

    merge de branchs

--- front-end/src/components/molecules/ProductCard.jsx
+++ front-end/src/components/molecules/ProductCard.jsx
@@ -15,36 +15,34 @@ export default function ProductCard() {
   };
 
   return (
-    <div className="container">
-      <div
-        className="card-container text-black mb-3"
-      >
-        <div className="card-body product-card-container">
-          <h5 className="card-title">R$12,90</h5>
-          <p className="card-text">Heineken longneck</p>
-          <img
-            src="https://cdn-veloxcode.s3.sa-east-1.amazonaws.com/banco-de-imagens/179J7yvZXHtX28zybgBladGh1hto3xrwRJZAIS9G.png"
-            alt="heineken"
-            style={ { width: 80 } }
-          />
+    <div className="card-container text-black mb-3">
+      <div className="card-body product-card-container">
+        <p className="card-text">Heineken longneck</p>
+        <div className="card-title">
+          <h3>R$12,90</h3>
         </div>
-        <div className="counter-container">
-          <button
-            className="btn-decrement"
-            type="button"
-            onClick={ () => decrement() }
-          >
-            -
-          </button>
-          <input className="card-header counter" value={ count } />
-          <button
-            className="btn-increment"
-            type="button"
-            onClick={ () => increment() }
-          >
-            +
-          </button>
-        </div>
+        <img
+          src="https://cdn-veloxcode.s3.sa-east-1.amazonaws.com/banco-de-imagens/179J7yvZXHtX28zybgBladGh1hto3xrwRJZAIS9G.png"
+          alt="heineken"
+          style={ { width: 80 } }
+        />
+      </div>
+      <div className="counter-container">
+        <button
+          className="btn-decrement"
+          type="button"
+          onClick={ () => decrement() }
+        >
+          -
+        </button>
+        <input className="card-header counter" value={ count } />
+        <button
+          className="btn-increment"
+          type="button"
+          onClick={ () => increment() }
+        >
+          +
+        </button>
       </div>
     </div>
   );
--- front-end/src/pages/Products.jsx
+++ front-end/src/pages/Products.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import '../styles/Products.css';
 import { useLocation } from 'react-router-dom';
+// import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Pagination, Navigation } from 'swiper';
 import NavBar from '../components/NavBar';
 import ProductCard from '../components/molecules/ProductCard';
+import Carousel from '../components/Carousel';
 
 export default function Products() {
   const location = useLocation();
-
+  SwiperCore.use([Pagination, Navigation]);
   const renderNavBar = () => {
     if (location.pathname !== 'login' && location.pathname !== 'register') {
       return (<NavBar />);
@@ -16,8 +19,28 @@ export default function Products() {
   return (
     <>
       { renderNavBar() }
+      <Carousel />
       <div className="products">
         <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+
+        {/* <Swiper
+          slidesPerView={ 3 }
+          spaceBetween={ 30 }
+          slidesPerGroup={ 3 }
+          loop
+          loopFillGroupWithBlank
+          pagination
+          navigation
+          className="mySwiper"
+        >
+          <SwiperSlide><ProductCard /></SwiperSlide>
+          <SwiperSlide><ProductCard /></SwiperSlide>
+          <SwiperSlide><ProductCard /></SwiperSlide>
+        </Swiper> */}
       </div>
     </>
   );

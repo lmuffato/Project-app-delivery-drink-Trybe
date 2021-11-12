2d6aa29a7c75004e39aa0c05e7e2ec7b2ea76349
iago <iago.pferreiravr@gmail.com>
2021-11-12 17:28:18 -0300

    feat(frontend-admin): Cria a página Admin
    Renderiza o componente UserForm na página Admin

---
+++ front-end/src/pages/Admin.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import UserForm from '../components/UserForm';
+
+function Admin() {
+  return (
+    <main>
+      <UserForm />
+    </main>
+  );
+}
+
+export default Admin;

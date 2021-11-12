d0184950a7a8ac5cdbd8edd3157a874e42cf79d6
Patrick Morais <ppternunes@gmail.com>
2021-11-12 11:37:03 -0300

    add header / products - order router

---
+++ front-end/src/pages/ProductsPage/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import MenuCostumer from '../../components/MenuCustomer';
+
+const index = () => {
+  const teste = 'oi';
+  return (
+    <section className="productsPage">
+      <MenuCostumer />
+      {teste}
+    </section>
+  );
+};
+
+export default index;
--- front-end/src/routes/index.js
+++ front-end/src/routes/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Route, Switch, BrowserRouter as Routes, Redirect } from 'react-router-dom';
 import LoginPage from '../pages/Login';
 import RegisterPage from '../pages/Register';
+import ProductsPage from '../pages/ProductsPage';
+import Orders from '../pages/Orders';
 
 function RoutesComponent() {
   return (
@@ -12,6 +14,8 @@ function RoutesComponent() {
         </Route>
         <Route exact path="/login" component={ LoginPage } />
         <Route exact path="/register" component={ RegisterPage } />
+        <Route exact path="/customer/products" component={ ProductsPage } />
+        <Route exact path="/customer/orders" component={ Orders } />
       </Switch>
     </Routes>
   );

d57e02065369e1d616eb1205732fcd7fb76dfc86
Eduardo Costa Maciel <costaeduardomaciel@gmail.com>
2021-11-12 19:24:37 -0300

    Fix: correçao de erros Eslint

--- back-end/src/api/services/register.js
+++ back-end/src/api/services/register.js
@@ -14,7 +14,7 @@ const checkUserIfExist = (user) => {
 
 const register = async (name, email, passWord, role = 'customer') => {
   const password = md5(passWord);
-  const user = await User.findOne({ where: {[Op.or]: [{name}, {email}]} });
+  const user = await User.findOne({ where: { [Op.or]: [{ name }, { email }] } });
   checkUserIfExist(user);
 
   await User.create({ name, email, password, role });

5f5a2eda4cde4db0f26a55ebd6872e9dd27a9cd4
Felipe Muller <felipeavmuller@gmail.com>
2021-11-12 17:15:28 -0300

    Desafio 3 - Validar botao

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,21 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function Login() {
+  const PASSWORD_LENGTH = 6;
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [disabled, setDisabled] = useState(true);
+
+  const handleChange = ({ target }) => {
+    const { value, name } = target;
+    console.log(value, name);
+    if (name === 'email') setEmail(value);
+    if (name === 'password') setPassword(value);
+  };
+
+  useEffect(() => {
+    const validateEmail = /^[\S]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/g.test(email);
+    if (validateEmail && password.length >= PASSWORD_LENGTH) {
+      setDisabled(false);
+    } else { setDisabled(true); }
+  }, [email, password]);
+
   return (
     <div>
       Login
       <input
+        value={ email }
+        name="email"
         type="email"
         data-testid="common_login__input-email"
+        onChange={ (e) => handleChange(e) }
       />
       Senha
       <input
+        value={ password }
+        name="password"
         type="password"
         data-testid="common_login__input-password"
+        onChange={ handleChange }
       />
       <button
         data-testid="common_login__button-login"
         type="button"
+        disabled={ disabled }
       >
         LOGIN
       </button>

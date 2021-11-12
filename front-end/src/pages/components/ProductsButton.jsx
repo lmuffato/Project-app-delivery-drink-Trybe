2a16cd3fe7c80389af9aed69128eb6a5fe509185
Rodrigo Rech <rodrigorech92@gmail.com>
2021-11-12 12:39:23 -0300

    Feat: [front] Nav Button who send customer to /customer/products created

---
+++ front-end/src/pages/components/ProductsButton.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function ProductsButton({ history }) {
+  const handleProductsClick = () => {
+    history.push('/customer/orders');
+  };
+
+  return (
+    <div>
+      <label htmlFor="products-btn">
+        <input
+          id="products-btn"
+          value="PRODUTOS"
+          data-testid="customer_products__element-navbar-link-orders"
+          onClick={ handleProductsClick }
+        />
+      </label>
+    </div>
+  );
+}
+
+ProductsButton.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};

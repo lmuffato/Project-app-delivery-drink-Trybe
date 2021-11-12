5526b6f5510d087e2523e437b77fd1ab350e03e8
Murilo Gonçalves <murilo.gon@live.com>
2021-11-12 14:46:53 -0300

    feat: change colors from hex to hsl

--- front-end/src/styles/themes.js
+++ front-end/src/styles/themes.js
@@ -1,11 +1,16 @@
 const theme = {
   light: {
-    primary: '#036B52',
-    secondary: '#2FC18C',
-    tertiary: '#421981',
-    quaternary: '#056CF9',
-    dark: '#001813',
-    light: '#F2FFFC',
+    primary: 'hsl(166, 95%, 22%)',
+    secondary: 'hsl(158, 61%, 47%)',
+    tertiary: 'hsl(264, 68%, 30%)',
+    quaternary: 'hsl(215, 96%, 50%)',
+    dark: 'hsl(168, 100%, 5%)',
+    'dark-light': 'hsl(168, 10%, 50%)',
+    light: 'hsl(166, 100%, 100%)',
+    'light-dark': 'hsl(166, 100%, 90%)',
+    danger: 'red',
+    warn: 'yello',
+    info: 'blue',
   },
 };
 

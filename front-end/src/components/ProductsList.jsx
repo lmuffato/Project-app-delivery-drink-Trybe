d99527b367c6f8e60c55237027bacb9bd33342b3
iago <iago.pferreiravr@gmail.com>
2021-11-12 15:40:24 -0300

    fix(frontend-login): Corrigindo erros de lint

--- front-end/src/components/ProductsList.jsx
+++ front-end/src/components/ProductsList.jsx
@@ -3,28 +3,32 @@ import axios from 'axios';
 import { ProductsContext } from '../contexts/Products';
 
 const listProducts = (products) => {
-  products.forEach((product) => {
-    <div data-testid={ products.id }>
+  products.map((product, index) => (
+    <div data-testid={ products.id } key={ index }>
       <h1>{ product.name }</h1>
       <img src={ product.url } alt={ product.name } />
       <h3>{ product.price }</h3>
       <button type="button">-</button>
       <p>0</p>
       <button type="button">+</button>
-    </div>;
-  });
+    </div>
+  ));
 };
 
 function ProductList() {
   const { values, setValues } = useContext(ProductsContext);
-  useEffect(async () => {
-    try {
+  useEffect(() => {
+    async function fetchData() {
       const products = await axios.get('/products');
       setValues({ ...values, products });
+    }
+
+    try {
+      fetchData();
     } catch ({ response }) {
       console.log(response.data.data);
     }
-  }, []);
+  }, [setValues, values]);
 
   return (
     <>

a833ad485ad9f8b288762d0d9b55bcca2ca7c539
Felipe Muller <felipeavmuller@gmail.com>
2021-11-12 16:31:37 -0300

    Pagina de login

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -4,6 +4,30 @@ function Login() {
   return (
     <div>
       Login
+      <input
+        type="email"
+        data-testid="common_login__input-email"
+      />
+      Senha
+      <input
+        type="password"
+        data-testid="common_login__input-password"
+      />
+      <button
+        data-testid="common_login__button-login"
+        type="button"
+      >
+        LOGIN
+      </button>
+      <button
+        type="button"
+        data-testid="common_login__button-register"
+      >
+        Ainda não tenho conta
+      </button>
+      <p data-testid="common_login__element-invalid-email">
+        Erro
+      </p>
     </div>
   );
 }

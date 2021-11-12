989de83e7610ba7c650e713487da570b16f9b4a3
Eduardo Costa Maciel <costaeduardomaciel@gmail.com>
2021-11-12 19:10:24 -0300

    Fix: funçao getAllUser, remove a exclusao do password no findAll

--- back-end/src/api/services/user.js
+++ back-end/src/api/services/user.js
@@ -1,7 +1,8 @@
 const { User } = require('../../database/models');
 
 const getAllUser = async () => {
-  const users = await User.findAll({ attributes: { exclude: ['password'] } });
+  // const users = await User.findAll({ attributes: { exclude: ['password'] } });
+  const users = await User.findAll();
   return { status: 200, data: users };
 };
 

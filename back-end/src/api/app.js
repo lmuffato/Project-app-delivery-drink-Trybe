5b0930d5378b67636fdd217594da54537325afa0
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2021-11-12 18:24:32 -0300

    fix: esLint

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -12,8 +12,7 @@ app.use(cors());
 app.use('/', express.static(path.join(__dirname, '..', '..', 'public')));
 app.use(bodyParser.json());
 
-app-use(cors())
-
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.status(200).send('OK FUNCIONANDO');

d46b01f06275b6ecdd20f10b51a64f7669fcbe61
Southier <patrick.southier@hotmail.com>
2021-11-12 16:47:46 -0300

    refactor: :twisted_rightwards_arrows: resolve os conflitos não apontados anteriormente, no arquivo routes.jsx

--- front-end/src/routes/routes.jsx
+++ front-end/src/routes/routes.jsx
@@ -13,16 +13,10 @@ export default function Routes() {
         <Redirect to="/login" />
       </Route>
       <Route exact path="/login" component={ Login } />
-<<<<<<< HEAD
       <Route exact path="/register" component={ Register } />
-      <div id="NavBar">
-        <Route exact path="/products" component={ Login } />
-      </div>
-=======
       <Route exact path="/customer/products" component={ Products } />
       <Route exact path="/products" component={ Products } />
       <Route exact path="/sales" component={ Sales } />
->>>>>>> 2efbe5983fbd464f50f4d1a40af6a6adbe7ea511
       <Route component={ NotFound } />
     </Switch>
   );

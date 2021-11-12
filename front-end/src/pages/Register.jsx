635f3e03866fc54ec67803fec72d18aa03597dc9
Felipe Muller <felipeavmuller@gmail.com>
2021-11-12 19:03:46 -0300

    Requisito 7

--- front-end/src/pages/Register.jsx
+++ front-end/src/pages/Register.jsx
@@ -3,7 +3,32 @@ import React from 'react';
 function Register() {
   return (
     <div>
-      Registro
+      Nome
+      <input
+        type="text"
+        data-testid="common_register__input-name"
+        placeholder="Digite seu nome"
+      />
+      Email
+      <input
+        type="email"
+        data-testid="common_register__input-email"
+        placeholder="Digite seu e-mail"
+      />
+      <input
+        type="password"
+        data-testid="common_register__input-password"
+        placeholder="Digite sua senha"
+      />
+      <button
+        type="button"
+        data-testid="common_register__button-register"
+      >
+        CADASTRAR
+      </button>
+      <p data-testid="common_register__element-invalid_register">
+        Erro
+      </p>
     </div>
   );
 }

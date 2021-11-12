89a53d84b8905840b89b664f33c64075ba8860f8
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2021-11-12 05:47:15 -0300

    feat: requirement 2, validations

---
+++ front-end/src/components/EmailValidations.js
@@ -0,0 +1,6 @@
+// import React, { useState } from 'react';
+
+// function isEmail(e) {
+//   const [email, setEmail] = useState('');
+//   const [isValid, setIsValid] = useState(false)
+// }
--- front-end/src/pages/Login.js
+++ front-end/src/pages/Login.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 // import axios from 'axios';
 
 export default function Login() {
-  // const [email, setEmail] = useState('');
-  // const [password, setPassword] = useState('');
-  // const [loginButton, setLoginButton] = useState(false);
-  // function loginBtn() {}
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loginButton, setLoginButton] = useState(false);
+  const history = useHistory();
+  useEffect(() => {
+    const validateFields = () => {
+      const minDigits = 6;
+      const regex = /^[\w.]+@[a-z]+\.\w{2,3}$/g;
+      const resultButton = password.length > minDigits && email.match(regex);
+      setLoginButton(resultButton);
+    };
+    validateFields();
+  }, [email, password]);
+
+  function loginBtn() {
+    history.push('/register');
+    const isEmail = { email };
+    const formatedEmail = JSON.stringify(isEmail);
+    localStorage.setItem('user', formatedEmail);
+  }
 
   return (
     <main>
@@ -16,7 +33,7 @@ export default function Login() {
           name="Login-email"
           className="email-input"
           placeholder="exemplo@email.com"
-          onChange={ console.log('validation') }
+          onChange={ (e) => setEmail(e.target.value) }
           data-testid="common_login__input-email"
         />
         <input
@@ -24,14 +41,15 @@ export default function Login() {
           name="Login-password"
           className="password-input"
           placeholder="Digite a senha"
-          onChange={ console.log('validation') }
+          onChange={ (e) => setPassword(e.target.value) }
           data-testid="common_login__input-password"
         />
         <button
           type="submit"
           className="button-input"
-          onClick={ console.log('validation') }
-          // disabled={ !loginButton }
+          onClick={ loginBtn }
+          disabled={ !loginButton }
+          data-testid="common_login__button-login"
         >
           Login
         </button>
@@ -44,7 +62,7 @@ export default function Login() {
         Registre-se
       </button>
       <div>
-        <p>Aqui vai um erro</p>
+        <p common_login__element-invalid-email> Algo deu ruim </p>
       </div>
     </main>
   );

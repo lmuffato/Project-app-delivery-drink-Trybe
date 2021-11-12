57f7077596b265f1b497a02d13a3399ab5b35b1a
Felipe Muller <felipeavmuller@gmail.com>
2021-11-12 19:10:15 -0300

    Requisito 8

--- front-end/src/pages/Register.jsx
+++ front-end/src/pages/Register.jsx
@@ -1,21 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Register() {
+  const PASSWORD_LENGTH = 6;
+  const NAME_LENGTH = 12;
+
+  const [userName, setUserName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [disabled, setDisabled] = useState(true);
+
+  const handleChange = ({ target }) => {
+    const { value, name } = target;
+    if (name === 'email') setEmail(value);
+    if (name === 'password') setPassword(value);
+    if (name === 'name') setUserName(value);
+  };
+
+  useEffect(() => {
+    const validateEmail = /^[\S]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/g.test(email);
+    if (validateEmail
+       && password.length >= PASSWORD_LENGTH
+       && userName.length >= NAME_LENGTH) {
+      setDisabled(false);
+    } else { setDisabled(true); }
+  }, [email, password, userName]);
+
   return (
     <div>
       Nome
       <input
+        onChange={ handleChange }
+        name="name"
+        value={ userName }
         type="text"
         data-testid="common_register__input-name"
         placeholder="Digite seu nome"
       />
       Email
       <input
+        onChange={ handleChange }
+        name="email"
+        value={ email }
         type="email"
         data-testid="common_register__input-email"
         placeholder="Digite seu e-mail"
       />
       <input
+        onChange={ handleChange }
+        name="password"
+        value={ password }
         type="password"
         data-testid="common_register__input-password"
         placeholder="Digite sua senha"
@@ -23,6 +56,7 @@ function Register() {
       <button
         type="button"
         data-testid="common_register__button-register"
+        disabled={ disabled }
       >
         CADASTRAR
       </button>

49b300fbcce50845d3649663b8e245a6681ff576
Eduardo Costa Maciel <costaeduardomaciel@gmail.com>
2021-11-12 19:04:58 -0300

    Fix: funçao register, altera condiçao no 'where' do findOne

--- back-end/src/api/services/register.js
+++ back-end/src/api/services/register.js
@@ -1,11 +1,12 @@
 const md5 = require('md5');
+const { Op } = require('sequelize');
 
 const { User } = require('../../database/models');
 
 const checkUserIfExist = (user) => {
   const errMessage = {
     status: 409,
-    message: 'User already exist',
+    message: 'Conflict',
   };
 
   if (user) throw errMessage;
@@ -13,15 +14,14 @@ const checkUserIfExist = (user) => {
 
 const register = async (name, email, passWord, role = 'customer') => {
   const password = md5(passWord);
-  const user = await User.findOne({ where: { name, email } });
-
+  const user = await User.findOne({ where: {[Op.or]: [{name}, {email}]} });
   checkUserIfExist(user);
-  
+
   await User.create({ name, email, password, role });
 
   return {
     status: 201,
-    message: 'User created',
+    message: 'Created',
   };
 };
 

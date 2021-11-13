7279d2110c5c1c73beaac5c79b0fc994fceb3537
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2021-11-12 21:08:03 -0300

    feat: Login logic

--- front-end/src/pages/Login.js
+++ front-end/src/pages/Login.js
@@ -1,20 +1,38 @@
 import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
 import { validateLogin } from '../components/ultility';
+import Error from '../components/Error';
+
+const datatestid = 'common_login__element-invalid-email';
 
 export default function Login() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [emailInput, setEmailInput] = useState('');
+  const [passwordInput, setPasswordInput] = useState('');
   const [isValid, setIsValid] = useState(false);
+  const [error, setError] = useState(false);
+  const history = useHistory();
   useEffect(() => {
-    const sucessValidate = validateLogin(email, password);
+    const sucessValidate = validateLogin(emailInput, passwordInput);
     setIsValid(sucessValidate);
-  }, [email, password]);
+  }, [emailInput, passwordInput]);
 
-  function loginBtn() {
-    const isEmail = { email };
-    const formatedEmail = JSON.stringify(isEmail);
-    localStorage.setItem('user', formatedEmail);
-  }
+  const handleButtonClick = async (e) => {
+    e.preventDefault();
+    await axios.post('http://localhost:3001/login', {
+      email: emailInput,
+      password: passwordInput,
+    })
+      .then(((res) => {
+        const { email, password } = res.data.user;
+        dispatch(saveUser({ email, password }));
+        history.push('/customer/products');
+      }))
+      .catch((err) => {
+        console.log(err);
+        setError(true);
+      });
+  };
 
   return (
     <main>
@@ -25,7 +43,7 @@ export default function Login() {
           name="Login-email"
           className="email-input"
           placeholder="exemplo@email.com"
-          onChange={ (e) => setEmail(e.target.value) }
+          onChange={ (e) => setEmailInput(e.target.value) }
           data-testid="common_login__input-email"
         />
         <input
@@ -33,13 +51,13 @@ export default function Login() {
           name="Login-password"
           className="password-input"
           placeholder="Digite a senha"
-          onChange={ (e) => setPassword(e.target.value) }
+          onChange={ (e) => setPasswordInput(e.target.value) }
           data-testid="common_login__input-password"
         />
         <button
           type="submit"
           className="button-input"
-          onClick={ loginBtn }
+          onClick={ (e) => handleButtonClick(e) }
           disabled={ !isValid }
           data-testid="common_login__button-login"
         >
@@ -50,11 +68,13 @@ export default function Login() {
         type="button"
         className="register-input"
         data-testid="common_login__button-register"
+        onClick={ () => history.push('/register') }
       >
         Registre-se
       </button>
       <div>
-        <p data-testid="common_login__element-invalid-email">Usuário ou Senha inválido</p>
+        { error && <Error testeid={ datatestid }>Usuário ou Senha inválido</Error>}
+
       </div>
     </main>
   );

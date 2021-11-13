1ab2d9e3b316e1cabfcca3302833e7457ef826ef
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-11-13 11:23:44 -0300

    substituido bordy-parser.json() por express.json()

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,15 +1,17 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
 const routes = require('../routes');
+const errorMiddleware = require('../middlewares/errorMiddleware');
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use('/user', routes.useRoutes);
 app.get('/coffee', (_req, res) => res.status(418).end());
+
+app.use(errorMiddleware);
 
 module.exports = app;

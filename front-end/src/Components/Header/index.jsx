ce06e90db8e0d6d305394eea0d94ca7bb99764de
lucas-paz <lucas.hpaz@outlook.com>
2021-11-13 17:19:55 -0300

    feat: creates component header

--- front-end/src/Components/Header/index.jsx
+++ front-end/src/Components/Header/index.jsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
 
-function Header(props) {
+function Header({ links }) {
+  const { user } = useContext(UserContext);
+
+  const onExit = () => { };
+
   return (
-  <div>
-    
-  </div>
-);
+    <header>
+      {links.map((link, i) => (
+        <Link to={ link.url } key={ `header${i}` }>
+          {link.name}
+        </Link>
+      ))}
+      {user.name}
+      <Link to="/login">
+        <button type="button" onClick={ () => onExit() }>
+          Sair
+        </button>
+      </Link>
+    </header>
+  );
 }
+
+Header.propTypes = {
+  links: PropTypes.arrayOf(PropTypes.object),
+}.required;
 
 export default Header;

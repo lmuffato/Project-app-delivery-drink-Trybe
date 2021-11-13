e5f96123b5ff5e151798f34abf21173a416f03e9
lucas-paz <lucas.hpaz@outlook.com>
2021-11-13 16:45:09 -0300

    rename directories to more intuitive names

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,18 +1,13 @@
 import React from 'react';
+import Router from './Routes.js';
 import './App.css';
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
-import Login from './pages/Login';
+
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/">
-          <Redirect to="/login" />
-        </Route>
-        <Route path="/login" component={ Login } />
-      </Switch>
-    </BrowserRouter>
+    <div>
+      <Routes />
+    </div>
   );
 }
 

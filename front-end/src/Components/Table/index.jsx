d2ab01ff94c37c7cd63c9fcd370c310c4876ed39
lucas-paz <lucas.hpaz@outlook.com>
2021-11-13 18:00:03 -0300

    feat: creates table

--- front-end/src/Components/Table/index.jsx
+++ front-end/src/Components/Table/index.jsx
@@ -1,11 +1,49 @@
-// import React from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-// function Table(props) {
-//   return (
-//   <div>
+function Table({ headers, payload, hasButton, onClick }) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Item</th>
+          {headers.map((header, i) => (
+            <th key={ `head${i}` }>{header}</th>
+          ))}
+          { hasButton && <th>Excluir</th> }
+        </tr>
+      </thead>
+      <tbody>
+        { payload.map((item, i) => (
+          <tr key={ `row${i + 1}` }>
+            <td>{ i + 1 }</td>
+            { Object.values(item).map((value) => (
+              <td key={ `${value}` }>{ value }</td>
+            ))}
+            { hasButton && (
+              <td>
+                <button type="button" onClick={ () => onClick(item.name) }>
+                  Remover
+                </button>
+              </td>
+            )}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
 
-//   </div>
-// );
-// }
+Table.defaultProps = {
+  hasButton: false,
+  onClick: () => null,
+};
 
-// export default Table;
+Table.propTypes = {
+  headers: PropTypes.arrayOf(PropTypes.string).isRequired,
+  payload: PropTypes.arrayOf(PropTypes.object).isRequired,
+  hasButton: PropTypes.bool,
+  onClick: PropTypes.func,
+};
+
+export default Table;

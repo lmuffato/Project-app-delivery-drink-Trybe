f9f9ea36ba98f83607dffb5e6dce0e4c04fb5a81
Rodrigo Rech <rodrigorech92@gmail.com>
2021-11-13 19:00:21 -0300

    Feat: customerProducts page with headers, ProductsCards and ShopCartCard

---
+++ front-end/src/pages/CustomerProducts.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import Headers from './components/Headers';
+import ProductsCards from './components/ProductsCards';
+import ShopCartCard from './components/ShopCartCard';
+
+function CustomerProducts({ history }) {
+  const [value, setValue] = useState([]);
+
+  const handleCallback = (p) => {
+    setValue(p
+      .reduce((acc, product) => acc + Number(product.price) * product.quantity, 0));
+  };
+
+  return (
+    <div>
+      <Headers history={ history } />
+      <ProductsCards callback={ handleCallback } />
+      <ShopCartCard value={ value } />
+    </div>
+  );
+}
+
+export default CustomerProducts;
+
+CustomerProducts.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};

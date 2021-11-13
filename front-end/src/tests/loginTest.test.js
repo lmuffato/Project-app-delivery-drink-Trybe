56362bc10262da709c3719234cc6c7523292098f
Orlando Flores <orlando_flores@id.uff.br>
2021-11-13 15:07:14 -0300

    test: (login) creates test to check all ids

--- front-end/src/tests/loginTest.test.js
+++ front-end/src/tests/loginTest.test.js
@@ -6,9 +6,35 @@ import App from '../App';
 const email = 'test@email.com';
 const password = 'test-login';
 const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3RAZW1haWwuY29tIiwicm9sZSI6ImFkbWluaXN0cmFkb3IiLCJuYW1lIjoidGVzdCIsImlkIjo0LCJpYXQiOjE2MzY4MjUzNjksImV4cCI6MTYzNzY4OTM2OX0.h7BE_gB0b1GF1SpEUKUWe0qgDojDVbFK0RZD1OYTxTI';
+const EMAIL_TEST_ID = 'common_login__input-email';
+const BUTTON_REGISTER_TEST_ID = 'common_login__button-register';
+const PASSWORD_TEST_ID = 'common_login__input-password';
+const BUTTON_LOGIN_TEST_ID = 'common_login__button-login';
+const ELEMENT_INVALID_EMAIL_TEST_ID = 'common_login__element-invalid-email';
 
 const mockPostAxios = () => {
   jest
   .spyOn('axios', post)
   .mockImplementation(() => Promise.resolve({ status: 200, statusText: 'Ok', data: token }))
 }
+
+describe('Teste da página de login', () => {
+  beforeEach(cleanup);
+
+  it('Renderiza os itens corretos', () => {
+    render(<App />);
+
+    const emailInput = screen.getByTestId(EMAIL_TEST_ID);
+    const passwordInput = screen. getByTestId(PASSWORD_TEST_ID);
+    const loginButton = screen.getByTestId(BUTTON_LOGIN_TEST_ID);
+    const registerButton = screen.getByTestId(BUTTON_REGISTER_TEST_ID);
+
+    expect(emailInput).toBeInTheDocument();
+    expect(passwordInput).toBeInTheDocument();
+    expect(loginButton).toBeInTheDocument();
+    expect(loginButton).toBeDisabled();
+    expect(registerButton).toBeInTheDocument();
+  });
+
+  // it('Botão de login desabilitado')
+})

dfe4c082851d3ec9765e9fc47c06d73a32ee473a
Hudson Carlos <hc88806@gmail.com>
2021-11-14 11:32:42 -0400

    register on test start

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,18 +1,12 @@
 import React from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
-import Register from './pages/register';
 
 function App() {
   return (
-    <Routes>
-      <Route exact path="/">
-        {/* Source:  https://gist.github.com/mjackson/b5748add2795ce7448a366ae8f8ae3bb */}
-        <Route path="/" element={ <Navigate replace to="/login" /> } />
-      </Route>
-      <Route path="/login" element={ <Login /> } />
-      <Route path="/register" element={ <Register /> } />
-    </Routes>
+    <Switch>
+      <Route path="/" component={ Login } />
+    </Switch>
   );
 }
 
---
+++ front-end/src/pages/Login.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function login() {
+  return (
+    <h1>oi</h1>
+  );
+}
+
+export default login;

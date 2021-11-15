3d0c486319c9dd3713914698577a4a6da209f88f
Paulo Vitor Borges <pvtrybe209@gmail.com>
2021-11-15 12:48:16 -0300

    fix packege lock

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 
 const userRoute = require('./routes/users');
 const productsRoute = require('./routes/products');
@@ -13,7 +15,7 @@ const error = require('./middleware/error');
 app.use('/users', userRoute);
 app.use('/products', productsRoute);
 app.use('/sales', salesRoute);
-
+app.get('/carlos', (_req,res) => res.status(200).json({message:'ok'}));
 app.get('/coffee', (_req, res) => res.status(418).end());
 
 app.use(error)

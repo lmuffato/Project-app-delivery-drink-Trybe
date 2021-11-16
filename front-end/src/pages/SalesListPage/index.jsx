c9310ae59bce49b173e4c4ab9786046dc6c48597
Gui-lira <gui.lira.martins@gmail.com>
2021-11-16 18:40:51 -0300

    pagina quase pronta

---
+++ front-end/src/pages/SalesListPage/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import SalesList from '../../components/SalesList';
+
+export default function SalesListPage() {
+  return (
+    <div>
+      <SalesList />
+    </div>
+  );
+}
--- front-end/src/routes/AllRoute.jsx
+++ front-end/src/routes/AllRoute.jsx
@@ -8,7 +8,7 @@ import CheckoutComprador from '../components/CheckoutComprador';
 import VendaDetalhes from '../components/VendaDetalhes';
 import AdminManagerUsers from '../pages/AdminManagerUsers';
 import NotFound from '../components/NotFound';
-import SalesList from '../components/SalesList';
+import SalesListPage from '../pages/SalesListPage';
 
 export default function AllRoute() {
   return (
@@ -16,11 +16,11 @@ export default function AllRoute() {
       <Route path="/login" element={ <Login /> } />
       <Route path="/" element={ <Navigate replace to="/login" /> } />
       <Route path="/register" element={ <Cadastro /> } />
-      <Route path="/customer/orders" element={ <SalesList /> } />
+      <Route path="/customer/orders" element={ <SalesListPage /> } />
       <Route path="/customer/products" element={ <Produtos /> } />
       <Route path="/customer/orders/:id" element={ <PedidoDetalhes /> } />
       <Route path="/customer/checkout" element={ <CheckoutComprador /> } />
-      <Route path="/seller/orders" element={ <SalesList /> } />
+      <Route path="/seller/orders" element={ <SalesListPage /> } />
       <Route path="/seller/orders/:id" element={ <VendaDetalhes /> } />
       <Route path="/admin/manage" element={ <AdminManagerUsers /> } />
       <Route element={ <NotFound /> } />

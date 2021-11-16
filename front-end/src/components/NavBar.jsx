e4ceca841dd9c1e959f0a833acb8f498c9b019d4
iago <iago.pferreiravr@gmail.com>
2021-11-16 16:03:42 -0300

    refactor(frontend-admin): Muda data-testids para ficarem dinâmicos

--- front-end/src/components/NavBar.jsx
+++ front-end/src/components/NavBar.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-function NavBar() {
+function NavBar({ ids: { productId, pageId, userId }, names: { pageName, userName } }) {
   return (
     <ul>
-      <li><NavLink to="/products">Produtos</NavLink></li>
-      <li><NavLink to="/orders">Meus Pedidos</NavLink></li>
-      <li>Username</li>
-      <li><NavLink to="/logout">Sair</NavLink></li>
+      { productId
+        && <li data-testid={ productId }><NavLink to="/products">Produtos</NavLink></li> }
+      <li data-testid={ pageId }><NavLink to="/orders">{ pageName }</NavLink></li>
+      <li data-testid={ userId }>{ userName }</li>
+      <li
+        data-testid="customer_products__element-navbar-link-logout"
+      >
+        <NavLink to="/logout">Sair</NavLink>
+      </li>
     </ul>
   );
 }
 
 export default NavBar;
+
+NavBar.propTypes = {
+  ids: PropTypes.object,
+  names: PropTypes.object,
+}.isRequired;

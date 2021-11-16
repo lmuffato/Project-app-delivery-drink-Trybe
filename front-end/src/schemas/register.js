39a006aed752f0a1b8e9fbd68eb9bb4f42d9a02c
jonathan <jonathanfernandesjf98@gmail.com>
2021-11-16 12:59:09 -0300

    feat: utilizacao do joi para validaçoes de registro de usuario

--- front-end/src/schemas/register.js
+++ front-end/src/schemas/register.js
@@ -3,18 +3,15 @@ import Joi from 'joi';
 const nameMinLength = 12;
 const passwordMinLength = 6;
 
-
 export default Joi.object({
   email: Joi.string()
     .regex(/^\w+([.-_]?\w+)*@\w+([.-_]?\w+)*(\.\w{2,3})+$/)
     .message('Formato de e-mail inválido')
     .required(),
-
   password: Joi.string()
     .min(passwordMinLength)
     .message('A senha precisa ter pelo menos 6 caracteres')
     .required(),
-  
   name: Joi.string()
     .min(nameMinLength)
     .message('O nome de usuário deve conter pelo menos 12 caracteres')

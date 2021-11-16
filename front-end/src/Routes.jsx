a2c5a09e2edec1f8a6bc551e39fc7d715d924437
jonathan <jonathanfernandesjf98@gmail.com>
2021-11-16 20:05:47 -0300

    feat: adicao da rota que direciona para o componente registerPage

--- front-end/src/Routes.jsx
+++ front-end/src/Routes.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
-import RegisterForm from './components/registerForm';
+import RegisterPage from './pages/RegisterPage';
 import Auth from './pages/Auth';
 
 export default function Routes() {
@@ -8,7 +8,7 @@ export default function Routes() {
     <Switch>
       <Route path="/customer/products" render={ () => <h1>Produtos</h1> } />
       <Route path="/login" component={ Auth } />
-      <Route path="/register" component={ RegisterForm } />
+      <Route path="/register" component={ RegisterPage } />
       <Route path="/" render={ () => <Redirect to="/login" /> } />
     </Switch>
   );

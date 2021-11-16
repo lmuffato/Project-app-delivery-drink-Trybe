235d738d9aaecaf870af9158e43cd58ff5c0d254
rogerio-lambert <rogerio.cl@gmail.com>
2021-11-16 20:32:13 -0300

    feat (test): Create test base file for create sale

---
+++ back-end/src/tests/createSale.test.js
@@ -0,0 +1,25 @@
+// const test = {
+//   totalPrice: 320.21,
+//   deliveryAddress: 'rua dos bobos',
+//   deliveryNumber: 69,
+//   userId: 1,
+//   sellerId: 3,
+//   products: [
+//     {
+//       productId: 1,
+//       quantity: 2,
+//     },
+//     {
+//       productId: 3,
+//       quantity: 4,
+//     },
+//     {
+//       productId: 5,
+//       quantity: 1,
+//     },
+//   ],
+// };
+// const testCreate = async () => {
+//   console.log(await createSale(test));
+// };
+// testCreate();

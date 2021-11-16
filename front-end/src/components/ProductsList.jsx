a42d9d3c2dbbcaa1675132f02109da1edb0918d4
Marcus Hüsemann <mchusemann@gmail.com>
2021-11-16 20:54:55 -0300

    fix(ProductsList): arruma useEffect e configuraçao do axios

--- front-end/src/components/ProductsList.jsx
+++ front-end/src/components/ProductsList.jsx
@@ -2,37 +2,40 @@ import React, { useContext, useEffect } from 'react';
 import axios from 'axios';
 import { ProductsContext } from '../contexts/Products';
 
-const listProducts = (products) => {
-  products.map((product, index) => (
-    <div data-testid={ products.id } key={ index }>
-      <h1>{ product.name }</h1>
-      <img src={ product.url } alt={ product.name } />
-      <h3>{ product.price }</h3>
-      <button type="button">-</button>
-      <p>0</p>
-      <button type="button">+</button>
-    </div>
-  ));
-};
+const listProducts = (products) => products.map((product, index) => (
+  <div data-testid={ products.id } key={ index }>
+    <h1>{ product.name }</h1>
+    <img src={ product.urlImage } alt={ product.name } />
+    <h3>{ product.price }</h3>
+    <button type="button">-</button>
+    <p>0</p>
+    <button type="button">+</button>
+  </div>
+));
 
 function ProductList() {
   const { values, setValues } = useContext(ProductsContext);
   useEffect(() => {
     async function fetchData() {
-      const products = await axios.get('/products');
-      setValues({ ...values, products });
+      const token = JSON.parse(localStorage.getItem('token'));
+      try {
+        const { data: { data } } = await axios.get(
+          'http://localhost:3001/products',
+          { headers: {
+            Authorization: `Bearer ${token}` } },
+        );
+        setValues({ ...values, data });
+        console.log(data);
+      } catch (err) {
+        console.log(err);
+      }
     }
-
-    try {
-      fetchData();
-    } catch ({ response }) {
-      console.log(response.data.data);
-    }
-  }, [setValues, values]);
+    fetchData();
+  }, []);
 
   return (
     <>
-      { values.products ? listProducts(values.products) : <h1>Loading...</h1> }
+      { values.data ? listProducts(values.data) : <h1>Loading...</h1> }
       <p>teste</p>
     </>
   );

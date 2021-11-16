24946ee357a8f4d003330953418d1463004ae804
rafael <rafittu@outlook.com>
2021-11-16 17:06:01 -0300

    build: add path npm dependency

--- back-end/src/services/userService.js
+++ back-end/src/services/userService.js
@@ -1,10 +1,11 @@
 const md5 = require('md5');
 const jwt = require('jsonwebtoken');
+const fs = require('fs');
 const { User } = require('../database/models');
 
 const create = async (name, email, password, role) => {
   const MD5password = md5(password);
-  const SECRET = MD5password;
+  // const SECRET = MD5password;
 
   const user = await User.create(
     { name, email, password: MD5password, role },

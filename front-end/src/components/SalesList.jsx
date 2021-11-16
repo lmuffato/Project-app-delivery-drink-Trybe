b193476bb1b582eed6c8255ffab27e3685ea2956
Gui-lira <gui.lira.martins@gmail.com>
2021-11-16 17:22:30 -0300

    correção lint front

--- front-end/src/components/SalesList.jsx
+++ front-end/src/components/SalesList.jsx
@@ -1,22 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import CardSale from './cardSale';
 
-
 export default function SalesList() {
   const [sales, setSales] = useState([]);
   useEffect(() => {
     const getSales = async () => {
       const token = localStorage.getItem('token');
-      const path = location.href;
-      const url = path.includes('seller') 
-      ? 'http://localhost:3100/sales/seller' : 'http://localhost:3100/sales/user';
+      const path = window.location.href;
+      const url = path.includes('seller')
+        ? 'http://localhost:3100/sales/seller' : 'http://localhost:3100/sales/user';
       const response = await fetch(url, { headers: { authentication: token } });
       const json = await response.json();
       setSales([...json]);
     };
     getSales();
   });
-  const renderSales = () => sales.map((sale) => <CardSale sale={ sale } />)
+  const renderSales = () => sales.map((sale) => (
+    <CardSale key={ sale.id } sale={ sale } />));
 
   return (
     <div>

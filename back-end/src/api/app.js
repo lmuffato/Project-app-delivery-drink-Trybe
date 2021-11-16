bba654608c98ba3e6b651bda270aed3dd93a6095
Bruno Mendes <mendessbruno@gmail.com>
2021-11-16 16:07:08 -0300

    feat: criado schema para validaçao do usuario e alterado nome do controller de login para userController

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 
 const loginController = require('./controllers/loginController');
 const loginvalidationMid = require('./middlewares/loginValidationMid');
+const registerValidationMid = require('./middlewares/registerValidationMid');
 
 const app = express();
 app.use(express.json());
@@ -13,5 +14,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.get('/coffee', (_req, res) => res.status(418).end());
 
 app.post('/login', loginvalidationMid, loginController.login);
+app.post('/register', registerValidationMid, loginController.login);
 
 module.exports = app;

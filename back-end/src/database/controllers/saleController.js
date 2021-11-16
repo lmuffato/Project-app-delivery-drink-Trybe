798d09d4e5fdd0ff8e12e5ceb194111171f9eb73
Marcelo Maurício <mmauricio.design@gmail.com>
2021-11-16 19:47:10 -0300

    Created Sale Service and Controller

--- back-end/src/database/controllers/saleController.js
+++ back-end/src/database/controllers/saleController.js
@@ -5,7 +5,7 @@ async function create(req, res) {
   try {
     const { code } = await saleService.create(req.body);
 
-    return res.status(code);
+    return res.status(code).json({ message: 'Works' });
   } catch(e) {
     return res.status(HTTP_NOT_FOUND).json({ error: e.message });
   }
--- back-end/src/database/services/saleService.js
+++ back-end/src/database/services/saleService.js
@@ -1,17 +1,21 @@
-const { Sale, SaleProduct } = require('../models');
+const { Sale, SalesProducts } = require('../models');
 const { HTTP_CREATED, HTTP_CONFLICT, HTTP_OK_STATUS } = require('../../status');
 
 
 async function create(body) {
   const statusOrder = 'pendente';
+  
+  const { userId, sellerId, products, totalPrice, deliveryAddress, deliveryNumber } = body;
 
-  const { userId, sellerId, products, totalPrice, deliveryAddres, deliveryNumber } = body;
+  const saleId = await Sale.create({ user_id: userId, seller_id: sellerId, total_price: totalPrice, delivery_address: deliveryAddress, delivery_number: deliveryNumber, sale_date: new Date(), status: statusOrder });
 
-  const newSale = await Sale.create({ user_id: userId, seller_id: sellerId, total_price: totalPrice, delivery_addres: deliveryAddres, delivery_number: deliveryNumber, sale_date: new Date(), status: statusOrder });
+  console.log('Chegou aqui! service', products);
 
-  products.forEach((product) => {
+  products.forEach(async (product) => {
     const { id, quantity } = product;
-    await SaleProduct.create({ sale_id: newSale.id , product_id: id, quantity: quantity });
+    console.log(id, quantity, 'Dentro do each');
+
+    await SalesProducts.create({ sale_id: saleId.dataValues.id , product_id: id, quantity: quantity })
   });
 
   return { code: HTTP_CREATED };

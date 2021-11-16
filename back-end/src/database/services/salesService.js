57d10362a98a2b167f133c4faf9b896cfcf42791
Marcelo Maurício <mmauricio.design@gmail.com>
2021-11-16 15:57:25 -0300

    Refatoring create function for Sale

--- back-end/src/database/services/salesService.js
+++ back-end/src/database/services/salesService.js
@@ -3,12 +3,16 @@ const { HTTP_CREATED, HTTP_CONFLICT, HTTP_OK_STATUS } = require('../../status');
 
 
 async function create(body) {
-  const isUserResgistered = await readByEmailService(email);
   const statusOrder = 'pendente';
 
-  const { userId, sellerId, totalPrice, deliveryAddres, deliveryNumber, saleDate } = body;
+  const { userId, sellerId, products, totalPrice, deliveryAddres, deliveryNumber } = body;
 
-  await Sale.create({ user_id: userId, seller_id: sellerId, total_price: totalPrice, delivery_addres: deliveryAddres, delivery_number: deliveryNumber, sale_date: new Date(), status: statusOrder });
+  const newSale = await Sale.create({ user_id: userId, seller_id: sellerId, total_price: totalPrice, delivery_addres: deliveryAddres, delivery_number: deliveryNumber, sale_date: new Date(), status: statusOrder });
+
+  products.forEach((product) => {
+    const { id, quantity } = product;
+    await SaleProduct.create({ sale_id: newSale.id , product_id: id, quantity: quantity });
+  });
 
   return { data, code: HTTP_CREATED };
 };

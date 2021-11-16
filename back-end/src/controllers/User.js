d46552f5ee92f34cf72a2e40ba954362aba006e3
rafael <rafittu@outlook.com>
2021-11-16 16:35:03 -0300

    feat: add jwt authorization

--- back-end/src/controllers/User.js
+++ back-end/src/controllers/User.js
@@ -1,4 +1,5 @@
 const md5 = require('md5');
+const jwt = require('jsonwebtoken');
 const { User } = require('../database/models');
 
 const CUSTOMER_ROLE = 'customer';
@@ -8,13 +9,18 @@ const createUser = async (req, res) => {
     const userData = req.body;
     const { password, name, email, role } = userData;
     const MD5password = md5(password);
+    const SECRET = MD5password;
 
     const user = await User.create(
       { name, email, password: MD5password, role },
     );
     if (!user) throw new Error('Algo deu errado');
 
-    return res.status(201).json(user);
+    const { passowrd: _, ...userPayload } = user;
+
+    const token = jwt.sign(userPayload, SECRET);
+
+    return res.status(201).json(token);
   } catch (e) {
     console.log(e.message);
     res.status(500).json({ message: e.message });

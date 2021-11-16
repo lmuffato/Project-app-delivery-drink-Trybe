8e47ccd69d77284681db5f99f25f6dfcd80cdefe
iago <iago.pferreiravr@gmail.com>
2021-11-16 16:05:01 -0300

    feat(frontend-admin): Implementa a lógica de habilitar ou desabilitar o botão de registro da pagina do admin
    Faz também as verificações dos preenchimentos dos campos do formulário de cadastro de novos usuários

--- front-end/src/components/UserForm.jsx
+++ front-end/src/components/UserForm.jsx
@@ -1,6 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function UserForm() {
+  const [userName, setUserName] = useState('');
+  const [userEmail, setUserEmail] = useState('');
+  const [userPassword, setUserPassword] = useState('');
+  const [userRole, setUserRole] = useState('Vendedor');
+  const [disabled, setDisabled] = useState(true);
+
+  useEffect(() => {
+    const verifyInfo = () => {
+      const regex = /\S+@\S+\.\S+/;
+      const minimumNameLength = 12;
+      const minimumPasswordLength = 6;
+      if (userName.length >= minimumNameLength
+        && regex.test(userEmail)
+        && userPassword.length >= minimumPasswordLength
+        && userRole) {
+        setDisabled(false);
+      } else {
+        setDisabled(true);
+      }
+    };
+
+    verifyInfo();
+  }, [userEmail, userName.length, userPassword.length, userRole]);
+
   return (
     <form>
       <fieldset>
@@ -10,6 +34,7 @@ function UserForm() {
             type="text"
             id="new-user-name"
             data-testid="admin_manage__input-name"
+            onChange={ (e) => setUserName(e.target.value) }
           />
         </label>
         <label htmlFor="new-user-email">
@@ -18,14 +43,16 @@ function UserForm() {
             type="text"
             id="new-user-email"
             data-testid="admin_manage__input-email"
+            onChange={ (e) => setUserEmail(e.target.value) }
           />
         </label>
         <label htmlFor="new-user-password">
           Senha
           <input
-            type="text"
+            type="password"
             id="new-user-password"
-            data-testid=" admin_manage__input-password"
+            data-testid="admin_manage__input-password"
+            onChange={ (e) => setUserPassword(e.target.value) }
           />
         </label>
         <label htmlFor="new-user-type">
@@ -34,15 +61,19 @@ function UserForm() {
             name="new-user-type"
             id="new-user-type"
             data-testid="admin_manage__select-role"
+            defaultValue="Vendedor"
+            onChange={ (e) => setUserRole(e.target.value) }
           >
-            <option value="">Vendedor</option>
-            <option value="">Administrador</option>
-            <option value="">Cliente</option>
+            <option value="Vendedor">Vendedor</option>
+            <option value="Administrador">Administrador</option>
+            <option value="Cliente">Cliente</option>
           </select>
         </label>
         <button
           type="button"
           data-testid="admin_manage__button-register"
+          disabled={ disabled }
+          onClick={ () => console.log('Teste') }
         >
           Cadastrar
         </button>

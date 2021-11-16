0e026e93c75bae2d4c6b6d565407719812d151a4
mallmann02 <leomallmann020302@gmail.com>
2021-11-16 17:12:43 -0300

    feat(customer_flow): render products from database

--- front-end/src/screens/CustomerProducts.jsx
+++ front-end/src/screens/CustomerProducts.jsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import NavBar from '../components/NavBar';
+import ProductCard from '../components/ProductCard';
+import ContextProducts from '../context/ContextProducts';
 
 function CustomerProducts() {
+  const { findProducts, allProducts: products } = useContext(ContextProducts);
+  const [loading, setLoading] = useState(false);
+  // const [products, setProducts] = useState([{}]);
+
+  useEffect(async () => {
+    setLoading(true);
+    async function getProducts() {
+      await findProducts();
+    }
+    getProducts();
+    setLoading(false);
+  }, []);
+
+  const renderProductCard = () => products.map((product, index) => (
+    <ProductCard key={ index } { ...product } />
+  ));
+
   return (
     <div>
       <NavBar />
+      { loading
+        ? <p>Loading</p>
+        : renderProductCard() }
     </div>
   );
 }

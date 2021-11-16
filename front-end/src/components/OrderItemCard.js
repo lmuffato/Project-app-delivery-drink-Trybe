50006db88db806bdac54f73726d33b2f7a4373be
Gustavo Sousa <gustavohl98@gmail.com>
2021-11-16 16:56:28 -0300

    Initial implementation of Checkout Page

---
+++ front-end/src/components/OrderItemCard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function OrderItemCard() {
+  return (
+    <tr>
+      <td>1</td>
+      <td>Cerveja Stella 250ml</td>
+      <td>3</td>
+      <td>R$3,50</td>
+      <td>R$10,50</td>
+      <td>Remover</td>
+    </tr>
+  );
+}
+
+export default OrderItemCard;
--- front-end/src/pages/CustomerCheckout.js
+++ front-end/src/pages/CustomerCheckout.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import NavBar from '../components/NavBar';
+import OrderItemCard from '../components/OrderItemCard';
 
 function CustomerCheckout() {
+  const { cart } = useSelector((state) => state.product);
+
   return (
-    <div>
-      Customer checkout page
-    </div>
+    <>
+      <NavBar />
+      Finalizar Pedido
+
+      <table style={ { border: '1px solid black' } }>
+        <thead>
+          <tr>
+            <th scope="col">Item</th>
+            <th scope="col">Descrição</th>
+            <th scope="col">Quantidade</th>
+            <th scope="col">Valor Unitário</th>
+            <th scope="col">Sub-total</th>
+            <th scope="col">Remover Item</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            cart
+              .map((product) => <OrderItemCard product={ product } key={ product.id } />)
+          }
+        </tbody>
+      </table>
+    </>
   );
 }
 

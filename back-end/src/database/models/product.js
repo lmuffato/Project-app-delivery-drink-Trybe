a9b691d2731c391091744972a5d4dd0d8cb0ed7e
Vinicius Rodrigues <v.rodriguesjk@gmail.com>
2021-11-16 16:44:55 -0300

    Fix: replacing key name from urlImage to url_image

--- back-end/src/database/models/product.js
+++ back-end/src/database/models/product.js
@@ -5,7 +5,7 @@ module.exports = (sequelize, DataTypes) => {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     name: DataTypes.STRING,
     price: DataTypes.DECIMAL(10, 2),
-    urlImage: DataTypes.STRING,
+    url_image: DataTypes.STRING,
   },
     {
       timestamps: false,

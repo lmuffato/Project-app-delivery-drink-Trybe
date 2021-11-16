aab23ac278263fbcebf7922c39ae2a425fdce792
rodolfo_oliveira <rod.vgo@gmail.com>
2021-11-16 18:07:32 -0300

    resolvendo conflitos rota sales com main

--- back-end/src/api/services/saleService.js
+++ back-end/src/api/services/saleService.js
@@ -3,7 +3,6 @@ const { Sales, SalesProducts } = require('../../database/models');
 const registerSale = async (saleData) => {
   const { products, ...data } = saleData;
 
-  console.log(products);
   const { dataValues } = await Sales.create(data);
 
   products.map(({ productId, quantity }) => SalesProducts.create({

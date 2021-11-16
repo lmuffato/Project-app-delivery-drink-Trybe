d12fcd40608ab3eee489372ad9058982de181b11
rafael <rafittu@outlook.com>
2021-11-16 16:14:29 -0300

    feat: apply MD5 to store password

--- back-end/src/controllers/User.js
+++ back-end/src/controllers/User.js
@@ -1,16 +1,26 @@
+const md5 = require('md5');
 const { User } = require('../database/models');
+
+const CUSTOMER_ROLE = 'customer';
 
 const createUser = async (req, res) => {
   try {
     const userData = req.body;
+    const { password, name, email, role } = userData;
 
-    const user = await User.create(userData);
+    const MD5password = md5(password);
+
+    const user = await User.create(
+      { name, email, password: MD5password, role },
+    );
+
+    console.log(user);
 
     if (!user) {
       throw new Error('Algo deu errado');
     }
 
-    return res.status(200).json(user);
+    return res.status(201).json(user);
   } catch (e) {
     console.log(e.message);
     res.status(500).json({ message: e.message });

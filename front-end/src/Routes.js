c280f45e5a66564f60e0f26e5e92f49292736914
lucas-paz <lucas.hpaz@outlook.com>
2021-11-17 08:37:17 -0300

    feat: adapt the router

--- front-end/src/Routes.js
+++ front-end/src/Routes.js
@@ -4,6 +4,8 @@ import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 // Components
 import Login from './pages/Login';
 import Checkout from './pages/Checkout';
+import SignUp from './pages/SignUp';
+import Products from './pages/Products';
 
 export default function Router() {
   return (
@@ -12,10 +14,10 @@ export default function Router() {
         <Route exact path="/">
           <Redirect to="/login" />
         </Route>
-
         <Route path="/login" component={ Login } />
-
         <Route path="/checkout" component={ Checkout } />
+        <Route path="/register" component={ SignUp } />
+        <Route path="/customer/products" component={ Products } />
       </Switch>
     </BrowserRouter>
   );
--- front-end/src/pages/Products.js
+++ front-end/src/pages/Products.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import CardProducts from '../components/CardProducts';
+import CardProducts from '../Components/CardProducts';
 import NavBar from '../components/NavBar';
 
 function Products() {

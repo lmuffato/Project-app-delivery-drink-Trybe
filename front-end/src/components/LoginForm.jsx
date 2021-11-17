cba76a78d1148207bd04d8565a23938eb0885d94
biazidioti <bzferreira.bio@gmail.com>
2021-11-17 15:47:27 -0300

    feat: login - email and passwaord validation

--- front-end/src/components/LoginForm.jsx
+++ front-end/src/components/LoginForm.jsx
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
+import emailValidation from '../validations/loginValidation';
 
 export default function Login() {
+  const [emailInput, setEmailInput] = useState('');
+  const [passwordInput, setPasswordInput] = useState('');
+  const [disabledBtn, setDisabledBtn] = useState(true);
+
+  /* const handleButtonClick = () => {
+  }; */
+
   return (
     <form>
       <label htmlFor="input-email">
@@ -10,8 +18,13 @@ export default function Login() {
           id="input-email"
           name="email"
           placeholder="email@trybe.com.br"
+          onChange={ (e) => {
+            setEmailInput(e.target.value);
+            setDisabledBtn(!emailValidation(emailInput, passwordInput));
+          } }
         />
       </label>
+      <h2>{emailInput}</h2>
       <br />
       <label htmlFor="input-password">
         <input
@@ -20,16 +33,28 @@ export default function Login() {
           id="input-password"
           name="senha"
           placeholder="digite sua senha"
+          onChange={ (e) => {
+            setPasswordInput(e.target.value);
+            setDisabledBtn(!emailValidation(emailInput, passwordInput));
+          } }
         />
       </label>
+      <h2>{passwordInput}</h2>
       <br />
       <button
         data-testid="common_login__button-login"
         type="button"
+        onClick={ () => handleButtonClick() }
+        disabled={ disabledBtn }
+      >
+        LOGIN
+      </button>
+      <button
+        data-testid="common_login__button-register"
+        type="button"
       >
         Ainda não tenho conta
       </button>
-      <button data-testid="common_login__button-register" type="button">LOGIN</button>
     </form>
   );
 }

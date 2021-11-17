cc184e1f40a6e808901e372e45aaf862cef1eaa2
Parker9309 <juan.tentiomaru9309@gmail.com>
2021-11-17 18:31:37 -0300

    feat: Navbar component created

---
+++ front-end/src/Components/NavBar.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../Styles/Navbar.css';
+
+function Navbar() {
+  return (
+    <div className="footer">
+      <ul className="icons-container">
+        <Link to="/customer/products">
+          <li
+            data-testid="customer_products__element-navbar-link-products"
+          >
+            <p>PRODUTOS</p>
+          </li>
+        </Link>
+        <Link to="/customer/checkout">
+          <li
+            data-testid="customer_products__element-navbar-link-orders"
+          >
+            <p>MEUS PEDIDOS</p>
+          </li>
+        </Link>
+        <li><p>NAME</p></li>
+        <Link to="/">
+          <li><p>SAIR</p></li>
+        </Link>
+      </ul>
+    </div>
+  );
+}
+
+export default Navbar;

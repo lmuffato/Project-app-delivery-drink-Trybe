f0f06c77a875099d91c3647f2940addb9abb9134
AndreBarroso <andre_barroso17@hotmail.com>
2021-11-17 17:29:59 -0300

    feat: inclues cors no backend

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
+
 app.use(bodyParser.json());
+
+
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 

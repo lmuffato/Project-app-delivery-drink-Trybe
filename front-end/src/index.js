861ab733fe5c79fe18b189000b4a1fbbc6a823b4
giovanni-maldonado <giovannitadeu08@gmail.com>
2021-11-17 17:05:50 -0300

    feat: setup basico do tailwind & arrumando o lint

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import reportWebVitals from './reportWebVitals';
-import './index.css';
-import App from './App';
 import { Provider } from 'react-redux';
+import reportWebVitals from './reportWebVitals';
+import './style/main.css';
+import App from './App';
 import store from './redux/store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App/>
+    <Provider store={ store }>
+      <App />
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),

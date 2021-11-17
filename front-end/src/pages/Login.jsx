d09174b0878e1387c7b081c1e66cfb65866d404e
AndreBarroso <andre_barroso17@hotmail.com>
2021-11-17 17:18:04 -0300

    feat: requisitçao login front back 100% testado e fincional

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,15 +1,32 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState, useContext } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import ErrorLogin from '../Components/ErrorLogin';
 
+import UserContext from '../context/userContext';
+import { doLogin } from '../services/endpointsAPI';
+
 export default function Login() {
+  const history = useHistory();
+  const { setUserData } = useContext(UserContext);
+
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [loginButton, setLoginButton] = useState(false);
   const [errorMessage, setErrorMessage] = useState(false);
 
-  const toggleErrorMessage = (user) => {
-    if (!user.password || !user.email) {
+  // const toggleErrorMessage = (user) => {
+  //   if (!user.password || !user.email) {
+  //     setErrorMessage(true);
+  //   }
+  // };
+
+  const clickLoginButton = async () => {
+    try {
+      const login = await doLogin(email, password);
+      console.log(login);
+      setUserData(login);
+      history.push('/customer/products');
+    } catch (error) {
       setErrorMessage(true);
     }
   };
@@ -52,7 +69,7 @@ export default function Login() {
         <button
           variant="primary"
           disabled={ !loginButton }
-          onClick={ ({ target }) => toggleErrorMessage(target.value) }
+          onClick={ clickLoginButton }
           data-testid="common_login__button-login"
           type="button"
         >

9eb81519938ae6bc09255ee9e0a38e30744f051a
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2021-11-17 07:15:40 -0300

    fix: altera retorno do login

--- back-end/src/controller/loginController.js
+++ back-end/src/controller/loginController.js
@@ -19,8 +19,10 @@ const loginUser = async (req, res) => {
     if (login.message) {
       return res.status(login.code).json({ message: login.message });
     }
+
+    const { name, role } = login;
     
-    return res.status(200).json({ token });
+    return res.status(200).json({ name, email, role, token });
   } catch (e) {
     return res.status(500).json({ message: e.message });
   }

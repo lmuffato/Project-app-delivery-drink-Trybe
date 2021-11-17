66f38b92bb1e700786c78d64d98ec6370fd9fb64
Marcelo Maurício <mmauricio.design@gmail.com>
2021-11-17 16:11:02 -0300

    Created a getByUserId function in Sale Controller

--- back-end/src/database/controllers/saleController.js
+++ back-end/src/database/controllers/saleController.js
@@ -3,9 +3,22 @@ const saleService = require('../services/saleService');
 
 async function create(req, res) {
   try {
-    const { code } = await saleService.create(req.body);
+    const { code, data } = await saleService.create(req.body);
 
-    return res.status(code).json({ message: 'Works' });
+    return res.status(code).json(data);
+  } catch(e) {
+    return res.status(HTTP_NOT_FOUND).json({ error: e.message });
+  }
+};
+
+async function getByUserId(req, res) {
+  try {
+    const { id } = req.params;
+    const { code, error, data } = await saleService.getByUserId(id);
+
+    if (error) return res.status(code).json({ error });
+
+    return res.status(code).json(data);
   } catch(e) {
     return res.status(HTTP_NOT_FOUND).json({ error: e.message });
   }
@@ -13,4 +26,5 @@ async function create(req, res) {
 
 module.exports = {
   create,
+  getByUserId,
 }

02db13bca785bfef5660c389155f69dfc396f341
Gustavo Sousa <gustavohl98@gmail.com>
2021-11-17 19:13:25 -0300

    Create route POST /sales

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -5,6 +5,7 @@ const path = require('path');
 const errorHandler = require('./middlewares/errorHandler');
 const userController = require('./controllers/userController');
 const productController = require('./controllers/productController');
+const saleController = require('./controllers/saleController');
 
 const app = express();
 
@@ -12,14 +13,14 @@ app.use('/', express.static(path.join(__dirname, '..', '..', 'public')));
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use(cors());
-
 app.get('/', (req, res) => {
   res.status(200).send('OK FUNCIONANDO');
 });
 
 app.get('/products/:id', productController.getOne);
 app.get('/products', productController.getAll);
+
+app.post('/sales', saleController.create);
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 

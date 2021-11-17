ff608df1603fc19b97035941a29fb669c003613a
Parker9309 <juan.tentiomaru9309@gmail.com>
2021-11-17 14:17:25 -0300

    feat: error login message component done

---
+++ front-end/src/Components/ErrorLogin.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function ErrorLogin() {
+  return (
+    <div>
+      <p data-testid="common_login__element-invalid-email">
+        Login e/ou senha inválidos
+      </p>
+    </div>
+
+  );
+}
+
+export default ErrorLogin;

b3ec032d412867cdeebe4c1c93310ac2c2b003f3
hannibal1207 <mathausannibal@gmail.com>
2021-11-17 17:03:31 -0300

    fluxo de vendas

--- back-end/src/controller/index.js
+++ back-end/src/controller/index.js
@@ -1,9 +1,11 @@
 const login = require('./loginController');
 const user = require('./userController');
 const product = require('./productControllers');
+const sale = require('./saleController');
 
 module.exports = {
   login,
   user,
   product,
+  sale,
 };
--- back-end/src/database/migrations/20211110001208-create-sales.js
+++ back-end/src/database/migrations/20211110001208-create-sales.js
@@ -8,44 +8,49 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      user_id: {
+      userId: {
         type: Sequelize.INTEGER,
-        references: { model: 'users', key: 'id' },
-        field: "user_id",
-        onUpdate: 'CASCADE',
-        onDelete: 'CASCADE',
-        foreignKey: true,
-      },
-      seller_id: {
-        type: Sequelize.INTEGER,
+        field: 'user_id',
         references: { model: 'users', key: 'id' },
         onUpdate: 'CASCADE',
         onDelete: 'CASCADE',
         foreignKey: true,
       },
-      total_price: {
-        type: Sequelize.DECIMAL(10,2)
+      sellerId: {
+        type: Sequelize.INTEGER,
+        field: 'seller_id',
+        references: { model: 'users', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        foreignKey: true,
       },
-      delivery_address: {
-        type: Sequelize.STRING
+      totalPrice: {
+        type: Sequelize.INTEGER,  /* DECIMAL(10,2), */
+        field: 'total_price'
       },
-      delivery_number: {
-        type: Sequelize.STRING
+      deliveryAddress: {
+        type: Sequelize.STRING,
+        field: 'delivery_address'
       },
-      sale_date: {
-        type: Sequelize.DATE
+      deliveryNumber: {
+        type: Sequelize.STRING,
+        field: 'delivery_number'
+      },
+      saleDate: {
+        type: Sequelize.DATE,
+        field: 'sale_date'
       },
       status: {
         type: Sequelize.STRING
       },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
+      // createdAt: {
+      //  allowNull: false,
+      //  type: Sequelize.DATE
+      //},
+     // updatedAt: {
+     //   allowNull: false,
+     //   type: Sequelize.DATE
+     // }
     });
   },
   down: async (queryInterface, _Sequelize) => {

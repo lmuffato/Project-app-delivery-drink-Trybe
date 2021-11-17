198eb78bc0d44e711acce4b1c15e83051b1f8a78
Parker9309 <juan.tentiomaru9309@gmail.com>
2021-11-17 14:16:31 -0300

    fix: Routes fixed

--- front-end/src/pages/Routes.jsx
+++ front-end/src/pages/Routes.jsx
@@ -2,6 +2,13 @@ import React from 'react';
 import { Redirect, Switch, Route } from 'react-router-dom';
 import Register from './Register';
 import Login from './Login';
+import CustomerProducts from './CustomerProducts';
+import CustomerCheckout from './CustomerCheckout';
+import CustomerOrder from './CustomerOrder';
+import OrderDetails from './OrderDetails';
+import SellerOrders from './SellersOrders';
+import SellerOrdersDetails from './SellerOrdersDetails';
+import AdminManage from './AdminManage';
 
 export default function Rout() {
   return (

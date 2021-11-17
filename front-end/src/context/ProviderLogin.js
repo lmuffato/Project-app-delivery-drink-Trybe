bff12cf45ef471c65fe2b442eec85f095cec4140
mallmann02 <leomallmann020302@gmail.com>
2021-11-17 16:12:07 -0300

    feat(login): share logged user data in context

--- front-end/src/context/ProviderLogin.js
+++ front-end/src/context/ProviderLogin.js
@@ -1,20 +1,33 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import ContextLogin from './ContextLogin';
-import storeUser from '../utils/putUserDataInLocalStorage';
+import {
+  verifyUserExistance,
+  storeUser,
+} from '../utils/LocalStorageFunctions';
 
 const axios = require('axios').default;
 
 function ProviderLogin({ children }) {
   const [invalidEmailError, setInvalidEmailError] = useState(false);
   const [signUpErrorMessage, setSignUpErrorMessage] = useState(false);
+  const [userData, setUserData] = useState();
   const urlBase = 'http://localhost:3001';
+  const history = useHistory();
+
+  useEffect(() => {
+    const user = verifyUserExistance();
+    if (!user) history.push('/login');
+    setUserData(user);
+  }, []);
 
   const makeLogin = async (email, password) => {
     setInvalidEmailError(false);
     try {
       const { data } = await axios.post(`${urlBase}/login`, { email, password });
       storeUser(data);
+      setUserData(data);
       return true;
     } catch (error) {
       setInvalidEmailError(true);
@@ -44,6 +57,7 @@ function ProviderLogin({ children }) {
         makeLogin,
         signUpErrorMessage,
         invalidEmailError,
+        userData,
       } }
     >
       {children}

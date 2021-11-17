b4d865ba8beb1809a825ef6edfded1cac4aeb12b
Joao Nasc <oi@joaonasc.dev>
2021-11-17 08:56:04 -0300

    feat: atualiza componente de rota de produtos

--- front-end/src/Routes.jsx
+++ front-end/src/Routes.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import RegisterForm from './components/registerForm';
 import Auth from './pages/Auth';
+import Products from './pages/customer/Products';
 
 export default function Routes() {
   return (
     <Switch>
-      <Route path="/customer/products" render={ () => <h1>Produtos</h1> } />
+      <Route path="/customer/products" component={ Products } />
       <Route path="/login" component={ Auth } />
       <Route path="/register" component={ RegisterForm } />
       <Route path="/" render={ () => <Redirect to="/login" /> } />

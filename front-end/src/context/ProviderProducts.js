a94cc61d267c1799017ed1ef3b4e2fa50c841129
mallmann02 <leomallmann020302@gmail.com>
2021-11-16 21:30:32 -0300

    feat(customer_flow): create products provider

--- front-end/src/context/ProviderProducts.js
+++ front-end/src/context/ProviderProducts.js
@@ -6,6 +6,7 @@ const axios = require('axios').default;
 
 function ProviderProducts({ children }) {
   const [allProducts, setAllProducts] = useState([{}]);
+  const [cartProducts, setCartProducts] = useState([{}]);
   const urlBase = 'http://localhost:3001';
 
   const findProducts = async () => {
@@ -14,11 +15,54 @@ function ProviderProducts({ children }) {
     return products;
   };
 
+  const findSpecificProduct = async (id) => {
+    const { data: product } = await axios.get(`${urlBase}/products/${id}`);
+    return product;
+  };
+
+  const increaseProductQuantity = async (id) => {
+    const productInCart = cartProducts.find((product) => product.id === id);
+
+    if (productInCart) {
+      productInCart.quantity += 1;
+      return;
+    }
+
+    const product = await findSpecificProduct(id);
+
+    setCartProducts([...cartProducts, { ...product, quantity: 1, id }]);
+  };
+
+  const setProductQuantity = async (id, quantity) => {
+    const productInCart = cartProducts.find((product) => product.id === id);
+
+    if (productInCart) {
+      productInCart.quantity = quantity;
+      return;
+    }
+
+    const product = await findSpecificProduct(id);
+
+    setCartProducts([...cartProducts, { ...product, quantity, id }]);
+  };
+
+  const decreaseProductQuantity = (id) => {
+    const productInCart = cartProducts.find((product) => product.id === id);
+
+    if (!productInCart || productInCart.quantity === 0) return;
+
+    productInCart.quantity -= 1;
+  };
+
   return (
     <ContextProducts.Provider
       value={ {
         findProducts,
         allProducts,
+        increaseProductQuantity,
+        cartProducts,
+        setProductQuantity,
+        decreaseProductQuantity,
       } }
     >
       {children}

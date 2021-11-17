42752dd13ae4b8cfb283d68bbce0155fdcd8cd79
Parker9309 <juan.tentiomaru9309@gmail.com>
2021-11-17 15:33:53 -0300

    fix: index lint

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import UserProvider from './context/userProvider';
+// import reportWebVitals from './reportWebVitals';
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 
 ReactDOM.render(
-  <React.StrictMode>,
+  <React.StrictMode>
+    ,
 
     <BrowserRouter>
       <UserProvider>
         <App />
       </UserProvider>
-    </BrowserRouter>,
+    </BrowserRouter>
+    ,
   </React.StrictMode>,
   document.getElementById('root'),
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();

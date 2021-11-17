a33a1c652a2f2e6367e6d067f06f9dce6b183ee8
Marcus Hüsemann <mchusemann@gmail.com>
2021-11-17 13:48:06 -0300

    feat(ProductsList): adiciona data-tesids

--- front-end/src/components/ProductsList.jsx
+++ front-end/src/components/ProductsList.jsx
@@ -3,13 +3,29 @@ import axios from 'axios';
 import { ProductsContext } from '../contexts/Products';
 
 const listProducts = (products) => products.map((product, index) => (
-  <div data-testid={ products.id } key={ index }>
+  <div data-testid={ product.id } key={ index }>
     <h1>{ product.name }</h1>
-    <img src={ product.urlImage } alt={ product.name } />
-    <h3>{ product.price }</h3>
-    <button type="button">-</button>
-    <p>0</p>
-    <button type="button">+</button>
+    <img
+      data-testid={ `customer_products__img-card-bg-image-${product.id}` }
+      src={ product.urlImage }
+      alt={ product.name }
+    />
+    <h3 data-testid={ `customer_products__element-card-price-${product.id}` }>
+      { product.price }
+    </h3>
+    <button
+      data-testid={ `customer_products__button-card-add-item-${product.id}` }
+      type="button"
+    >
+      +
+    </button>
+    <p data-testid={ `customer_products__input-card-quantity-${product.id}` }>0</p>
+    <button
+      data-testid={ `customer_products__button-card-rm-item-${product.id}` }
+      type="button"
+    >
+      -
+    </button>
   </div>
 ));
 
@@ -21,8 +37,11 @@ function ProductList() {
       try {
         const { data: { data } } = await axios.get(
           'http://localhost:3001/products',
-          { headers: {
-            Authorization: `Bearer ${token}` } },
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          },
         );
         setValues({ ...values, data });
         console.log(data);

36e477e72118c1bb942bac1a6288006ac49f74e6
Marcus Hüsemann <mchusemann@gmail.com>
2021-11-17 13:47:18 -0300

    feat: adiciona data-testids e importa e adiciona botao de logout

--- front-end/src/components/NavBar.jsx
+++ front-end/src/components/NavBar.jsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
+import ButtonLogout from './ButtonLogout';
+import { LoginContext } from '../contexts/Login';
 
 function NavBar() {
+  console.log(LoginContext);
+  const { values } = useContext(LoginContext);
+
   return (
     <ul>
-      <li><NavLink to="/products">Produtos</NavLink></li>
-      <li><NavLink to="/orders">Meus Pedidos</NavLink></li>
-      <li>Username</li>
-      <li><NavLink to="/logout">Sair</NavLink></li>
+      <li data-testid="customer_products__element-navbar-link-products">
+        <NavLink to="/products">
+          Produtos
+        </NavLink>
+      </li>
+      <li data-testid="customer_products__element-navbar-link-orders">
+        <NavLink to="/orders">
+          Meus Pedidos
+        </NavLink>
+      </li>
+      <li data-testid="customer_products__element-navbar-user-full-name">
+        { values.name || 'username' }
+      </li>
+      <li data-testid="customer_products__element-navbar-link-logout">
+        <ButtonLogout />
+      </li>
     </ul>
   );
 }

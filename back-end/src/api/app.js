b0bca02be8240f1cf597a607512bffa41f688492
Rodrigo Rech <rodrigorech92@gmail.com>
2021-11-18 18:32:35 -0300

    Fix: lint

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -12,8 +12,8 @@ const { checkPassword } = require('../middlewares/checkPassword');
 const { validateToken } = require('../middlewares/validateToken');
 const { checkName } = require('../middlewares/checkName');
 // const validateProducts = require('../middlewares/validateProducts');
-const validateSeller = require('../middlewares/validateSeller');
-const validateCustomer = require('../middlewares/validateUser');
+// const validateSeller = require('../middlewares/validateSeller');
+// const validateCustomer = require('../middlewares/validateUser');
 const validateAddress = require('../middlewares/validateAddress');
 const uploadImages = require('../middlewares/uploadImages');
 

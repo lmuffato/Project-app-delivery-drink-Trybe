3c9520f3bf95d10418f0f54c30020cdf13490e60
iago <iago.pferreiravr@gmail.com>
2021-11-18 17:25:48 -0300

    feat(frontend-admin): Renderizando o NavBar personalizado dentro da tela  Admin

--- front-end/src/pages/Admin.jsx
+++ front-end/src/pages/Admin.jsx
@@ -1,11 +1,35 @@
+// import React from 'react';
+// import UserForm from '../components/UserForm';
+
+// function Admin() {
+//   return (
+//     <main>
+//       <UserForm />
+//     </main>
+//   );
+// }
+
 import React from 'react';
+// import { NavLink } from 'react-router-dom';
+import NavBar from '../components/NavBar';
 import UserForm from '../components/UserForm';
+
+const testIds = {
+  pageId: 'customer_products__element-navbar-link-orders',
+  userId: 'customer_products__element-navbar-user-full-name',
+};
+
+const navegationNames = {
+  pageName: 'Gerenciar usuários',
+  userName: 'Admin',
+};
 
 function Admin() {
   return (
-    <main>
+    <div>
+      <NavBar ids={ testIds } names={ navegationNames } />
       <UserForm />
-    </main>
+    </div>
   );
 }
 

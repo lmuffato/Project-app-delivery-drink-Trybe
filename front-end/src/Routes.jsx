7da721d18595b62162c0aa603215c5fe212a1a96
Joao Nasc <oi@joaonasc.dev>
2021-11-18 17:15:53 -0300

    feat: renderiza navbar nas rotas de customer

--- front-end/src/Routes.jsx
+++ front-end/src/Routes.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
+import NavBar from './components/NavBar';
 import RegisterForm from './components/registerForm';
 import Auth from './pages/Auth';
 
 export default function Routes() {
   return (
     <Switch>
-      <Route path="/customer/products" render={ () => <h1>Produtos</h1> } />
+      <Route path={ ['/customer'] }>
+        <NavBar />
+        <Route path="/customer/products" render={ () => <h1>Produtos</h1> } />
+      </Route>
       <Route path="/login" component={ Auth } />
       <Route path="/register" component={ RegisterForm } />
       <Route path="/" render={ () => <Redirect to="/login" /> } />

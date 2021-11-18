e4e848fc82b502d07e1aa85cd3182bc89bf1ce69
biazidioti <bzferreira.bio@gmail.com>
2021-11-18 15:29:58 -0300

    Merge conflits

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -12,6 +12,6 @@ app.use(cors());
 app.use('/user', useRoutes);
 app.get('/coffee', (_req, res) => res.status(418).end());
 
-app.get('/seller', getSalesRouter);
+app.use('/seller', getSalesRouter);
 
 module.exports = app;

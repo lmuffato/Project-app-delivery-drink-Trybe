aae00db45959b7e718d86c48af1c0ac5caa6ff2a
Marcelo Maurício <mmauricio.design@gmail.com>
2021-11-18 18:06:19 -0300

    Create a function to get the orders in the component and created a basic style for him

--- front-end/src/pages/OrderDetails/index.js
+++ front-end/src/pages/OrderDetails/index.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+import MenuCostumer from '../../components/MenuCustomer';
+import api from '../../services/api';
 
 import './style.css';
 
 function OrderDetails() {
+  const [sale, setSale] = useState({});
+  const { id } = useParams();
+
+  useEffect(() => {
+    const { token } = JSON.parse(localStorage.getItem('user'));
+
+    const fetchSale = async () => {
+      const saleFetched = await api.getSaleById(id, token);
+
+      setSale(saleFetched);
+    };
+
+    fetchSale();
+  }, [id]);
+
+  console.log(sale);
+
   return (
-    <div className="order--detail-container">
-      <p>Teste dos detalhes</p>
-    </div>
+    <section className="ordersPage">
+      <MenuCostumer />
+      <div className="saleDetailsContainer">
+        Teste dos detalhes
+      </div>
+    </section>
   );
 }
 

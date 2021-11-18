9ec69918571b6d41e54a7d723ac96031fa003ebe
Viegas <mvrdgs@gmail.com>
2021-11-18 11:04:18 -0300

    feat: ajustado header para alternar entre customer e seller

--- front-end/src/components/header.jsx
+++ front-end/src/components/header.jsx
@@ -4,25 +4,36 @@ import Context from '../context/Context';
 
 function Header() {
   const { user } = useContext(Context);
-  console.log(user);
+  const { name, role } = user;
 
   return (
     <div className="container">
-      <div className="border">
-        <h1 data-testid="customer_products__element-navbar-link-products">Produtos</h1>
-      </div>
-      <div className="border">
-        <h1
-          data-testid="customer_products__element-navbar-link-orders"
-        >
-          Meus Pedidos
-        </h1>
-      </div>
+      { role === 'customer' && (
+        <>
+          <div className="border">
+            <h1 data-testid="customer_products__element-navbar-link-products">
+              Produtos
+            </h1>
+          </div>
+          <div className="border">
+            <h1
+              data-testid="customer_products__element-navbar-link-orders"
+            >
+              Meus Pedidos
+            </h1>
+          </div>
+        </>
+      ) }
+      { role === 'seller' && (
+        <div className="border">
+          <h1 data-testid="customer_products__element-navbar-link-orders">Pedidos</h1>
+        </div>
+      )}
       <div className="border">
         <h1
           data-testid="customer_products__element-navbar-user-full-name"
         >
-          { user.name }
+          { name }
 
         </h1>
       </div>

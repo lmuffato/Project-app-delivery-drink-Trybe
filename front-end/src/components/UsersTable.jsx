7af73baca1a895fd4515b34eb2da195f90a805db
iago <iago.pferreiravr@gmail.com>
2021-11-18 19:54:56 -0300

    feat(frontend-admin): Implementando lógica de renderizar a tabela

--- front-end/src/components/UsersTable.jsx
+++ front-end/src/components/UsersTable.jsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { UsersContext } from '../contexts/Users';
+
+const tableHeaders = ['Item', 'Nome', 'E-mail', 'Tipo', 'Excluir'];
 
 function UsersTable() {
+  const { usersList, removeUser } = useContext(UsersContext);
+
   return (
     <table>
-      Tabela
+      <thead>
+        { tableHeaders.map((header, index) => (<th key={ index }>{header}</th>))}
+      </thead>
+      <tbody>
+        {
+          usersList.map(({ id, email, name, role }, index) => (
+            <tr key={ index }>
+              <td data-testid="admin_manage__element-user-table-item-number-">{index}</td>
+              <td data-testid="admin_manage__element-user-table-name-">{name}</td>
+              <td data-testid="admin_manage__element-user-table-email-">{email}</td>
+              <td data-testid="admin_manage__element-user-table-role-">{role}</td>
+              <button
+                type="button"
+                data-testid="admin_manage__element-user-table-remove-"
+                onClick={ () => removeUser(id) }
+              >
+                Excluir
+              </button>
+            </tr>
+          ))
+        }
+      </tbody>
     </table>
   );
 }

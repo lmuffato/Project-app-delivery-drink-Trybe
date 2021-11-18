3023cb9ae66a0d543de8191229d20e555a155291
Felipe Muller <felipeavmuller@gmail.com>
2021-11-18 16:57:54 -0300

    Requisito 17 - Checkout Page

---
+++ front-end/src/components/CheckoutCard.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function CheckoutCard({ index, name, qty, price, total }) {
+  return (
+    <tr>
+      <td
+        data-testid={ `customer_checkout__element-order-table-item-number-${index}` }
+      >
+        { Number(index) + 1 }
+      </td>
+      <td
+        data-testid={ `customer_checkout__element-order-table-name-${index}` }
+      >
+        { name }
+      </td>
+      <td
+        data-testid={ `customer_checkout__element-order-table-quantity-${index}` }
+      >
+        { qty }
+      </td>
+      <td
+        data-testid={ `customer_checkout__element-order-table-unit-price-${index}` }
+      >
+        { `R$ ${Number(price).toFixed(2).replace('.', ',')}` }
+      </td>
+      <td
+        data-testid={ `customer_checkout__element-order-table-sub-total-${index}` }
+      >
+        { `R$ ${total.toFixed(2).replace('.', ',')}` }
+      </td>
+      <td
+        data-testid={ `customer_checkout__element-order-table-remove-${index}` }
+      >
+        <button type="button">Remover</button>
+      </td>
+    </tr>
+  );
+}
+
+CheckoutCard.propTypes = {
+  index: PropTypes.number,
+  name: PropTypes.string,
+  qty: PropTypes.number,
+  price: PropTypes.number,
+  total: PropTypes.number,
+}.isRequired;
+
+export default CheckoutCard;
--- front-end/src/pages/Checkout.jsx
+++ front-end/src/pages/Checkout.jsx
@@ -1,9 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import CheckoutCard from '../components/CheckoutCard';
+import CustomerAddress from '../components/CustomerAddress';
 
 function Checkout() {
+  const [products, setProducts] = useState([]);
+  const [totalCheckout, setTotalCheckout] = useState(0);
+
+  useEffect(() => {
+    setTotalCheckout(products
+      .reduce((totalP, product) => totalP + product.total, 0));
+  });
+
+  useEffect(() => {
+    setProducts(JSON.parse(localStorage.getItem('carrinho')));
+  }, {});
+
   return (
     <>
-      Checkout
+      Finalizar Pedido
+      <div>
+        <table>
+          <tr>
+            <th>Item</th>
+            <th>Descrição</th>
+            <th>Quantidade</th>
+            <th>Valor Unitário</th>
+            <th>Sub-total</th>
+            <th>Remover Item</th>
+          </tr>
+          {products.map((product, index) => (
+            <CheckoutCard
+              key={ index }
+              index={ index }
+              name={ product.name }
+              qty={ product.quantity }
+              price={ product.price }
+              total={ product.total }
+            />
+          ))}
+        </table>
+        <div>
+          <span
+            data-testid="customer_checkout__element-order-total-price"
+          >
+            { `Total R$ ${totalCheckout.toFixed(2).replace('.', ',')}` }
+          </span>
+        </div>
+      </div>
+      <CustomerAddress />
     </>
   );
 }

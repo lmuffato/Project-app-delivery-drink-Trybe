ee7a41638f4983ad377f8596b093a37d59aa252b
Luciano Amâncio <madruga665@hotmail.com>
2021-11-18 08:04:21 -0300

    chore: correção dos data-testids

--- front-end/src/components/RequestCard/index.jsx
+++ front-end/src/components/RequestCard/index.jsx
@@ -7,19 +7,19 @@ export default function RequestCard({ requestId, status, date, price }) {
     <div className={ styles.cardContainer }>
       <div
         className={ styles.requestId }
-        data-testid={ `customer_orders__element-order-${requestId}` }
+        data-testid={ `customer_orders__element-order-id-${requestId}` }
       >
         <span>Pedido</span>
         <span>{requestId}</span>
       </div>
       <div
         className={ styles[status] }
-        data-testid={ `customer_orders__element-delivery-status-${requestId}` }
+        data-testid={ `customer_orders__element-delivery-status-id-${requestId}` }
       >
         {status}
       </div>
       <div className={ styles.dateAndPrice }>
-        <span data-testid={ `customer_orders__element-order-date-${requestId}` }>
+        <span data-testid={ `customer_orders__element-order-date-id-${requestId}` }>
           {date}
         </span>
         <span>{`R$ ${price}`}</span>

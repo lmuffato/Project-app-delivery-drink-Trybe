7080cf877cbe05505ac2c7cf35dcf09fc0bb3234
Orlando Flores <orlando_flores@id.uff.br>
2021-11-18 19:06:23 -0300

    feat: uses context to get cartProducts

--- front-end/src/screens/CustomerCheckout.jsx
+++ front-end/src/screens/CustomerCheckout.jsx
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Button, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
 import DeliveryDetails from '../components/DeliveryDetails';
 import ProductsCheckoutTable from '../components/ProductsCheckoutTable';
+import ContextProducts from '../context/ContextProducts';
 
 function CustomerCheckout() {
-  const total = 100.87;
+  const { cartProducts, calculateSubtotal } = useContext(ContextProducts);
   return (
     <div>
-      <ProductsCheckoutTable />
+      <ProductsCheckoutTable checkoutCart={ cartProducts } />
       <p
         data-testid="customer_checkout__element-order-total-price"
       >
-        { `Total: R$ ${total}` }
+        { `R$ ${calculateSubtotal(cartProducts).toString().replace('.', ',')}` }
       </p>
       <DeliveryDetails />
+      <Button
+        data-testid="customer_checkout__button-submit-order"
+      >
+        <Link
+          to="/"
+        >
+          <Typography>
+            Finalizar pedido
+          </Typography>
+        </Link>
+      </Button>
     </div>
   );
 }

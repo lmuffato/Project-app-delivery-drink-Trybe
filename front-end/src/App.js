3af7f42d813b4b9ad94c57467e06288661fea054
Hernanne Domiciano <78621976+hernannegp@users.noreply.github.com>
2021-11-18 15:42:58 -0300

    feat: colocando uma rota pra testar

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,6 +6,7 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import CustomerProducts from './pages/CustomerProducts';
 import CustomerCheckout from './pages/CustomerCheckout';
+import Orders from './components/Orders';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
       <Route path="/register" component={ Register } />
       <Route exact path="/customer/products" component={ CustomerProducts } />
       <Route exact path="/customer/checkout" component={ CustomerCheckout } />
+      <Route exact path="/teste" component={ Orders } />
     </Switch>
   );
 }

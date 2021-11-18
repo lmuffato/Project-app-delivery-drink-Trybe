040f319d596e800d93fca1364ed1faaea53be6a8
Bruno Mendes <mendessbruno@gmail.com>
2021-11-18 16:08:17 -0300

    fix: alterado service do login para retornar um objeto com nome, email, token e role

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const userController = require('./controllers/userController');
 const loginvalidationMid = require('./middlewares/loginValidationMid');
 const registerValidationMid = require('./middlewares/registerValidationMid');
+const authMid = require('./middlewares/authMid');
 
 const app = express();
 app.use(express.json());
@@ -15,5 +16,7 @@ app.get('/coffee', (_req, res) => res.status(418).end());
 
 app.post('/login', loginvalidationMid, userController.login);
 app.post('/register', registerValidationMid, userController.create);
+
+app.get('/products', authMid);
 
 module.exports = app;

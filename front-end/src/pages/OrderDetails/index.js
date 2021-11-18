96c07c63bd19504e8c9f00e5d8aa0d096cdacb7d
Marcelo Maurício <mmauricio.design@gmail.com>
2021-11-18 17:33:15 -0300

    Starting the order details by order id

---
+++ front-end/src/pages/OrderDetails/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './style.css';
+
+function OrderDetails() {
+  return (
+    <div className="order--detail-container">
+      <p>Teste dos detalhes</p>
+    </div>
+  );
+}
+
+export default OrderDetails;

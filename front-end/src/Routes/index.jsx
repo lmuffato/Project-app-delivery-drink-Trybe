c9eaf890dd309aacbd232cde10194e236fe127a6
Joao Nasc <oi@joaonasc.dev>
2021-11-19 07:30:54 -0300

    refactor: login e register exibem o mesmo componente

--- front-end/src/Routes/index.jsx
+++ front-end/src/Routes/index.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import NavBar from '../components/NavBar';
-import RegisterPage from '../pages/RegisterPage';
 import Auth from '../pages/Auth';
 import ProtectedRoute from './ProtectedRoute';
 
 export default function Routes() {
   return (
     <Switch>
-      <Route path="/login" component={ Auth } />
-      <Route path="/register" component={ RegisterPage } />
+      <Route path={ ['/login', '/register'] } component={ Auth } />
       <ProtectedRoute path={ ['/customer', '/'] }>
         <NavBar />
         <Route path="/customer/products" render={ () => <h1>Produtos</h1> } />

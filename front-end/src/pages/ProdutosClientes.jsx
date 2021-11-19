2f0fbf5bbb5a9ae435542251e58609a275770eb0
Micaela Alves <micaela.alves93@gmail.com>
2021-11-18 23:38:36 -0300

    feat -  layout  pagina produtos/ container cards

--- front-end/src/pages/ProdutosClientes.jsx
+++ front-end/src/pages/ProdutosClientes.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 
 import NavBar from '../components/NavBar/NavBar';
 import CardProduto from '../components/CardProduto/CardProduto';
+import '../styles/ProdutosClientes.css';
 
 function ProdutosClientes() {
   return (
     <>
       <NavBar />
-      <CardProduto />
+      <main className="main-cards">
+        <CardProduto />
+      </main>
     </>
   );
 }

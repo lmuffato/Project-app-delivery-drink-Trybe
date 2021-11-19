5bfd06ae33db605f8e3782f95d928abc82453466
Joao Nasc <oi@joaonasc.dev>
2021-11-19 07:31:44 -0300

    feat: move a lógica de login para componente e gera condição para exibição de forms

--- front-end/src/pages/Auth.jsx
+++ front-end/src/pages/Auth.jsx
@@ -1,33 +1,13 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { useHistory } from 'react-router-dom';
-import { AuthContext } from '../contexts/auth';
+import PropTypes from 'prop-types';
+import React from 'react';
 import imgManHoldingBeer from '../images/man-holding-beer.png';
 import styles from '../styles/pages/Auth.module.scss';
-import loginSchema from '../schemas/login';
-import useInputs from '../hooks/useInputs';
+import LoginForm from '../components/LoginForm';
+import RegisterForm from '../components/RegisterForm';
 
-export default function Auth() {
-  const history = useHistory();
-  const [inputsValues, setInputsValues] = useInputs({ email: '', password: '' });
-  const { Alert, alertIsVisible, authFormSubmit, validateForm } = useContext(AuthContext);
-  const [buttonDisabled, setButtonDisabled] = useState(true);
-
-  useEffect(() => {
-    const formValidation = validateForm({ schema: loginSchema, values: inputsValues });
-    setButtonDisabled(!formValidation.isValid);
-  }, [inputsValues, validateForm]);
-
-  async function logIn(event) {
-    await authFormSubmit(event, {
-      formSchema: loginSchema,
-      formValues: inputsValues,
-      authType: 'login',
-    }, () => history.push('/customer/products'));
-  }
-
+export default function Auth({ location }) {
   return (
     <section className={ styles.auth }>
-      { alertIsVisible && <Alert dataTestId="common_login__element-invalid-email" /> }
       <span className={ styles.manHoldingBeer }>
         <img
           src={ imgManHoldingBeer }
@@ -35,44 +15,17 @@ export default function Auth() {
         />
       </span>
       <img src="./logo-compact.svg" alt="tchau problema" />
-      <form onSubmit={ logIn }>
-        <label htmlFor="email">
-          E-mail:
-          <input
-            id="email"
-            type="email"
-            placeholder="Digite o seu e-mail"
-            onChange={ setInputsValues }
-            data-testid="common_login__input-email"
-          />
-        </label>
-        <label htmlFor="password">
-          Senha:
-          <input
-            id="password"
-            type="password"
-            placeholder="Digite a sua senha"
-            onChange={ setInputsValues }
-            data-testid="common_login__input-password"
-          />
-        </label>
-        <button
-          type="submit"
-          data-testid="common_login__button-login"
-          className="primary"
-          disabled={ buttonDisabled }
-        >
-          Entrar
-        </button>
-        <button
-          onClick={ () => history.push('/register') }
-          type="button"
-          data-testid="common_login__button-register"
-          className="link"
-        >
-          Ainda não tenho conta
-        </button>
-      </form>
+      { location.pathname.includes('login') ? (
+        <LoginForm />
+      ) : (
+        <RegisterForm />
+      ) }
     </section>
   );
 }
+
+Auth.propTypes = {
+  location: PropTypes.shape({
+    pathname: PropTypes.string,
+  }).isRequired,
+};

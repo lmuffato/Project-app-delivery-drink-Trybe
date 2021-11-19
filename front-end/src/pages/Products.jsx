9ca8cc603fe5a2e6d34c351a2d8d4d608c20379c
Marcus Hüsemann <mchusemann@gmail.com>
2021-11-19 14:31:43 -0300

    feat: configura provider a partir da paginas de Produtos

--- front-end/src/pages/Products.jsx
+++ front-end/src/pages/Products.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
 import ProductsList from '../components/ProductsList';
+import { ProductsProvider } from '../contexts/Products';
+import { CartProvider } from '../contexts/Cart';
 
 function Products() {
   return (
     <>
       <NavBar />
-      <ProductsList />
+      <ProductsProvider>
+        <CartProvider>
+          <ProductsList />
+        </CartProvider>
+      </ProductsProvider>
     </>
   );
 }

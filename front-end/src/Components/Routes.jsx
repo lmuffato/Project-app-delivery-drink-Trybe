8e9b28184c117052ca949959c077cbd441fb13b3
Parker9309 <juan.tentiomaru9309@gmail.com>
2021-11-19 09:59:38 -0300

    fix: Routes fixed

--- front-end/src/Components/Routes.jsx
+++ front-end/src/Components/Routes.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
 import { Redirect, Switch, Route } from 'react-router-dom';
-import Register from '../pages/Register';
-import Login from '../pages/Login';
-import CustomerCheckout from '../pages/CustomerCheckout';
+import Register from './Register';
+import Login from './Login';
+import CustomerProducts from './CustomerProducts';
+import CustomerCheckout from './CustomerCheckout';
+import CustomerOrder from './CustomerOrder';
+import OrderDetails from './OrderDetails';
+import SellerOrders from './SellersOrders';
+import SellerOrdersDetails from './SellerOrdersDetails';
+import AdminManage from './AdminManage';
 import FinishedOrder from '../pages/FinishedOrder';
+
 
 export default function Rout() {
   return (
     <Switch>
+      <Route path="/customer/orders/:id" component={ OrderDetails } />
+      <Route path="/sellers/orders/:id" component={ SellerOrdersDetails } />
       <Route path="/login" component={ Login } />
       <Route path="/register" component={ Register } />
+      <Route path="/customer/products" component={ CustomerProducts } />
       <Route path="/customer/checkout" component={ CustomerCheckout } />
       <Route path="/customer/finished" component={ FinishedOrder } />
+      <Route path="/customer/orders" component={ CustomerOrder } />
+      <Route path="/sellers/orders" component={ SellerOrders } />
+      <Route path="/admin/manage" component={ AdminManage } />
       <Route exact path="/" render={ () => <Redirect to="/login" /> } />
     </Switch>
   );

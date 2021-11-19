78112c5d445f5bd5597a3de279aad4127a1dba43
Parker9309 <juan.tentiomaru9309@gmail.com>
2021-11-19 12:49:46 -0300

    feat: increment and decrement logic implemented

--- front-end/src/Components/CardProduct.jsx
+++ front-end/src/Components/CardProduct.jsx
@@ -1,28 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import '../Styles/Cardproducs.css';
 
-const CardProduct = ({ id, drink, cost, thumb }) => (
-  <div key={ id } className="cardProduct">
-    <div>
-      <p className="unitPrice">{`R$ ${cost}`}</p>
-      <img
-        className="imageDrinkCard"
-        alt="drink"
-        data-testid={ `customer_products__element-card-price-${id}` }
-        src={ thumb }
-      />
-    </div>
-    <div className="footerCardDrink">
-      <h5>{drink}</h5>
-      <div className="addQuantities">
-        <button type="button">-</button>
-        <p>0</p>
-        <button type="button"> + </button>
+export default function CardProduct({ id, drink, cost, thumb }) {
+  const [counter, setCounter] = useState(0);
+
+  function increment() {
+    setCounter(counter + 1);
+  }
+
+  function decrement() {
+    if (counter <= 0) {
+      return 0;
+    }
+    setCounter(counter - 1);
+  }
+
+  return (
+    <div key={ id } className="cardProduct">
+      <div>
+        <p className="unitPrice">{`R$ ${cost}`}</p>
+        <img
+          className="imageDrinkCard"
+          alt="drink"
+          data-testid={ `customer_products__element-card-price-${id}` }
+          src={ thumb }
+        />
+      </div>
+      <div className="footerCardDrink">
+        <h5>{drink}</h5>
+        <div className="addQuantities">
+          <button
+            data-testid={
+              `customer_products__button-card-rm-item-${id}`
+            }
+            onClick={ decrement }
+            type="button"
+          >
+            -
+
+          </button>
+          <p
+            data-testid={
+              `customer_products__input-card-quantity-${id}`
+            }
+          >
+            { counter }
+
+          </p>
+          <button
+            data-testid={
+              `customer_products__button-card-add-item-${id}`
+            }
+            onClick={ increment }
+            type="button"
+          >
+            {' '}
+            +
+            {' '}
+
+          </button>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+}
 
 CardProduct.propTypes = {
   key: PropTypes.string,
@@ -30,5 +72,3 @@ CardProduct.propTypes = {
   cost: PropTypes.string,
   thumb: PropTypes.string,
 }.isRequired;
-
-export default CardProduct;

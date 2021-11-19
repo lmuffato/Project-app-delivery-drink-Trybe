81da820e64afbbfb6f390f8e760bac33a95bec98
Joao Nasc <oi@joaonasc.dev>
2021-11-19 07:12:35 -0300

    feat: move a lógica de autenticação para hook

--- front-end/src/contexts/auth.js
+++ front-end/src/contexts/auth.js
@@ -1,74 +1,18 @@
 import PropTypes from 'prop-types';
-import React, { createContext, useState } from 'react';
-import { useHistory } from 'react-router-dom';
-import useAlert from '../hooks/useAlert';
-import api from '../services/api';
+import React, { createContext } from 'react';
+import useAuth from '../hooks/useAuth';
 
 export const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
-  const [user, setUser] = useState({});
   const {
+    user,
+    logOut,
+    alertIsVisible,
     Alert,
-    alertMessage,
-    alertType,
-    isVisible: alertIsVisible,
-    showAlert,
-  } = useAlert();
-  const history = useHistory();
-
-  function logOut() {
-    history.push('/login');
-    localStorage.removeItem('user');
-    setUser({});
-  }
-
-  function throwAuthFailedAlert(error) {
-    const { request } = error;
-    const message = request.response ? request.response.message : error.message;
-    alertType('danger');
-    alertMessage(message);
-    showAlert(true);
-  }
-
-  function signInUser(userData) {
-    setUser(userData);
-    localStorage.setItem('user', JSON.stringify(userData));
-  }
-
-  function validateForm({ schema, values }) {
-    showAlert(false);
-    const { error } = schema.validate(values);
-
-    function throwErrorIfIsNotValid(customErrorMessage) {
-      if (error !== undefined) {
-        throw new Error(customErrorMessage || error.message);
-      }
-    }
-
-    return {
-      isValid: error === undefined,
-      errorMessage: error ? error.message : '',
-      throwErrorIfIsNotValid,
-    };
-  }
-
-  function authenticate({ type, request }) {
-    return api.post(`/${type}`, request);
-  }
-
-  async function authFormSubmit(event, { formSchema, formValues, authType }, callback) {
-    event.preventDefault();
-    try {
-      validateForm({ schema: formSchema, values: formValues }).throwErrorIfIsNotValid();
-      const response = await authenticate({ type: authType, request: formValues });
-      const { name, email, role, token } = response.data;
-      signInUser({ name, email, role, token });
-      if (callback) callback();
-    } catch (error) {
-      throwAuthFailedAlert(error);
-    }
-  }
+    authFormSubmit,
+    validateForm,
+  } = useAuth();
 
   return (
     <AuthContext.Provider

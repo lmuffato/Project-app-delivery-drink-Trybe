427ea51ef5df123309217c25a0785487d82ab92b
Joao Nasc <oi@joaonasc.dev>
2021-11-19 16:19:11 -0300

    feat: valida jwt para proteger rotas

--- front-end/src/Routes/ProtectedRoute.jsx
+++ front-end/src/Routes/ProtectedRoute.jsx
@@ -1,14 +1,34 @@
 import PropTypes from 'prop-types';
-import React, { useContext } from 'react';
+import jwt from 'jsonwebtoken';
+import React, { useContext, useEffect, useState } from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import { AuthContext } from '../contexts/auth';
 
 export default function ProtectedRoute({ children, ...rest }) {
-  const { user } = useContext(AuthContext);
+  const { user, logOut } = useContext(AuthContext);
+  const [tokenValidated, setTokenValidated] = useState(false);
+  const [tokenValid, setTokenValid] = useState(false);
+
+  useEffect(() => {
+    if (user) {
+      try {
+        if (user.token) {
+          const validJWT = jwt.verify(user.token, process.env.REACT_APP_JWT_SECRET_KEY);
+          setTokenValid(validJWT !== undefined);
+        }
+      } catch (error) {
+        logOut();
+      }
+      setTokenValidated(true);
+    }
+  }, [user, logOut]);
+
+  if (!tokenValidated) return <h1>Carregando...</h1>;
+
   return (
     <Route
       { ...rest }
-      render={ () => (user.token ? children : <Redirect to="/login" />) }
+      render={ () => (tokenValid ? children : <Redirect to="/login" />) }
     />
   );
 }

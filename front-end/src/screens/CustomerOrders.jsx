ebfb5d217bbf75d247f293b55ecc6442d8f33f9a
rogerio-lambert <rogerio.cl@gmail.com>
2021-11-18 21:36:59 -0300

    feat(customerOrder): add first background structure

--- front-end/src/screens/CustomerOrders.jsx
+++ front-end/src/screens/CustomerOrders.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
+import { verifyUserExistance } from '../utils/LocalStorageFunctions';
+import NavBar from '../components/NavBar';
+import getUrlServer from '../utils/getServerUrl';
+
+require('dotenv').config();
+console.log(process.env.URL_SERVER);
 
 function CustomerOrders() {
+  const [clientOrders, setClientOrders] = useState([]);
+  const user = verifyUserExistance();
+  const history = useHistory();
+  if (!user) history.push('/login');
+  const { token, id } = user;
+  useEffect(() => {
+    const getClientOrders = async (userId, userToken) => {
+      const headers = { Authorization: userToken };
+      const orders = await axios.get(`${getUrlServer()}/sales/${userId}`, { headers });
+      return orders;
+    };
+    setClientOrders(getClientOrders(id, token));
+  }, []);
   return (
     <div>
-      CustomerOrders
+      <NavBar />
+
     </div>
   );
 }

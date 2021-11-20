f8f5326568e9f5f1b64d20026865db72e2c47ed2
giovanni-maldonado <giovannitadeu08@gmail.com>
2021-11-20 12:39:22 -0300

    feat: rotas do get, post e delete na app.js

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const userController = require('./controllers/userController');
 const loginvalidationMid = require('./middlewares/loginValidationMid');
 const registerValidationMid = require('./middlewares/registerValidationMid');
+const adminValidationMid = require('./middlewares/adminValidationMid');
 
 const app = express();
 app.use(express.json());
@@ -15,5 +16,10 @@ app.get('/coffee', (_req, res) => res.status(418).end());
 
 app.post('/login', loginvalidationMid, userController.login);
 app.post('/register', registerValidationMid, userController.create);
+
+app.route('/admin')
+  .get(adminValidationMid, userController.getAllUsers)
+  .post(adminValidationMid, userController.create)
+  .delete(adminValidationMid, userController.deleteUser);
 
 module.exports = app;

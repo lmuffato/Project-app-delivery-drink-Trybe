0e8b19a460a9dc2ddd73e646206f19c84813370c
giovanni-maldonado <giovannitadeu08@gmail.com>
2021-11-20 14:17:13 -0300

    feat: validação dos campos e inserção de usuário

--- front-end/src/pages/Admin.jsx
+++ front-end/src/pages/Admin.jsx
@@ -1,6 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+
+const axios = require('axios');
 
 export default function Admin() {
+  const [userName, setUserName] = useState('');
+  const [userEmail, setUserEmail] = useState('');
+  const [userPassword, setUserPassword] = useState('');
+  const [userRole, setUserRole] = useState('seller');
+  const [disabled, setDisabled] = useState(true);
+  const url = 'http://localhost:3001';
+
+  useEffect(() => {
+    const verifyInfo = () => {
+      const regex = /\S+@\S+\.\S+/;
+      const minimumNameLength = 12;
+      const minimumPasswordLength = 6;
+      if (userName.length >= minimumNameLength
+        && userPassword.length >= minimumPasswordLength
+        && regex.test(userEmail)
+        && userRole) {
+        setDisabled(false);
+      } else {
+        setDisabled(true);
+      }
+    };
+    verifyInfo();
+  }, [userEmail, userName.length, userPassword.length, userRole]);
+
+  const insertUser = async () => {
+    const token = JSON.parse(localStorage.getItem('user').token);
+
+    const config = {
+      headers: { Authorization: `Bearer ${token}` },
+    };
+    try {
+      const bodyParameters = {
+        name: userName,
+        email: userEmail,
+        password: userPassword,
+        role: userRole,
+      };
+      await axios
+        .post(`${url}/admin`, bodyParameters, config)
+        .then((res) => res.json());
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <form>
       <fieldset>
@@ -10,6 +57,7 @@ export default function Admin() {
             type="text"
             id="new-user"
             data-testid="admin_manage__input-name"
+            onChange={ (e) => setUserName(e.target.value) }
           />
         </label>
         <label htmlFor="new-email">
@@ -18,6 +66,7 @@ export default function Admin() {
             type="email"
             id="new-email"
             data-testid="admin_manage__input-email"
+            onChange={ (e) => setUserEmail(e.target.value) }
           />
         </label>
         <label htmlFor="new-password">
@@ -26,6 +75,7 @@ export default function Admin() {
             type="password"
             id="new-password"
             data-testid="admin_manage__input-password"
+            onChange={ (e) => setUserPassword(e.target.value) }
           />
         </label>
         <label htmlFor="new-user-type">
@@ -33,16 +83,23 @@ export default function Admin() {
           <select
             id="new-user-type"
             data-testid="admin_manage__select-role"
+            defaultValue={ userRole }
+            onChange={ (e) => setUserRole(e.target.value) }
           >
-            <option value="">Vendedor</option>
-            <option value="">Administrador</option>
-            <option value="">Cliente</option>
+            <option value="seller">Vendedor</option>
+            <option value="administrator">Administrador</option>
+            <option value="customer">Cliente</option>
           </select>
         </label>
-        <button type="submit" data-testid="admin_manage__button-register">
+        <button
+          type="submit"
+          data-testid="admin_manage__button-register"
+          disabled={ disabled }
+          onClick={ insertUser }
+        >
           Cadastrar
         </button>
       </fieldset>
     </form>
-  )
+  );
 }

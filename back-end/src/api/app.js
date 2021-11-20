750ab9c8688649ed71fd4ecd138beb65f22c6429
AndreBarroso <andre_barroso17@hotmail.com>
2021-11-20 20:52:49 -0300

    feat: cria endpoint para renderizar imagens do backend

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -2,11 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+const path = require('path');
+
 const app = express();
 
 app.use(cors());
 
 app.use(bodyParser.json());
+
+app.use('/images', express.static(path.join(__dirname, '..', 'uploads')));
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 

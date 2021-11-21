613702e65d15759183e4ca5ef9f936684df15679
Orlando Flores <orlando_flores@id.uff.br>
2021-11-20 21:33:43 -0300

    fix: default date value in sales model

--- back-end/src/database/migrations/20211109212703-create-sales.js
+++ back-end/src/database/migrations/20211109212703-create-sales.js
@@ -29,7 +29,6 @@ module.exports = {
       saleDate: {
         allowNull: false,
         type: Sequelize.DATE,
-        defaultValue: new Date(),
         field: 'sale_date',
       },
       status: {

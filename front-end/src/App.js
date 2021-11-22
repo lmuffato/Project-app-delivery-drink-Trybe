c1a82a1738573857d3cf630c3340dbbe0f716d3f
Luciano Amâncio <madruga665@hotmail.com>
2021-11-22 10:06:11 -0300

    feat: add pagina de detalhes do pedido

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,16 +4,21 @@ import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import ProductPage from './pages/ProductPage';
 import { PricesProvider } from './context/productsProvider';
-import './App.css';
 import MyRequestsPage from './pages/MyRequestsPage';
+import ClientOrderDetails from './pages/ClientOrderDetails';
+import './App.css';
 
 function App() {
   const userStorage = localStorage.getItem('user');
   const keepUserLoggedIn = userStorage ? '/customer/products' : '/login';
   return (
     <Routes>
-      <Route exact path="/login" element={ <LoginPage /> } />
       <Route path="/" element={ <Navigate replace to={ keepUserLoggedIn } /> } />
+      <Route path="/login" element={ <Navigate replace to={ keepUserLoggedIn } /> } />
+      <Route path="/register" element={ <Navigate replace to={ keepUserLoggedIn } /> } />
+
+      <Route exact path="/" element={ <Navigate replace to="/login" /> } />
+      <Route path="/login" element={ <LoginPage /> } />
       <Route path="/register" element={ <RegisterPage /> } />
       <Route
         path="/customer/products"
@@ -24,6 +29,7 @@ function App() {
         }
       />
       <Route path="/customer/orders" element={ <MyRequestsPage /> } />
+      <Route path="/customer/orders/:id" element={ <ClientOrderDetails /> } />
     </Routes>
   );
 }

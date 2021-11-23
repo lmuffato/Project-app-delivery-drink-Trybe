6f2c9ef303d013b7b4cd5255ead96c3dab7a5105
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-11-23 16:15:41 -0300

    fix lint

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,8 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const middlewares = require('../middlewares');
 const { useRoutes, sellerRoutes } = require('../routes');
-require('dotenv').config();
 
 const app = express();
 
@@ -11,11 +11,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use('/user', useRoutes);
+app.use('/seller', sellerRoutes);
 app.get('/coffee', (_req, res) => res.status(418).end());
 
 app.use(middlewares.routeNotFound);
 
 app.use(middlewares.errorMiddleware);
-// app.use('/seller', sellerRoutes);
 
 module.exports = app;

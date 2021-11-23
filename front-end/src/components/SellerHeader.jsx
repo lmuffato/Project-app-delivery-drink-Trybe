2f4229ee5f5e27c7a85c1eaccefdaf79a68cc021
Felipe Muller <felipeavmuller@gmail.com>
2021-11-23 18:51:19 -0300

    Tela seller/orders

---
+++ front-end/src/components/SellerHeader.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function SellerHeader() {
+  return (
+    <header>
+      <Link to="/customer/products">
+        <button
+          data-testid="customer_products__element-navbar-link-orders"
+          type="button"
+        >
+          PEDIDOS
+        </button>
+      </Link>
+      <Link to="/perfil">
+        <button
+          data-testid="customer_products__element-navbar-user-full-name"
+          type="button"
+        >
+          {JSON.parse(localStorage.getItem('user')).name}
+        </button>
+      </Link>
+      <Link to="/login">
+        <button
+          data-testid="customer_products__element-navbar-link-logout"
+          type="button"
+          onClick={ () => localStorage.removeItem('user') }
+        >
+          SAIR
+        </button>
+      </Link>
+    </header>
+  );
+}
+
+export default SellerHeader;

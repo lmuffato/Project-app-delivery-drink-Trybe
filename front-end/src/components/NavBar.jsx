fb970ab469491c61e4f00582c740fe0103fe00f6
Joao Nasc <oi@joaonasc.dev>
2021-11-23 16:51:37 -0300

    fix: links de navbar

--- front-end/src/components/NavBar.jsx
+++ front-end/src/components/NavBar.jsx
@@ -11,10 +11,16 @@ export default function NavBar() {
     <header className={ styles.nav }>
       <img src="/logo.svg" alt="tchau problema" />
       <nav>
-        <Link to="/" data-testid="customer_products__element-navbar-link-products">
+        <Link
+          to="/customer/products"
+          data-testid="customer_products__element-navbar-link-products"
+        >
           Produtos
         </Link>
-        <Link to="/" data-testid="customer_products__element-navbar-link-orders">
+        <Link
+          to="/customer/orders"
+          data-testid="customer_products__element-navbar-link-orders"
+        >
           Meus pedidos
         </Link>
       </nav>

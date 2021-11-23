6d8d050778299c82bad26c829dbc8af9f9839524
igson <igson660@gmail.com>
2021-11-23 15:51:08 -0500

    inicia req 13

--- front-end/src/services/requests.js
+++ front-end/src/services/requests.js
@@ -1,12 +1,37 @@
+import { parseJwt } from '../utils/utils'
+
 async function postUser(userData, rota) {
   const response = await fetch(`http://localhost:3001/${rota}`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(userData),
   });
-  const data = await response.json();
+  const token = await response.json();
+
+  const { dataValues: { name, email, role } } = parseJwt(data);
+
+  const data =  {
+    token,
+    name,
+    email,
+    role,
+  }
 
   return { data, status: response.status };
 }
 
 export default postUser;
+
+// localStorage.setItem('token', response.token);
+// JSON.parse(localStorage.getItem('ranking'));
+// localStorage.setItem('ranking', JSON.stringify(rankingOnstorage));
+
+// const parseJwt = (token) => {
+//   try {
+//     return JSON.parse(atob(token.split('.')[1]));
+//   } catch (e) {
+//     return null;
+//   }
+// };
+
+// console.log(parseJwt(token))
--- front-end/src/utils/utils.js
+++ front-end/src/utils/utils.js
@@ -1,6 +1,9 @@
-export const handleInput = ({ target }, data, setData) => {
-  const { name, value } = target;
-  setData({ ...data, [name]: value });
-};
-
 export const goRoute = (route, history) => history.push(route);
+
+export const parseJwt = (token) => {
+  try {
+    return JSON.parse(atob(token.split('.')[1]));
+  } catch (e) {
+    return null;
+  }
+};

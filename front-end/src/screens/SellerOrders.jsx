6a28ddb56c5787b89a8dc12eb6044ac6ba933c64
mallmann02 <leomallmann020302@gmail.com>
2021-11-23 20:12:34 -0300

    feat(seller): create orders screen

--- front-end/src/screens/SellerOrders.jsx
+++ front-end/src/screens/SellerOrders.jsx
@@ -1,9 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Grid } from '@mui/material';
+import NavBar from '../components/NavBar';
+import SellerViewOrderCard from '../components/SellerViewOrderCard';
 
 function SellerOrders() {
+  const gridStyle = {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    gap: 5,
+    marginTop: 5,
+    marginLeft: 5,
+  };
+
+  const [sales, setSales] = useState();
+  const apiBaseUrl = 'http://localhost:3001';
+
+  useEffect(() => {
+    async function getSales() {
+      const { data } = await axios.get(`${apiBaseUrl}/sales`);
+      setSales(data);
+    }
+    getSales();
+  }, []);
+
   return (
     <div>
-      SellerOrders
+      <NavBar sellerView />
+
+      { sales && (
+        <Grid sx={ gridStyle }>
+          { sales.map((sale, index) => {
+            const {
+              id,
+              status,
+              totalPrice,
+              deliveryAddress,
+              deliveryNumber,
+              saleDate,
+            } = sale;
+
+            const saleProps = {
+              id,
+              status,
+              totalPrice,
+              deliveryAddress,
+              deliveryNumber,
+              saleDate,
+            };
+
+            return <SellerViewOrderCard { ...saleProps } key={ index } />;
+          }) }
+        </Grid>) }
     </div>
   );
 }

17fba78ae3de73dd9bc8eeba93751792981b42f8
Lucas Lara <lucasml@live.com>
2021-11-23 18:11:03 -0300

    Option value ok

--- front-end/src/pages/CheckoutComprador.jsx
+++ front-end/src/pages/CheckoutComprador.jsx
@@ -8,17 +8,33 @@ export default function CheckoutComprador() {
   const { aux, total } = useContext(CheckoutContext);
   const userData = localStorage.getItem('user');
   const [seller, setSeller] = useState([]);
+  const [address, setAddress] = useState('');
+  const [chooseSeller, setChooseSeller] = useState('Fulana Pereira');
+  const [addressNumber, setAddressNumber] = useState('');
   const userName = JSON.parse(userData);
 
   async function getSellerId(user) {
     const sellerIncome = await getSeler(user);
     setSeller(sellerIncome);
-    setLoading(false);
+  }
+  console.log(chooseSeller);
+
+  function handleSeller({ target: { value } }) {
+    setChooseSeller(value);
+  }
+
+  function handleAddress({ target: { value } }) {
+    setAddress(value);
+  }
+
+  function handleNumber({ target: { value } }) {
+    setAddressNumber(value);
   }
 
   useEffect(() => {
     getSellerId(userName.token);
-  }, [userName.token]);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <div>
       <Header title="Produtos" subtitle="Meus Pedidos" name={ userName.name } />
@@ -37,11 +53,27 @@ export default function CheckoutComprador() {
       <h3 data-testid="customer_checkout__element-order-total-price">
         {`TOTAL R$ ${total}`}
       </h3>
-      <select>
-        {seller.map((sellerid) => (
-          <option key={ sellerid.index }>{sellerid.name}</option>
-        ))}
-      </select>
+      <form>
+        <select onChange={ handleSeller } value={ chooseSeller }>
+          {seller.map((sellerid) => (
+            <option key={ sellerid.index } value={ sellerid.name }>
+              {sellerid.name}
+            </option>
+          ))}
+        </select>
+        <input
+          type="text"
+          placeholder="Digite seu Endereço"
+          onChange={ handleAddress }
+          value={ address }
+        />
+        <input
+          type="text"
+          placeholder="numero"
+          onChange={ handleNumber }
+          value={ addressNumber }
+        />
+      </form>
     </div>
   );
 }

bae545e7539ef3eea685bcfc7339774236677e88
jonathan <jonathanfernandesjf98@gmail.com>
2021-11-23 15:57:25 -0300

    feature: adicao de uma renderizacao condicional associada a rota de administrador

--- front-end/src/components/NavBar.jsx
+++ front-end/src/components/NavBar.jsx
@@ -5,8 +5,35 @@ import { FaSignOutAlt as SignOutIcon } from 'react-icons/fa';
 import styles from '../styles/components/NavBar.module.scss';
 import { AuthContext } from '../contexts/auth';
 
-export default function NavBar() {
+export default function NavBar(props) {
+  const { location } = props;
   const { user, logOut } = useContext(AuthContext);
+  console.log(location);
+  if (location && location.pathname === '/admin/manage') {
+    return (
+      <header className={ styles.nav }>
+        <img src="/logo.svg" alt="tchau problema" />
+        <nav>
+          <Link to="/" data-testid="customer_products__element-navbar-link-orders">
+            Gerenciar Usuários
+          </Link>
+        </nav>
+        <div>
+          <strong data-testid="customer_products__element-navbar-user-full-name">
+            {user.name}
+          </strong>
+          <button
+            onClick={ logOut }
+            type="button"
+            data-testid="customer_products__element-navbar-link-logout"
+          >
+            <SignOutIcon />
+            Sair
+          </button>
+        </div>
+      </header>
+    );
+  }
   return (
     <header className={ styles.nav }>
       <img src="/logo.svg" alt="tchau problema" />

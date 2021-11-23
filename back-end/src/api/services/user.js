266a33d374b27803b4159afb2a8f4e12ed3b3220
Rafael Medeiros <62731714+RafaelMedeirosGomes@users.noreply.github.com>
2021-11-23 15:35:14 -0300

    fix: :bug: ajustando a rota login para retornar o ID do usuario

--- back-end/src/api/services/user.js
+++ back-end/src/api/services/user.js
@@ -4,20 +4,25 @@ const md5 = require('md5');
 const { user: userModel } = require('../../database/models');
 const { getSecretKey } = require('../../utils/parseToken');
 
+const generateToken = (object) => {
+  const JWT_SECRET = getSecretKey();
+  return jwt.sign(object, JWT_SECRET, { expiresIn: '24h' });
+};
+
 exports.findAll = async () => {
   const users = await userModel.findAll({});
   return users;
 };
 
 exports.login = async ({ email, password }) => {
-  const JWT_SECRET = getSecretKey();
   const hashedPassword = md5(password);
   const user = await userModel.findOne(
     { where: { email, password: hashedPassword } },
   );
   if (user) {
   return {
-    token: jwt.sign({ email }, JWT_SECRET, { expiresIn: '12h' }),
+    token: generateToken({ email }),
+    id: user.id,
     name: user.name,
     email: user.email,
     role: user.role,
@@ -28,9 +33,8 @@ exports.login = async ({ email, password }) => {
 
 exports.create = async ({ fullName: name, email, password }) => {
   const hashedPassword = md5(password);
-  const JWT_SECRET = getSecretKey();
   const user = await userModel.findOne({ where: [{ name }, { email }] });
   if (user) return null;
   await userModel.create({ name, email, password: hashedPassword, role: 'customer' });
-  return jwt.sign({ email }, JWT_SECRET, { expiresIn: '12h' });
+  return generateToken({ email });
 };

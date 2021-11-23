c1333cfd1435e92879114d379b35a737c353481e
Gustavo Sousa <gustavohl98@gmail.com>
2021-11-23 17:03:24 -0300

    Initial implementation of order details page

---
+++ front-end/src/pages/OrderDetails.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavBar from '../components/NavBar';
+
+function OrderDetails() {
+  return (
+    <>
+      <NavBar />
+      Detalhes do Pedido
+    </>
+  );
+}
+
+export default OrderDetails;

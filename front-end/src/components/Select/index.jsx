4a2c959f784c1c96e094b436d44e34226165df55
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-11-23 15:55:43 -0300

    adiciona Select para os formulários

---
+++ front-end/src/components/Select/index.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Select = ({ id, name, onChange, value, options, ...others }) => (
+  <select
+    id={ id }
+    name={ name }
+    value={ value }
+    onChange={ onChange }
+    { ...others }
+  >
+    {
+      options.map((option, index) => (
+        <option
+          key={ `${option.name}_${index}` }
+          value={ option.value }
+        >
+          { option.name }
+        </option>))
+    }
+  </select>
+);
+
+Select.propTypes = {
+  name: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired,
+  options: PropTypes.arrayOf(PropTypes.shape(
+    { name: PropTypes.string, value: PropTypes.string },
+  )).isRequired,
+};
+
+export default Select;

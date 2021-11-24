9c0542bde3656b9c7c2a9c76dc8f4838777807bc
Marcus Hüsemann <mchusemann@gmail.com>
2021-11-24 18:05:25 -0300

    fix: remove Cart Provider

--- front-end/src/pages/Products.jsx
+++ front-end/src/pages/Products.jsx
@@ -2,16 +2,13 @@ import React from 'react';
 import NavBar from '../components/NavBar';
 import ProductsList from '../components/ProductsList';
 import { ProductsProvider } from '../contexts/Products';
-import { CartProvider } from '../contexts/Cart';
 
 function Products() {
   return (
     <>
       <NavBar />
       <ProductsProvider>
-        <CartProvider>
-          <ProductsList />
-        </CartProvider>
+        <ProductsList />
       </ProductsProvider>
     </>
   );

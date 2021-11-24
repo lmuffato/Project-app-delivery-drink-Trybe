15771672c348e040cbc8f7aa3e20057093804b94
iago <iago.pferreiravr@gmail.com>
2021-11-24 16:31:26 -0300

    feat(frontend-checkout): Criando a tela de checkout

--- front-end/src/pages/Checkout.jsx
+++ front-end/src/pages/Checkout.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import ButtonTotal from '../components/ButtonTotal';
+import NavBar from '../components/NavBar';
+
+const testIds = {
+  productId: 'customer_products__element-navbar-link-products',
+  pageId: 'customer_products__element-navbar-link-orders',
+  userId: 'customer_products__element-navbar-user-full-name',
+};
+
+const navegationNames = {
+  pageName: 'Meus pedidos',
+};
 
 function Checkout() {
   return (
-    <h1>Checkout</h1>
+    <>
+      <NavBar ids={ testIds } names={ navegationNames } />
+      <p>Checkout</p>
+      <ButtonTotal
+        buttonId="customer_checkout__element-order-total-price"
+        isDisabled={ false }
+      />
+    </>
   );
 }
 

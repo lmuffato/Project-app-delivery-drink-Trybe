302a37865645d468b8652c68dc59d51eaa58fe42
Bruno Mendes <mendessbruno@gmail.com>
2021-11-23 21:16:15 -0300

    fix: gambiarra para usar dados em camelCase

--- back-end/src/api/services/salesService.js
+++ back-end/src/api/services/salesService.js
@@ -1,31 +1,31 @@
-const { sales, sales_products } = require('../../database/models');
+const { sales, salesProducts } = require('../../database/models');
 require('dotenv').config();
+
+const USERID = 'user_id';
+const SELLERID = 'seller_id';
+const TOTALPRICE = 'total_price';
+const DELIVERYADDRESS = 'delivery_address';
+const DELIVERYNUMBER = 'delivery_number';
+const SALEDATE = 'sale_date';
+const SALEID = 'sale_id';
+const PRODUCTID = 'product_id';
 
 const create = async ({ userId, sellerId, totalPrice, deliveryAddress,
   deliveryNumber, productList }) => {
   const saleId = await sales.create({ 
-    // eslint-disable-next-line camelcase
-    user_id: userId,
-    // eslint-disable-next-line camelcase
-    seller_id: sellerId,
-    // eslint-disable-next-line camelcase
-    total_price: totalPrice,
-    // eslint-disable-next-line camelcase
-    delivery_address: deliveryAddress,
-    // eslint-disable-next-line camelcase
-    delivery_number: deliveryNumber,
-    // eslint-disable-next-line camelcase
-    sale_date: Date.now(),
+    [USERID]: userId,
+    [SELLERID]: sellerId,
+    [TOTALPRICE]: totalPrice,
+    [DELIVERYADDRESS]: deliveryAddress,
+    [DELIVERYNUMBER]: deliveryNumber,
+    [SALEDATE]: Date.now(),
     status: 'Pendente',
    });
 
    productList.forEach(async (product) => {
-    // eslint-disable-next-line camelcase
-    await sales_products.create({ 
-      // eslint-disable-next-line camelcase
-      sale_id: saleId.id,
-      // eslint-disable-next-line camelcase
-      product_id: product.id,
+    await salesProducts.create({ 
+      [SALEID]: saleId.id,
+      [PRODUCTID]: product.id,
       quantity: product.qtd,
     });
   });

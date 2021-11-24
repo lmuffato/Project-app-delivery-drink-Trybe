28e006667081d61b95a470aa4535ed2b50b600fb
Orlando Flores <orlando_flores@id.uff.br>
2021-11-23 23:53:50 -0300

    feat: renders OrderDetails and SellerOrderTable and NavBarAdmin in SellerOrderDetails

--- front-end/src/screens/SellerOrdersDetails.jsx
+++ front-end/src/screens/SellerOrdersDetails.jsx
@@ -1,10 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import NavBarAdmin from '../components/NavBarAdmin';
+import OrderDetails from '../components/OrderDetails';
+import SellerOrderTable from '../components/SellerOrderTable';
+
+const urlBase = 'http://localhost:3001';
+const axios = require('axios').default;
 
 function SellerOrdersDetails() {
+  const { id } = useParams();
+  const [isLoading, setIsLoading] = useState(false);
+  const [saleInfo, setSaleInfo] = useState([]);
+  const [productsQuantitiesInfo, setProductsQuantitiesInfo] = useState([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    const getSale = async () => {
+      try {
+        const {
+          data:
+          { sale, productsQuantities } } = await axios.get(`${urlBase}/sales/${id}`);
+        setSaleInfo(sale);
+        setProductsQuantitiesInfo(productsQuantities);
+      } catch (e) {
+        console.log(e.response);
+      }
+    };
+    getSale();
+    setIsLoading(false);
+  }, [id]);
   return (
-    <div>
-      SellerOrdersDetails
-    </div>
+    <>
+      <NavBarAdmin />
+      {!isLoading && (
+        <>
+          <OrderDetails data={ saleInfo } />
+          <SellerOrderTable
+            sale={ saleInfo }
+            productsQuantities={ productsQuantitiesInfo }
+          />
+        </>)}
+    </>
   );
 }
 

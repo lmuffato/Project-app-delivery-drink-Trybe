271cfd0b790df6ac709c38a8cb6b1d538b6065e7
Bruno Mendes <mendessbruno@gmail.com>
2021-11-24 16:06:02 -0300

    criado end-point para recuperar produto pelo id

--- back-end/src/api/controllers/productController.js
+++ back-end/src/api/controllers/productController.js
@@ -15,6 +15,19 @@ const getAll = async (req, res) => {
   }
 };
 
+const getById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const response = await productService.getById(id);
+    return res.status(HTTP_OK_STATUS).json(response);
+  } catch (error) {
+    return res.status(HTTP_ERROR_STATUS).json({
+      message: error,
+  });
+  }
+};
+
 module.exports = {
   getAll,
+  getById,
 };

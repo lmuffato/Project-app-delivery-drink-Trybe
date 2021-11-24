692e27699c6efe28b4cfcef7adf26e078db64e87
rafael <rafittu@outlook.com>
2021-11-24 17:17:34 -0300

    feat: remove role verification

--- back-end/src/schemas/userRegister.js
+++ back-end/src/schemas/userRegister.js
@@ -3,6 +3,5 @@ const { body } = require('express-validator');
 module.exports = [
   body('name').isString().exists(),
   body('email').isEmail().exists(),
-  body('role').isString().exists(),
   body('password').isString().exists(),
 ];

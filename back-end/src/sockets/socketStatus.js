0c11acb6fe46bad5c9e247d82125145a0e191c45
rogerio-lambert <rogerio.cl@gmail.com>
2021-11-23 22:01:48 -0300

    feat(socketStatus backend): add function to update status

--- back-end/src/sockets/socketStatus.js
+++ back-end/src/sockets/socketStatus.js
@@ -1,7 +1,11 @@
+const { Sale } = require('../database/models');
+
 module.exports = (io) => {
   io.on('connection', (socket) => {
-    socket.on('changeStus', async ({ _newStatus, _id }) => {
-
+    socket.on('changeStatus', async ({ newStatus, id }) => {
+      await Sale.update({ status: newStatus }, { where: { id } })
+        .then(() => io.emit('changeStatus', { newStatus, id }))
+        .catch((error) => console.log(error.message));
     });
   });  
 };

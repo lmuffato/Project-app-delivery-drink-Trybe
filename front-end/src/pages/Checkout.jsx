583cc3c65a15ad87b18b9d2ca565ab07151442af
Marcus Hüsemann <mchusemann@gmail.com>
2021-11-24 17:59:20 -0300

    feat: cria tabela com dados de checkout

--- front-end/src/pages/Checkout.jsx
+++ front-end/src/pages/Checkout.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import CheckoutTable from '../components/CheckoutTable';
 
 function Checkout() {
+  const testIds = {
+    productId: 'customer_checkout__element-order-table-item-number-',
+    productName: 'customer_checkout__element-order-table-name-',
+    productQuantity: 'customer_checkout__element-order-table-quantity-',
+    productUnitPrice: 'customer_checkout__element-order-table-unit-price-',
+    productSubTotal: 'customer_checkout__element-order-table-sub-total-',
+  };
+
   return (
-    <h1>Checkout</h1>
+    <CheckoutTable testIds={ testIds } />
   );
 }
 

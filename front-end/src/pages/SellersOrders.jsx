0476e1df4ce6ffe57afa2db75c56fcda00c4dab0
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-11-24 18:24:47 -0400

    fix: seller order page

--- front-end/src/pages/SellersOrders.jsx
+++ front-end/src/pages/SellersOrders.jsx
@@ -7,6 +7,8 @@ import userContext from '../context/userContext';
 
 import Navbar from '../Components/NavBar';
 
+import '../Styles/SellerOrders.css';
+
 const dataTestid48 = 'seller_orders__element-order-id';
 const dataTestid49 = ' seller_orders__element-delivery-status';
 const dataTestid50 = ' seller_orders__element-order-date';
@@ -20,27 +22,27 @@ export default function SellersOrders() {
   const loadingTag = <h3>Loading ...</h3>;
 
   const renderTags = (sale, index) => (
-    <Link to={ `/seller/orders/${sale.id} ` }>
-      <div key={ index }>
-        <div className="cardContainer">
-          <div data-testid={ `${dataTestid48}-${sale.id}` } className="pedido">
-            { sale.id }
+    <div key={ index } className="unit-card-container">
+      <Link to={ `/seller/orders/${sale.id} ` } className="link">
+        <div className="card">
+          <div data-testid={ `${dataTestid48}-${sale.id}` } className="">
+            Pedido { sale.id }
           </div>
-          <div data-testid={ `${dataTestid49}-${sale.id}` } className="status">
+          <div data-testid={ `${dataTestid49}-${sale.id}` } className="">
             { sale.status }
           </div>
-          <div data-testid={ `${dataTestid50}-${sale.id}` } className="moment">
+          <div data-testid={ `${dataTestid50}-${sale.id}` } className="">
             { sale.saleDate }
           </div>
-          <div data-testid={ `${dataTestid51}-${sale.id}` } className="total-price">
-            { sale.totalPrice }
+          <div data-testid={ `${dataTestid51}-${sale.id}` } className="">
+            R$ { sale.totalPrice }
           </div>
-          <div data-testid={ `${dataTestid52}-${sale.id}` } className="adress">
+          <div data-testid={ `${dataTestid52}-${sale.id}` } className="">
             { `${sale.deliveryAddress}, ${sale.deliveryNumber}` }
           </div>
         </div>
-      </div>
-    </Link>
+      </Link>
+    </div>
   );
 
   useEffect(() => {
@@ -50,14 +52,16 @@ export default function SellersOrders() {
   }, []);
 
   return (
-    <div>
+    <div className="container">
       { console.log(orders) }
       <Navbar />
-      {
-        isLoading
-          ? loadingTag
-          : orders.map((e, index) => renderTags(e, index))
-      }
+      <section className="card-container">
+        {
+          isLoading
+            ? loadingTag
+            : orders.map((e, index) => renderTags(e, index))
+        }
+      </section>
     </div>
   );
 }

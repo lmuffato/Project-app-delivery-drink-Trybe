ed3f52d0a8dd0688eeb8c5896d2c4bff760e66ad
MaxRudim <max.hermsdorff@gmail.com>
2021-11-24 14:16:40 -0300

    feat: :sparkles: add user.test.js

--- back-end/src/tests/login.test.js
+++ back-end/src/tests/login.test.js
@@ -1,80 +1,83 @@
 const chai = require("chai");
 const { expect } = require("chai");
 const faker = require("faker");
-const { stub } = require("sinon");
 const chaiHttp = require("chai-http");
 const app = require("../api/app");
 
 chai.use(chaiHttp);
 
 const mockTrueUser = {
-  email: "teste@testando.com",
-  password: "12345678"
+  email: "zebirita@email.com",
+  password: "$#zebirita#$",
 };
 
 const mockLoginError = {
   email: faker.internet.email(),
-  password: faker.internet.password()
+  password: faker.internet.password(),
 };
 
 const mockLoginNoPassword = {
   email: "teste@testando.com",
-  password: ""
+  password: "",
 };
 
 const mockLoginInvalidPassword = {
   email: "teste@testando.com",
-  password: "123"
+  password: "123",
 };
 
 const requestHelper = async (body) => {
   return chai.request(app).post("/login").send(body);
-}
+};
 
 describe('Testa a rota "/login"', () => {
+  describe("Testa o resultado de requisição de login bem sucedida", () => {
+    it("Retorna o status http 200 - OK caso o usuário exista", async () => {
+      const { status } = await requestHelper(mockTrueUser);
 
-  it("Retorna o status http 200 - OK caso o usuário exista", async () => {
-    const { status } = await requestHelper(mockTrueUser);
+      expect(status).to.be.equals(200);
+    });
 
-    expect(status).to.be.equals(200);
+    it("Retorna um objeto contendo o id, name, email e role do usuário que efetuou login", async () => {
+      const { body } = await requestHelper(mockTrueUser);
+      const ApiReturns = {
+        id: 3,
+        name: "Cliente Zé Birita",
+        email: "zebirita@email.com",
+        role: "customer",
+      };
+      expect(body).include(ApiReturns);
+    });
   });
 
-  it("Retorna um objeto contendo o id, name, email e role do usuário que efetuou login", async () => {
-    const { body } = await requestHelper(mockTrueUser);
-    const ApiReturns = {
-      id: 4,
-      name: 'Testando MySQL',
-      email: 'teste@testando.com',
-      role: 'customer',
-    }
+  describe("Testa o resultado de requisição de login feita por usuário não cadastrado no banco de dados", () => {
+    it("Retorna o status http 404 - NOT FOUND com a message: 'Incorrect username or password'", async () => {
+      const { status, body } = await requestHelper(mockLoginError);
 
-    expect(body).include(ApiReturns);
+      expect(status).to.be.equals(404);
+      expect(body.message).to.be.equals("Incorrect username or password");
+    });
   });
-});
 
-describe("Testa o resultado de requisição de login feita por usuário não cadastrado no banco de dados", () => {
-  it("Retorna o status http 404 - NOT FOUND com a message: 'Incorrect username or password'", async () => {
-    const { status, body } = await requestHelper(mockLoginError);
+  describe("Testa o resultado de requisição de login feita por usuário sem passar a senha", () => {
+    it("Retorna o status http 401 - UNAUTHORIZED com a message: '\"password\" is not allowed to be empty'", async () => {
+      const { status, body } = await requestHelper(mockLoginNoPassword);
 
-    expect(status).to.be.equals(404);
-    expect(body.message).to.be.equals("Incorrect username or password");
+      expect(status).to.be.equals(401);
+      expect(body.message).to.be.equals(
+        '"password" is not allowed to be empty'
+      );
+    });
   });
-});
 
-describe("Testa o resultado de requisição de login feita por usuário sem passar a senha", () => {
-  it("Retorna o status http 401 - UNAUTHORIZED com a message: '\"password\" is not allowed to be empty'", async () => {
-    const { status, body } = await requestHelper(mockLoginNoPassword);
+  describe("Testa o resultado de requisição de login feita por usuário que passa senha menor de 6 caracteres", () => {
+    it("Retorna o status http 404 - NOT FOUND com a message: '\"password\" length must be at least 6 characters long'", async () => {
+      const { status, body } = await requestHelper(mockLoginInvalidPassword);
 
-    expect(status).to.be.equals(401);
-    expect(body.message).to.be.equals("\"password\" is not allowed to be empty");
-  });
-});
-
-describe("Testa o resultado de requisição de login feita por usuário que passa senha menor de 6 caracteres", () => {
-  it("Retorna o status http 404 - NOT FOUND com a message: '\"password\" length must be at least 6 characters long'", async () => {
-    const { status, body } = await requestHelper(mockLoginInvalidPassword);
-
-    expect(status).to.be.equals(401);
-    expect(body.message).to.be.equals("\"password\" length must be at least 6 characters long");
+      expect(status).to.be.equals(401);
+      expect(body.message).to.be.equals(
+        '"password" length must be at least 6 characters long'
+      );
+    });
   });
 });
---
+++ back-end/src/tests/user.test.js
@@ -0,0 +1,165 @@
+const chai = require("chai");
+const { expect } = require("chai");
+const faker = require("faker");
+const chaiHttp = require("chai-http");
+const app = require("../api/app");
+
+chai.use(chaiHttp);
+
+let token =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwibmFtZSI6IkRlbGl2ZXJ5IEFwcCBBZG1pbiIsImVtYWlsIjoiYWRtQGRlbGl2ZXJ5YXBwLmNvbSIsInJvbGUiOiJhZG1pbmlzdHJhdG9yIiwiaWF0IjoxNjM3Njk2Njg2fQ.F5RblxMueIXmtL13rO2I2So9Tt0PXEJg8Br_UETiliI";
+
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+  const mockNewUser = {
+  name: faker.name.findName(),
+  email: faker.internet.email(),
+  password: faker.internet.password(),
+};
+
+const randomBigNumber = getRandomInt(500, 5000);
+
+const mockEditedUser = {
+  name: `Usuário de teste - Id: ${randomBigNumber}`,
+  email: faker.internet.email(),
+  password: faker.internet.password(),
+  role: 'customer'
+};
+
+const mockEditedInvalidPassword = {
+  name: `Usuário de teste - Id: ${randomBigNumber}`,
+  email: faker.internet.email(),
+  password: '',
+  role: 'customer'
+};
+
+const mockInvalidUser = {
+  name: faker.name.findName(),
+  email: faker.internet.email(),
+  password: "",
+};
+
+const requestPostHelper = async (endpoint, body) => {
+  return chai.request(app).post(endpoint).send(body);
+};
+
+const requestGetHelper = async (endpoint) => {
+  return chai.request(app).get(endpoint).set("Authorization", token);
+};
+
+const requestPutHelper = async (endpoint, body) => {
+  return chai.request(app).put(endpoint).send(body).set("Authorization", token);
+};
+
+const requestDeleteHelper = async (endpoint) => {
+  return chai.request(app).delete(endpoint).set("Authorization", token);
+};
+
+describe('Testa a rota "/users"', () => {
+  describe('Testa o método POST na rota "/users/register"', () => {
+    it("Retorna o status http 201 - CREATED e um objeto contendo o novo usuário adicionado", async () => {
+      const { status, body } = await requestPostHelper(
+        "/users/register",
+        mockNewUser
+      );
+
+      token = body.token;
+
+      const newUser = { ...mockNewUser };
+      delete newUser.password;
+
+      expect(status).to.be.equals(201);
+      expect(body).include(newUser);
+    });
+
+    it('Retorna o status http 406 - NOT ACCEPTABLE e a message: "Password is required" quando o novo usuário a ser cadastrado não posui senha', async () => {
+      const { status, body } = await requestPostHelper(
+        "/users/register",
+        mockInvalidUser
+      );
+
+      const expectedMessage = '"password" is not allowed to be empty';
+
+      expect(status).to.be.equals(400);
+      expect(body.message).include(expectedMessage);
+    });
+  });
+
+  describe('Testa o método GET na rota "/users"', () => {
+    it("Retorna o status http 200 - OK e um array de objetos contendo os dados dos usuários no banco de dados", async () => {
+      const { status, body } = await requestGetHelper("/users");
+
+      expect(status).to.be.equals(200);
+      expect(body).to.be.an("array");
+      expect(body).to.not.be.empty;
+    });
+  });
+
+  describe('Testa o método GET na rota "/users/:id"', () => {
+    it("Retorna o status http 200 - OK contendo um objeto referente ao usuário buscado", async () => {
+      const id = getRandomInt(1, 5);
+      const { status, body } = await requestGetHelper(`/users/${id}`);
+
+      expect(status).to.be.equals(200);
+      expect(id).to.be.equals(body.id);
+      expect(body).to.be.an("object");
+    });
+
+    it('Retorna o status http 404 - NOT FOUND contendo a message ""user" not found" quando o ID passado não constar no banco de dados', async () => {
+      const id = getRandomInt(500, 1000);
+
+      const { status, body } = await requestGetHelper(`/users/${id}`);
+
+      const expectedMessage = '"user" not found';
+
+      expect(status).to.be.equals(404);
+      expect(body.message).to.be.equals(expectedMessage);
+    });
+  });
+
+  describe('Testa o método PUT na rota "/users/:id"', () => {
+    it('Retorna o status http 200 - OK contendo um objeto referente ao usuário editado', async () => {
+      const id = getRandomInt(1, 5);
+
+      const { status } = await requestPutHelper(`/users/${id}`, mockEditedUser);
+
+      expect(status).to.be.equals(200);
+    });
+    it('Retorna o status http 406 - NOT ACCEPTABLE contendo a message: ""password" is not allowed to be empty" quando uma senha não é fornecida', async () => {
+      const id = getRandomInt(1, 5);
+
+      const { status, body } = await requestPutHelper(`/users/${id}`, mockEditedInvalidPassword);
+
+      const expectedMessage = '"password" is not allowed to be empty';
+
+      expect(status).to.be.equals(400);
+      expect(body.message).to.be.equals(expectedMessage);
+    });
+  });
+
+  describe('Testa o método DELETE na rota "/users/:id"', () => {
+    it('Retorna o status http 200 - OK contendo contendo a message: "Usuário deletado com sucesso" ao deletar um usuário', async () => {
+      const id = getRandomInt(1, 5);
+      const { status, body } = await requestDeleteHelper(`/users/${id}`);
+
+      // const expectedMessage = "Usuário deletado com sucesso";
+      console.log(body)
+
+      expect(status).to.be.equals(200);
+      // expect(body.message).to.be.equals(expectedMessage);
+    });
+
+    it('Retorna o status http 404 - NOT FOUND contendo contendo a message: ""user" not found" ao deletar um usuário inexistente', async () => {
+      const { status, body } = await requestDeleteHelper(`/users/${randomBigNumber}`);
+
+      const expectedMessage = '"user" not found';
+
+      expect(status).to.be.equals(404);
+      expect(body.message).to.be.equals(expectedMessage);
+    });
+  });
+});

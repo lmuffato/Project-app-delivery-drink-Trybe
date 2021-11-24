445f76b96d9dbf8da7458e3a9e057e2966065ace
Marcus Hüsemann <mchusemann@gmail.com>
2021-11-24 18:06:48 -0300

    feat: botao de remoçao de itens do checkout

---
+++ front-end/src/components/ButtonRemoveItem.jsx
@@ -0,0 +1,26 @@
+import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
+import { CartContext } from '../contexts/Cart';
+
+function ButtonRemoveItem(props) {
+  const { id } = props;
+  const { cart, setCart } = useContext(CartContext);
+
+  const removeItem = () => {
+    const updatedCart = cart.filter((product) => product.productId !== id);
+    setCart(updatedCart);
+  };
+
+  return (
+    <button type="button" onClick={ removeItem }>Remover</button>
+  );
+}
+
+ButtonRemoveItem.propTypes = {
+  id: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number,
+  ]).isRequired,
+};
+
+export default ButtonRemoveItem;

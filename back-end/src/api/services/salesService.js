26613dc98074972701c1d534f29c7538041a42c0
Bruno Mendes <mendessbruno@gmail.com>
2021-11-23 21:01:59 -0300

    fix: desabilitado lint de camelcase em cada linha

--- back-end/src/api/services/salesService.js
+++ back-end/src/api/services/salesService.js
@@ -1,22 +1,30 @@
-/* eslint-disable camelcase */
 const { sales, sales_products } = require('../../database/models');
 require('dotenv').config();
 
 const create = async ({ userId, sellerId, totalPrice, deliveryAddress,
   deliveryNumber, productList }) => {
   const saleId = await sales.create({ 
+    // eslint-disable-next-line camelcase
     user_id: userId,
+    // eslint-disable-next-line camelcase
     seller_id: sellerId,
+    // eslint-disable-next-line camelcase
     total_price: totalPrice,
+    // eslint-disable-next-line camelcase
     delivery_address: deliveryAddress,
+    // eslint-disable-next-line camelcase
     delivery_number: deliveryNumber,
+    // eslint-disable-next-line camelcase
     sale_date: Date.now(),
     status: 'Pendente',
    });
 
    productList.forEach(async (product) => {
+    // eslint-disable-next-line camelcase
     await sales_products.create({ 
+      // eslint-disable-next-line camelcase
       sale_id: saleId.id,
+      // eslint-disable-next-line camelcase
       product_id: product.id,
       quantity: product.qtd,
     });

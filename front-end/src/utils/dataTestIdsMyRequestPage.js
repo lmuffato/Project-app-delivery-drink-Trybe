dfd1fa8fac6a642962c0b08b20e06b218f1dabfd
Eduardo Costa Maciel <costaeduardomaciel@gmail.com>
2021-11-24 17:10:41 -0300

    Feat: cria os data testids do seller

--- front-end/src/utils/dataTestIdsMyRequestPage.js
+++ front-end/src/utils/dataTestIdsMyRequestPage.js
@@ -1,8 +1,16 @@
-const dataTestids = {
+export const dataTestids = {
   dataTestIdRequest: 'customer_orders__element-order-id-',
   dataTestIdStatus: 'customer_orders__element-delivery-status-id-',
   dataTestIdDate: 'customer_orders__element-order-date-id-',
   dataTestIdPrice: 'customer_orders__element-card-price-',
 };
 
-export default dataTestids;
+export const dataTestisSeller = {
+  dataTestIdRequest: 'seller_orders__element-order-id-',
+  dataTestIdStatus: 'seller_orders__element-delivery-status-',
+  dataTestIdDate: 'seller_orders__element-order-date-',
+  dataTestIdPrice: 'seller_orders__element-card-price-',
+  dataTestIdAddress: 'seller_orders__element-card-address-',
+};
+
+// export default dataTestids;

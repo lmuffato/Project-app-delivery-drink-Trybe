69281dce7b1b51722b12f71c1869fd7b8a37208e
lmuffato <lmiremuffato@gmail.com>
2021-11-24 20:43:14 -0300

    fix lock package

--- front-end/src/pages/OrderDetails.jsx
+++ front-end/src/pages/OrderDetails.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
 import Navbar from '../Components/NavBar';
+import { useParams } from "react-router-dom";
+// import React, { useEffect, useState, useContext } from 'react';
 
-export default function OrderDetails() {
+export default function OrderDetails({ match }) {
+  const { id } = match.params;
+  // const [isLoading, setIsLoading] = useState(false);
+
+  // useEffect(() => {
+  //   setIsLoading(true);
+  //   getSellersList();
+  //   setIsLoading(false);
+  // }, []);
+
+  // const printUrl = () => {
+  //   const params = useParams();
+  //     console.log(params);
+  // };
+
+  console.log(id);
+
   return (
     <div>
+      {/* { printUrl() } */}
       <Navbar />
     </div>
   );

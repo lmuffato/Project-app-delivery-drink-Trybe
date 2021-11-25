855c3eabac4e1909864e136e41d1959e4dca34e8
iago <iago.pferreiravr@gmail.com>
2021-11-25 12:47:39 -0300

    feat(frontend-checkout): Adicionando a lógica de calcular o total no componente  ButtonTotal

--- front-end/src/components/ButtonTotal.jsx
+++ front-end/src/components/ButtonTotal.jsx
@@ -3,7 +3,13 @@ import PropTypes from 'prop-types';
 import { CartContext } from '../contexts/Cart';
 
 function ButtonTotal({ buttonId, isDisabled }) {
-  const { total } = useContext(CartContext);
+  const { cart } = useContext(CartContext);
+
+  const calculateTotal = () => (
+    cart.map((item) => item.subTotal)
+      .reduce((acc, curr) => Number(acc) + Number(curr), 0));
+
+  const total = cart ? calculateTotal() : 0;
   return (
     <button
       data-testid={ buttonId }
@@ -12,7 +18,10 @@ function ButtonTotal({ buttonId, isDisabled }) {
       disabled={ isDisabled }
     >
       <p data-testid="customer_products__checkout-bottom-value">
-        { total.toString().replace('.', ',') }
+        { Number(total).toLocaleString('pt-BR', {
+          currency: 'BRL',
+          minimumFractionDigits: 2,
+        }) }
       </p>
     </button>
   );

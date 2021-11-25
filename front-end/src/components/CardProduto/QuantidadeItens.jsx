2c314fd00c65ad7272e02f9171897d1007b4337d
Micaela Alves <micaela.alves93@gmail.com>
2021-11-25 01:39:37 -0300

    feat - input recebe valor do estado

--- front-end/src/components/CardProduto/QuantidadeItens.jsx
+++ front-end/src/components/CardProduto/QuantidadeItens.jsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { string } from 'prop-types';
+import ProductsContext from '../../context/ProductsContext';
 
 function QuantidadeItens({ data }) {
   const { id } = data;
+  const { values: { productsCart } } = useContext(ProductsContext);
+  const [arrayQuantity] = productsCart.filter((product) => product.id === id);
+  const { quantity } = arrayQuantity || 0;
   return (
     <input
       data-testid={ `customer_products__input-card-quantity-${id}` }
       type="number"
-      defaultValue={ 0 }
+      value={ quantity }
     />
   );
 }

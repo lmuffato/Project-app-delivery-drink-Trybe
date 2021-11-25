d17f4c2072102fd5f1e1a8e12bee3b3af3a76e15
Marcus Hüsemann <mchusemann@gmail.com>
2021-11-25 17:02:20 -0300

    refactor: refatora para seguir o mesmo padrao da tela de Produtos

--- front-end/src/pages/Orders.jsx
+++ front-end/src/pages/Orders.jsx
@@ -1,24 +1,43 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
+import axios from 'axios';
 import OrderCard from '../components/OrderCard';
 import { OrdersContext } from '../contexts/Orders';
-// import NavBar from '../components/NavBar';
+import NavBar from '../components/NavBar';
 
 function Orders() {
-  const orderList = useContext(OrdersContext);
-  console.log(orderList.orders);
-  // console.log(props);
+  const { orderList, setOrderList } = useContext(OrdersContext);
+  const testIds = {
+    orderId: 'customer_orders__element-order-id-',
+    orderStatus: 'customer_orders__element-delivery-status-',
+    orderDate: 'customer_orders__element-order-date-',
+    orderPrice: 'customer_orders__element-card-price-',
+  };
+
+  useEffect(() => {
+    const { token, id } = JSON.parse(localStorage.getItem('user'));
+
+    async function fetchData() {
+      try {
+        const response = await axios.get(`http://localhost:3001/orders/${id}`, {
+          headers: {
+            Authorization: token,
+          },
+        });
+        setOrderList(response.data);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    fetchData();
+  }, []);
 
   if (!orderList.orders) return <h1>Loading...</h1>;
   return (
-    <div>
-      {/* <NavBar /> */}
-      {/* <UserForm />
-      <UsersTable /> */}
-      {
-        orderList.orders
-          .map((order, index) => <OrderCard key={ index } order={ order } />)
-      }
-    </div>
+    <>
+      <NavBar />
+      { orderList.map((order, key) => (
+        <OrderCard key={ key } testIds={ testIds } order={ order } />))}
+    </>
   );
 }
 

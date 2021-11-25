9ec9057c96bec8f715f259075771f902b0d1c277
Marcus Hüsemann <mchusemann@gmail.com>
2021-11-25 18:37:02 -0300

    refacor: remove data-testids e passa para dentro do componente da tabela

--- front-end/src/pages/Checkout.jsx
+++ front-end/src/pages/Checkout.jsx
@@ -2,16 +2,8 @@ import React from 'react';
 import CheckoutTable from '../components/CheckoutTable';
 
 function Checkout() {
-  const testIds = {
-    productId: 'customer_checkout__element-order-table-item-number-',
-    productName: 'customer_checkout__element-order-table-name-',
-    productQuantity: 'customer_checkout__element-order-table-quantity-',
-    productUnitPrice: 'customer_checkout__element-order-table-unit-price-',
-    productSubTotal: 'customer_checkout__element-order-table-sub-total-',
-  };
-
   return (
-    <CheckoutTable testIds={ testIds } />
+    <CheckoutTable />
   );
 }
 

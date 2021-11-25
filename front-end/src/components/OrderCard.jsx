dad3958eaf3ffa76f9c112fa98e1daa01f7c0026
Marcus Hüsemann <mchusemann@gmail.com>
2021-11-25 17:03:16 -0300

    refactor: refatora cards e adiciona navigation para tela de order details

--- front-end/src/components/OrderCard.jsx
+++ front-end/src/components/OrderCard.jsx
@@ -1,44 +1,49 @@
-import { array } from 'prop-types';
 import React from 'react';
-// import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { useNavigate } from 'react-router';
 import './orderCard.css';
 
-const OrderCard = ({ order }) => {
-  const { id, status, saleDate, totalPrice, deliveryAddress, deliveryNumber } = order;
-  // console.log(props.userId);
+const OrderCard = ({ order, testIds }) => {
+  const { id, status, saleDate, totalPrice } = order;
+  const { orderId, orderStatus, orderDate, orderPrice } = testIds;
+  const navigate = useNavigate();
+
+  const navigateHandler = () => navigate(`/customer/orders/${id}`);
 
   return (
-    <div className="order-container">
-      {/* <Link
-        to={ `/${}/orders/${id}` }
-        data-testid={}
-        key={ id }
-      > */}
-      <div className="order-card">
-        <span>
-          {id}
-        </span>
-        <span>
-          <h4>
-            {status}
-          </h4>
-        </span>
-        <span>
-          {saleDate}
-        </span>
-        <span>
-          {totalPrice}
-        </span>
-        <span>{deliveryAddress}</span>
-        <span>{deliveryNumber}</span>
+    <div
+      tabIndex={ id }
+      role="button"
+      className="order-container"
+      onKeyPress={ () => navigateHandler }
+      onClick={ () => navigateHandler }
+    >
+      <div>
+        <p>Pedido</p>
+        <h4 data-testid={ orderId + id }>{ id }</h4>
       </div>
-      {/* </Link> */}
+      <h3 data-testid={ orderStatus + id }>{ status }</h3>
+      <div>
+        <h4 data-testid={ orderDate + id }>{ saleDate }</h4>
+        <h4 data-testid={ orderPrice + id }>{ totalPrice }</h4>
+      </div>
     </div>
   );
 };
 
 OrderCard.propTypes = {
-  orderList: array,
-}.isRequired;
+  order: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    status: PropTypes.string.isRequired,
+    saleDate: PropTypes.string.isRequired,
+    totalPrice: PropTypes.string.isRequired,
+  }).isRequired,
+  testIds: PropTypes.shape({
+    orderId: PropTypes.string.isRequired,
+    orderStatus: PropTypes.string.isRequired,
+    orderDate: PropTypes.string.isRequired,
+    orderPrice: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default OrderCard;

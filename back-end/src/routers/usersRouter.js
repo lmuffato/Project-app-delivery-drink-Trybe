682f631bc156abe70a17122ec861f06c1fe73cb7
mallmann02 <leomallmann020302@gmail.com>
2021-11-25 18:50:41 -0300

    feat(users_list): create admin endpoint

--- back-end/src/routers/usersRouter.js
+++ back-end/src/routers/usersRouter.js
@@ -1,8 +1,14 @@
 const { Router } = require('express');
-const { listUsers } = require('../controllers/usersController');
+const {
+  listUsers,
+  adminList,
+} = require('../controllers/usersController');
+const { validateJWT } = require('../middlewares/validateJwt');
 
 const router = Router();
 
 router.get('/', listUsers);
+
+router.get('/admin', validateJWT,  adminList);
 
 module.exports = router;

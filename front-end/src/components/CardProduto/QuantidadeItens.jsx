93d729caf421689abd703e19082116a28b788905
Micaela Alves <micaela.alves93@gmail.com>
2021-11-25 02:22:09 -0300

    feat - alterar a quantidade pelo input

--- front-end/src/components/CardProduto/QuantidadeItens.jsx
+++ front-end/src/components/CardProduto/QuantidadeItens.jsx
@@ -4,14 +4,31 @@ import ProductsContext from '../../context/ProductsContext';
 
 function QuantidadeItens({ data }) {
   const { id } = data;
-  const { values: { productsCart } } = useContext(ProductsContext);
+  const { values: { productsCart },
+    actions: { setProductsCart } } = useContext(ProductsContext);
   const [arrayQuantity] = productsCart.filter((product) => product.id === id);
   const { quantity } = arrayQuantity || 0;
+
+  const handleChange = ({ target }, identifier) => {
+    const { value } = target;
+    const productIndex = productsCart.findIndex((item) => item.id === identifier);
+    if (productIndex < 0) {
+      setProductsCart((products) => [...products, { id: identifier, quantity: value }]);
+    } else {
+      const updatedProducts = productsCart.map((element, index) => {
+        if (index === productIndex) element.quantity = parseInt(value, 10);
+        return element;
+      });
+      setProductsCart(updatedProducts);
+    }
+  };
+
   return (
     <input
       data-testid={ `customer_products__input-card-quantity-${id}` }
       type="number"
       value={ quantity }
+      onChange={ (e) => handleChange(e, id) }
     />
   );
 }

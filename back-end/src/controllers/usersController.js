bbea6e4394ce2ee7cca3670e27de7a6897d8d85e
mallmann02 <leomallmann020302@gmail.com>
2021-11-25 18:52:44 -0300

    feat: controller to get users with user admin

--- back-end/src/controllers/usersController.js
+++ back-end/src/controllers/usersController.js
@@ -1,5 +1,6 @@
 const usersService = require('../services/usersService');
 const httpStatus = require('../utils/httpStatus');
+const { User } = require('../database/models');
 
 const listUsers = async (req, res) => {
   const { role } = req.query;
@@ -7,6 +8,22 @@ const listUsers = async (req, res) => {
   return res.status(httpStatus.ok).json(users);
 };
 
+const adminList = async (req, res) => {
+  const { role } = req.user;
+
+  if (role.toLowerCase() !== 'administrator') {
+    return res
+      .status(httpStatus.unauthorized)
+      .json();
+  }
+  const users = await User.findAll(
+    { where: { role: ['seller', 'customer'] } }
+  );
+
+  return res.status(httpStatus.ok).json(users);
+};
+
 module.exports = {
   listUsers,
+  adminList,
 };

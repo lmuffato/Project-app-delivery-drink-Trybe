7a853c271615763e35712f1eb07738ef0508984b
Orlando Flores <orlando_flores@id.uff.br>
2021-11-24 23:29:51 -0300

    refactor: component to match app style

--- front-end/src/components/SellerOrderTable.jsx
+++ front-end/src/components/SellerOrderTable.jsx
@@ -1,154 +1,110 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { DataGrid } from '@mui/x-data-grid';
+import {
+  Box,
+  Typography,
+  Container,
+  Button,
+} from '@mui/material';
+import convertDateFormat from '../utils/convertDateFormat';
+import ProductInSaleCard from './ProductInSaleCard';
 
-function SellerOrderTable({ sale, productsQuantities }) {
-  const rows = productsQuantities.map((item, index) => (
-    {
-      id: index,
-      name: sale.products[index].name,
-      quantity: item,
-      price: sale.products[index].price,
-    }
-  ));
-  const formatValue = (value) => (
-    `${parseFloat(value.toString()).toFixed(2).replace('.', ',')}`
-  );
+const testIdsPrefix = 'seller_order_details__';
 
-  function getSubTotal(params) {
-    const unitPrice = params.getValue(params.id, 'price');
-    const totalQuantity = params.getValue(params.id, 'quantity');
-    return formatValue(unitPrice * totalQuantity);
-  }
+function SellerOrderTable(props) {
+  const {
+    id,
+    seller: { name },
+    products,
+    status,
+    totalPrice,
+    saleDate,
+  } = props;
 
-  const calculateSubtotal = (list) => {
-    const total = list
-      .reduce((tot, curr) => tot + (+(curr.price) * curr.quantity), 0);
-
-    return total.toFixed(2);
+  const typographyBasicStyle = {
+    fontSize: 14,
+    color: '#000',
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: '5px',
   };
 
-  const tableColumns = [
-    {
-      field: 'id',
-      headerName: 'Item',
-      flex: 0.25,
-      type: 'number',
-      renderCell: (params) => (
-        <span
-          data-testid={
-            `seller_order_details__element-order-table-item-number--${params.id}`
-          }
-        >
-          {params.value + 1}
-        </span>),
-    },
-    {
-      field: 'name',
-      headerName: 'Descrição',
-      flex: 1,
-      headerAlign: 'center',
-      renderCell: (params) => (
-        <span
-          data-testid={ `seller_order_details__element-order-table-name-${params.id}` }
-        >
-          {params.value}
-        </span>),
-    },
-    {
-      field: 'quantity',
-      headerName: 'Quantidade',
-      type: 'number',
-      flex: 0.25,
-      renderCell: (params) => (
-        <span
-          data-testid={
-            `seller_order_details__element-order-table-quantity-${params.id}`
-          }
-        >
-          {params.value}
-        </span>),
-    },
-    {
-      field: 'price',
-      headerName: 'Valor Unitário',
-      flex: 0.25,
-      type: 'number',
-      renderCell: (params) => (
-        <span
-          data-testid={
-            `seller_order_details__element-order-table-unit-price-${params.id}`
-          }
-        >
-          {(params.value)}
-        </span>),
-    },
-    {
-      field: 'subtotal',
-      headerName: 'SubTotal',
-      flex: 0.25,
-      valueGetter: getSubTotal,
-      renderCell: (params) => (
-        <span
-          data-testid={
-            `seller_order_details__element-order-table-sub-total-${params.id}`
-          }
-        >
-          {params.value}
-        </span>),
-    },
-  ];
   return (
-    <>
-      <div
-        style={ {
-          width: '80%',
-          margin: 'auto',
-          backgroundColor: '#fff',
-          borderRadius: 10,
-          height: 400,
+    <Container>
+      <Box
+        sx={ {
+          backgroundColor: '#E5E5E5',
+          display: 'flex',
+          flexDirection: 'row',
+          alignItems: 'center',
+          paddingLeft: '15px',
+          justifyContent: 'space-around',
         } }
       >
-        <DataGrid
-          rows={ rows }
-          columns={ tableColumns }
-          pageSize={ 11 }
-          // autoHeight
-          disableSelectionOnClick
-          disableMultipleSelection
-          disableColumnMenu
-          rowHeight={ 35 }
-        />
-      </div>
-      <p>
-        R$
-        <span
-          data-testid="seller_order_details__element-order-total-price"
+        <Typography
+          sx={ typographyBasicStyle }
+          data-testid={ `${testIdsPrefix}element-order-details-label-order-id` }
         >
-          { ` ${calculateSubtotal(rows).toString().replace('.', ',')}` }
-        </span>
-      </p>
-    </>
+          <p>Pedido</p>
+          { id }
+        </Typography>
+        <Typography
+          sx={ typographyBasicStyle }
+          data-testid={ `${testIdsPrefix}element-order-details-label-seller-name` }
+        >
+          <p>P.Vend:</p>
+          { name }
+        </Typography>
+        <Typography
+          sx={ typographyBasicStyle }
+          data-testid={ `${testIdsPrefix}element-order-details-label-order-date` }
+        >
+          { convertDateFormat(saleDate) }
+        </Typography>
+        <Typography
+          sx={ typographyBasicStyle }
+          data-testid={ `${testIdsPrefix}element-order-details-label-delivery-status` }
+        >
+          { status }
+        </Typography>
+        <Button
+          data-testid={ `${testIdsPrefix}button-preparing-check` }
+        >
+          Preparar Pedido
+        </Button>
+        <Button
+          data-testid={ `${testIdsPrefix}button-dispatch-check` }
+          disabled
+        >
+          Saiu para entrega
+        </Button>
+      </Box>
+      <Box>
+        { products
+          .map((product, index) => (
+            <ProductInSaleCard
+              key={ index }
+              { ...product }
+              testIdsPrefix="seller_order_details__"
+            />)) }
+      </Box>
+      <Typography
+        data-testid={ `${testIdsPrefix}element-order-total-price` }
+      >
+        { totalPrice.replace('.', ',') }
+      </Typography>
+    </Container>
   );
 }
 
-export default SellerOrderTable;
-
 SellerOrderTable.propTypes = {
-  sale: PropTypes.shape({
-    id: PropTypes.number,
-    deliveryAddress: PropTypes.string,
-    deliveryNumber: PropTypes.string,
-    saleDate: PropTypes.string,
-    sellerId: PropTypes.number,
-    status: PropTypes.string,
-    totalPrice: PropTypes.string,
-    userId: PropTypes.number,
-    products: PropTypes.arrayOf(PropTypes.shape({
-      id: PropTypes.number,
-      name: PropTypes.string,
-      price: PropTypes.string,
-      urlImage: PropTypes.string,
-    })),
-  }),
-}.isRequired;
+  id: PropTypes.number.isRequired,
+  saleDate: PropTypes.string.isRequired,
+  status: PropTypes.string.isRequired,
+  totalPrice: PropTypes.number.isRequired,
+  seller: PropTypes.shape(PropTypes.string.isRequired).isRequired,
+  products: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default SellerOrderTable;

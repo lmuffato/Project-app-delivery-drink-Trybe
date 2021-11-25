ad37a862c5cc02fb207b66e358bb1b2b2b4da57d
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-11-25 17:04:57 -0300

    add LinkManagerUsers ao componente NavBar

--- front-end/src/components/NavBar/NavBar.jsx
+++ front-end/src/components/NavBar/NavBar.jsx
@@ -4,15 +4,26 @@ import LinkProducts from './LinkProducts';
 import LinkOrders from './LinkOrders';
 import UserFullName from './UserFullName';
 import LinkLogout from './LinkLogout';
+import LinkManagerUsers from './LinkManagerUsers';
 
 function NavBar() {
   const path = useLocation().pathname;
+  console.log(path.includes('admin'));
   return (
     <div>
       { path.includes('customer')
         ? <LinkProducts data-testid="customer_products__element-navbar-link-products" />
+        && <LinkOrders data-testid="customer_products__element-navbar-link-orders" />
         : null}
-      <LinkOrders data-testid="customer_products__element-navbar-link-orders" />
+
+      { path.includes('seller')
+        ? <LinkOrders data-testid="customer_products__element-navbar-link-orders" />
+        : null}
+
+      { path.includes('admin')
+        ? <LinkManagerUsers />
+        : null}
+
       <UserFullName data-testid="customer_products__element-navbar-user-full-name" />
       <LinkLogout data-testid="customer_products__element-navbar-link-logout" />
     </div>

9b079f7bc5f538120bc4a79647835c24400d5431
Micaela Alves <micaela.alves93@gmail.com>
2021-11-25 01:38:25 -0300

    feat - comportamento click addProduct

--- front-end/src/components/CardProduto/AddItem.jsx
+++ front-end/src/components/CardProduto/AddItem.jsx
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { string } from 'prop-types';
+import ProductsContext from '../../context/ProductsContext';
 
 function AddItem({ data }) {
   const { id } = data;
-
-  const addProduct = ({ target }) => {
-    
+  const { values: { productsCart },
+    actions: { setProductsCart } } = useContext(ProductsContext);
+  const addProduct = (identifier) => {
+    const productIndex = productsCart.findIndex((item) => item.id === identifier);
+    if (productIndex < 0) {
+      setProductsCart((products) => [...products, { id: identifier, quantity: 1 }]);
+    } else {
+      const updatedProducts = productsCart.map((element, index) => {
+        if (index === productIndex) element.quantity += 1;
+        return element;
+      });
+      setProductsCart(updatedProducts);
+    }
   };
 
   return (
     <button
       type="button"
       data-testid={ `customer_products__button-card-add-item-${id}` }
-      onClick={ addProduct }
+      onClick={ () => addProduct(id) }
     >
       +
     </button>

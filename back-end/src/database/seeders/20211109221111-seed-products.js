0a9fee20fa1cdd16e18e834adf1e2da48d9a43e6
rogerio-lambert <rogerio.cl@gmail.com>
2021-11-25 16:15:55 -0300

    refactor(seeders): change images extenction to png

--- back-end/src/database/seeders/20211109221111-seed-products.js
+++ back-end/src/database/seeders/20211109221111-seed-products.js
@@ -8,67 +8,67 @@ module.exports = {
         id: 1,
         name: 'Skol Lata 250ml',
         price: 2.20,
-        url_image:'http://localhost:3001/images/skol_lata_350ml.jpg',
+        url_image:'http://localhost:3001/images/skol_lata_350ml.png',
       },
       {
         id: 2,
         name: 'Heineken 600ml',
         price: 7.50,
-        url_image: 'http://localhost:3001/images/heineken_600ml.jpg',
+        url_image: 'http://localhost:3001/images/heineken_600ml.png',
       },
       {
         id: 3,
         name: 'Antarctica Pilsen 300ml',
         price: 2.49,
-        url_image: 'http://localhost:3001/images/antarctica_pilsen_300ml.jpg',
+        url_image: 'http://localhost:3001/images/antarctica_pilsen_300ml.png',
       },
       {
         id: 4, 
         name: 'Brahma 600ml',
         price: 7.50, 
-        url_image: 'http://localhost:3001/images/brahma_600ml.jpg'
+        url_image: 'http://localhost:3001/images/brahma_600ml.png'
       },
       {
         id: 5, 
         name: 'Skol 269ml',
         price: 2.19,
-        url_image: 'http://localhost:3001/images/skol_269ml.jpg',
+        url_image: 'http://localhost:3001/images/skol_269ml.png',
       },
       {
         id: 6,
         name: 'Skol Beats Senses 313ml',
         price: 4.49,
-        url_image: 'http://localhost:3001/images/skol_beats_senses_313ml.jpg'
+        url_image: 'http://localhost:3001/images/skol_beats_senses_313ml.png'
       },
       {
         id: 7,
         name: 'Becks 330ml',
         price: 4.99,
-        url_image: 'http://localhost:3001/images/becks_330ml.jpg'
+        url_image: 'http://localhost:3001/images/becks_330ml.png'
       },
       {
         id: 8,
         name: 'Brahma Duplo Malte 350ml',
         price:2.79,
-        url_image: 'http://localhost:3001/images/brahma_duplo_malte_350ml.jpg'
+        url_image: 'http://localhost:3001/images/brahma_duplo_malte_350ml.png'
       },
       {
         id: 9,
         name: 'Becks 600ml',
         price: 8.89,
-        url_image: 'http://localhost:3001/images/becks_600ml.jpg'
+        url_image: 'http://localhost:3001/images/becks_600ml.png'
       },
       {
         id: 10,
         name: 'Skol Beats Senses 269ml',
         price: 3.57,
-        url_image: 'http://localhost:3001/images/skol_beats_senses_269ml.jpg'
+        url_image: 'http://localhost:3001/images/skol_beats_senses_269ml.png'
       }, 
       {
         id: 11,
         name: 'Stella Artois 275ml',
         price:3.49,
-        url_image: 'http://localhost:3001/images/stella_artois_275ml.jpg',
+        url_image: 'http://localhost:3001/images/stella_artois_275ml.png',
       }, 
       ], { timestamps: false });
   },

c1769b22c6b0f2139a23def0279eec611fced8ae
iago <iago.pferreiravr@gmail.com>
2021-11-25 19:33:40 -0300

    refactor(frontend-checkout): Removendo as linhas que estavam sendo criadas dentro da CheckouTable e renderizando o componente OrderItem no lugar

--- front-end/src/components/CheckoutTable.jsx
+++ front-end/src/components/CheckoutTable.jsx
@@ -1,41 +1,13 @@
 import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import { CartContext } from '../contexts/Cart';
-import ButtonRemoveItem from './ButtonRemoveItem';
+import OrderItem from './OrderItem';
 
-function CheckoutTable(props) {
+function CheckoutTable() {
   const { cart } = useContext(CartContext);
-  const { testIds: {
-    // productId,
-    productName,
-    productQuantity,
-    productUnitPrice,
-    productSubTotal,
-    productRemove } } = props;
-
   const fillTable = ((cartt) => cartt.map(
-    ({ productId: id, name, quantity, unitPrice, subTotal }, key) => (
-      <tr key={ key }>
-        <td data-testid={ `customer_checkout__element-order-table-item-number--${key}` }>
-          { key + 1 }
-        </td>
-        <td data-testid={ productName + key }>{ name }</td>
-        <td data-testid={ productQuantity + key }>{ quantity }</td>
-        <td data-testid={ productUnitPrice + key }>
-          { Number(unitPrice).toLocaleString('pt-BR', {
-            currency: 'BRL',
-            minimumFractionDigits: 2,
-          }) }
-        </td>
-        <td data-testid={ productSubTotal + key }>
-          {/* Colocar a vírgula na moeda: https://pt.stackoverflow.com/questions/264503/personalizar-o-tofixed-para-utilizar-v%C3%ADrgula-como-separador-decimal */}
-          { Number(subTotal).toLocaleString('pt-BR', {
-            currency: 'BRL',
-            minimumFractionDigits: 2,
-          }) }
-        </td>
-        <td data-testid={ productRemove + key }><ButtonRemoveItem id={ id } /></td>
-      </tr>
+    (info, index) => (
+      <OrderItem info={ info } index={ index } key={ index } />
     ),
   ));
 

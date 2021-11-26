d6505d61ac07c64c03aa531d33f84642cf040b1f
Flavio Franco <flavio.win@gmail.com>
2021-11-26 17:19:19 -0300

    FEAT: implementando socket

--- back-end/src/database/seeders/20211110195609-sale.js
+++ back-end/src/database/seeders/20211110195609-sale.js
@@ -1,4 +1,29 @@
+'use strict';
+
 module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
+
+/* module.exports = {
   up: async (queryInterface, _Sequelize) => {
     await queryInterface.bulkInsert('sales',
       [{
@@ -35,3 +60,4 @@ module.exports = {
     await queryInterface.bulkDelete('sales', null, {});
   },
 };
+ */

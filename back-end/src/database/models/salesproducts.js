d53421860d6fbf77c4ecdf1559d69cf77a86950a
Marcus Hüsemann <mchusemann@gmail.com>
2021-11-26 12:16:03 -0300

    fix: remove conflito do merge

--- back-end/src/database/models/salesproducts.js
+++ back-end/src/database/models/salesproducts.js
@@ -15,19 +15,11 @@ module.exports = (sequelize, DataTypes) => {
       foreignKey: 'saleId',
       otherKey: 'productId',
     });
-<<<<<<< HEAD
-    models.Products.belongsToMany(models.Sales, {
-      as: 'sales',
-      through: SalesProducts,
-      foreignKey: 'productId',
-      otherKey: 'saleId',
-=======
     models.Products.belongsToMany(models.sales, {
       as: 'products',
       through: salesProducts,
       foreignKey: 'saleId',
       otherKey: 'productId',
->>>>>>> 9ccbd97ccb7a8494ff47dcf553a4a224ae602cbf
     })
   }
 

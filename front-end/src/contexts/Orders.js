1c12c88f20553c7efc7bc79fbe9bcb016717a864
Marcus Hüsemann <mchusemann@gmail.com>
2021-11-26 20:04:03 -0300

    feat: adiciona useEffect no contexto de orders

--- front-end/src/contexts/Orders.js
+++ front-end/src/contexts/Orders.js
@@ -1,10 +1,29 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 
 export const OrdersContext = createContext();
 
 export const OrdersProvider = ({ children }) => {
-  const [orderList, setOrderList] = useState('');
+  const [orderList, setOrderList] = useState();
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const { token, id } = JSON.parse(localStorage.getItem('user'));
+        const data = await axios.get(`http://localhost:3001/orders/${id}`, {
+          headers: {
+            Authorization: token,
+          },
+        });
+        console.log(data);
+        setOrderList(response);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    fetchData();
+  }, []);
 
   return (
     <OrdersContext.Provider value={ { orderList, setOrderList } }>

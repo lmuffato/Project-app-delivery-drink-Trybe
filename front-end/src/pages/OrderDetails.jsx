b3c1655bc25abdd7715bbb3821cd3c28a1ab0078
Viegas <mvrdgs@gmail.com>
2021-11-26 17:12:59 -0300

    feat: requsitos 25 e 26

--- front-end/src/pages/OrderDetails.jsx
+++ front-end/src/pages/OrderDetails.jsx
@@ -11,7 +11,7 @@ const DATA_ITEM_N = 'customer_order_details__element-order-table-item-number-';
 const DATA_ITEM_D = 'customer_order_details__element-order-table-name-';
 const DATA_ITEM_Q = 'customer_order_details__element-order-table-quantity-';
 const DATA_ITEM_P = 'customer_order_details__element-order-table-sub-total-';
-const DATA_ITEM_TP = 'customer_order_details__element-order-total-price-';
+const DATA_ITEM_TP = 'customer_order_details__element-order-total-price';
 
 function OrderDetails() {
   const { get } = useContext(Context);
@@ -19,6 +19,7 @@ function OrderDetails() {
   const { length } = id;
   const [order, setOrder] = useState();
   const [currProducts, setProducts] = useState([]);
+  const [isDisabled] = useState(true);
 
   useEffect(() => {
     const getOrders = async () => {
@@ -46,12 +47,19 @@ function OrderDetails() {
           {order.saleDate.split('T')[0].split('-').reverse().join('/')}
         </th>
         <th data-testid={ DATA_S }>{order.status}</th>
-        <button type="button" data-testid={ DATA_B }>Marcar como entrege</button>
+        <button
+          type="button"
+          data-testid={ DATA_B }
+          disabled={ isDisabled }
+        >
+          Marcar como entrege
+        </button>
       </tr>
     </thead>
   );
 
   if (!order) return (<div>Loading...</div>);
+
   return (
     <div>
       <table>
@@ -68,38 +76,48 @@ function OrderDetails() {
           </tr>
         </thead>
         <tbody>
-          { currProducts.map((product, index) => (
-            <tr key={ product.name }>
-              <td
-                data-testid={ `${DATA_ITEM_N}${index}` }
-              >
-                {index + 1}
-              </td>
-              <td data-testid={ `${DATA_ITEM_D}${product.saleId}` }>
-                {product.name}
-              </td>
-              <td data-testid={ `${DATA_ITEM_Q}${product.saleId}` }>
-                {product.SaleProduct.quantity}
-              </td>
-              <td data-testid={ `${DATA_ITEM_P}${product.saleId}` }>
-                {console.log(product)}
-                {product.price}
-              </td>
-              <td
-                data-testid={ `${DATA_ITEM_TP}${order.id}` }
-              >
-                {Number(product.SaleProduct.quantity) * Number(product.price) }
-              </td>
-            </tr>
-          ))}
+          { currProducts.map((product, index) => {
+            const total = (Number(product.SaleProduct.quantity) * Number(product.price))
+              .toFixed(2)
+              .toString()
+              .replace('.', ',');
+
+            return (
+              (
+                <tr key={ product.name }>
+                  <td
+                    data-testid={ `${DATA_ITEM_N}${index}` }
+                  >
+                    {index + 1}
+                  </td>
+                  <td data-testid={ `${DATA_ITEM_D}${product.saleId}` }>
+                    {product.name}
+                  </td>
+                  <td data-testid={ `${DATA_ITEM_Q}${product.saleId}` }>
+                    {product.SaleProduct.quantity}
+                  </td>
+                  <td data-testid={ `${DATA_ITEM_P}${product.saleId}` }>
+                    {product.price}
+                  </td>
+                  <td
+                    data-testid={ `${DATA_ITEM_TP}${order.id}` }
+                  >
+                    { total }
+                  </td>
+                </tr>
+              )
+            );
+          })}
         </tbody>
       </table>
       <h1
-        data-testid="customer_order_details__element-order-total-price"
+        data-testid={ DATA_ITEM_TP }
       >
-        {order.totalPrice}
+        {Number(order.totalPrice)
+          .toFixed(2)
+          .toString()
+          .replace('.', ',')}
       </h1>
-
     </div>
   );
 }

1e4b86810da257fb3dfabf8b3e86b0426a5606b3
Marcus Hüsemann <mchusemann@gmail.com>
2021-11-26 18:59:20 -0300

    refactor: altera retorno da funçao, back-end esta retornando diferente

--- front-end/src/pages/Orders.jsx
+++ front-end/src/pages/Orders.jsx
@@ -1,11 +1,11 @@
-import React, { useContext, useEffect } from 'react';
-import axios from 'axios';
+import React, { useContext } from 'react';
 import OrderCard from '../components/OrderCard';
 import { OrdersContext } from '../contexts/Orders';
 import NavBar from '../components/NavBar';
 
 function Orders() {
-  const { orderList, setOrderList } = useContext(OrdersContext);
+  const { orderList } = useContext(OrdersContext);
+
   const testIds = {
     pageProductsId: 'customer_products__element-navbar-link-products',
     pageOrdersId: 'customer_products__element-navbar-link-orders',
@@ -20,29 +20,11 @@ function Orders() {
     pageName: 'Meus pedidos',
   };
 
-  useEffect(() => {
-    const { token, id } = JSON.parse(localStorage.getItem('user'));
-
-    async function fetchData() {
-      try {
-        const response = await axios.get(`http://localhost:3001/orders/${id}`, {
-          headers: {
-            Authorization: token,
-          },
-        });
-        setOrderList(response.data);
-      } catch (err) {
-        console.log(err);
-      }
-    }
-    fetchData();
-  }, []);
-
-  if (!orderList.orders) return <h1>Loading...</h1>;
+  if (!orderList) return <h1>Loading...</h1>;
   return (
     <>
       <NavBar ids={ testIds } names={ navegationNames } />
-      { orderList.orders.map((order, key) => (
+      { orderList.map((order, key) => (
         <OrderCard key={ key } testIds={ testIds } order={ order } />))}
     </>
   );

2eb229d7154f0fd7c9610a919d0b08e2676776a6
Lucas André <lucas.cancado89@gmail.com>
2021-11-26 18:40:39 -0300

    implementa socket

--- back-end/src/api/server.js
+++ back-end/src/api/server.js
@@ -1,5 +1,22 @@
 const port = process.env.PORT || 3001;
+const conect = require('socket.io');
+const prot = require('http');
 const app = require('./app');
 
-app.listen(port);
+const http = prot.createServer(app);
+
+const io = conect(http, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'PUT', 'POST', 'PATCH'],
+  },
+});
+
+io.on('connection', (socket) => {
+  socket.on('clientOrderSatus', (data) => {
+    io.emit('serverNewStatus', data);
+  });
+});
+
+http.listen(port);
 console.log(`Api rodando na porta ${port}`);

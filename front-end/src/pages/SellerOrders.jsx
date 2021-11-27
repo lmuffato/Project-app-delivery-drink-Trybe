d59ad87ca44a58fac3dc590f2bd9bbf09a008bd3
iago <iago.pferreiravr@gmail.com>
2021-11-27 19:22:49 -0300

    refactor(frontend-sellers): Alterando a página SellerOrders para pegar as informações do contexto certo

--- front-end/src/pages/SellerOrders.jsx
+++ front-end/src/pages/SellerOrders.jsx
@@ -1,19 +1,19 @@
 import React, { useContext } from 'react';
 import OrderCard from '../components/OrderCard';
-import { OrdersContext } from '../contexts/Orders';
 import NavBar from '../components/NavBar';
+import { SellerOrdersContext } from '../contexts/SellerOrders';
 
 function SellerOrders() {
-  const { orderList } = useContext(OrdersContext);
+  const { orderList } = useContext(SellerOrdersContext);
 
   const testIds = {
-    pageProductsId: 'customer_products__element-navbar-link-products',
-    pageOrdersId: 'customer_products__element-navbar-link-orders',
-    userId: 'customer_products__element-navbar-user-full-name',
-    orderId: 'customer_orders__element-order-id-',
-    orderStatus: 'customer_orders__element-delivery-status-',
-    orderDate: 'customer_orders__element-order-date-',
-    orderPrice: 'customer_order_details__element-order-details-label-order-id',
+    pageProductsId: 'seller_products__element-navbar-link-products',
+    pageOrdersId: 'seller_products__element-navbar-link-orders',
+    userId: 'seller_products__element-navbar-user-full-name',
+    orderId: 'seller_orders__element-order-id-',
+    orderStatus: 'seller_orders__element-delivery-status-',
+    orderDate: 'seller_orders__element-order-date-',
+    orderPrice: 'customer_orders__element-card-price-',
   };
 
   const navegationNames = {
@@ -21,11 +21,12 @@ function SellerOrders() {
   };
 
   if (!orderList) return <h1>Loading...</h1>;
+  console.log(orderList);
   return (
     <>
       <NavBar ids={ testIds } names={ navegationNames } />
-      { orderList.orders.map((order, key) => (
-        <OrderCard key={ key } testIds={ testIds } order={ order } />))}
+      { orderList.map((order, key) => (
+        <OrderCard key={ key } testIds={ testIds } order={ order } index={ key } />))}
     </>
   );
 }

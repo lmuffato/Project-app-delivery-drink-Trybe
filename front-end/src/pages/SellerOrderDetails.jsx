874dd50153ba9bd804eb8f6cf097472e42c14c8f
iago <iago.pferreiravr@gmail.com>
2021-11-27 17:25:01 -0300

    feat(frontend-orders): Cria pagina SellerOrderDetails

---
+++ front-end/src/pages/SellerOrderDetails.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import NavBar from '../components/NavBar';
+import OrderDetailsTable from '../components/OrderDetailsTable';
+
+function SellerOrderDetails() {
+  const testIds = {
+    pageOrdersId: 'customer_products__element-navbar-link-orders',
+    userId: 'customer_products__element-navbar-user-full-name',
+    orderId: 'seller_order_details__element-order-details-label-order-id',
+    // sellerName: 'customer_order_details__element-order-details-label-seller-name',
+    orderDate: 'seller-order-details-label-order-date',
+    orderStatus: 'seller_order_details__element-order-details-label-delivery-status',
+    buttonPreparing: 'seller_order_details__button-preparing-check',
+    buttonDelivery: 'seller_order_details__button-dispatch-check',
+    tableItemNumber: 'seller-order-table-item-number-',
+    tableItemName: 'seller-order-table-name-',
+    tableItemQuantity: 'seller_order_details__element-order-table-quantity-',
+    tableItemSubTotal: 'seller_order_details__element-order-table-sub-total-',
+    tableItemTotalPrice: 'seller_order_details__element-order-total-price-',
+    tableTotalPrice: 'seller_order_details__element-order-total-price',
+  };
+
+  const navegationNames = {
+    pageName: 'Meus Pedidos',
+  };
+
+  return (
+    <>
+      <NavBar ids={ testIds } names={ navegationNames } />
+      <OrderDetailsTable testIds={ testIds } />
+    </>
+  );
+}
+
+export default SellerOrderDetails;

772e78385930962e528f112389542f4d32d5026b
iago <iago.pferreiravr@gmail.com>
2021-11-27 13:49:09 -0300

    feat(frontend-orders): Colocando os data-testids no componente OrderDetails

--- front-end/src/pages/OrderDetails.jsx
+++ front-end/src/pages/OrderDetails.jsx
@@ -1,17 +1,34 @@
 import React from 'react';
+import NavBar from '../components/NavBar';
 import OrderDetailsTable from '../components/OrderDetailsTable';
 
 function ProductDetails() {
   const testIds = {
-    productId: 'customer_order_details__element-order-table-item-number-',
-    productName: 'customer_order_details__element-order-table-name-',
-    productQuantity: 'customer_order_details__element-order-table-quantity-',
-    productUnitPrice: 'customer_order_details__element-order-table-sub-total-',
-    productSubTotal: 'customer_order_details__element-order-total-price-',
+    pageProductsId: 'customer_products__element-navbar-link-products',
+    pageOrdersId: 'customer_products__element-navbar-link-orders',
+    userId: 'customer_products__element-navbar-user-full-name',
+    orderId: 'customer_order_details__element-order-details-label-order-id',
+    sellerName: 'customer_order_details__element-order-details-label-seller-name',
+    orderDate: 'customer_order_details__element-order-details-label-order-date',
+    orderStatus: 'customer_order_details__element-order-details-label-delivery-status',
+    buttonDelivery: 'customer_order_details__button-delivery-check',
+    tableItemNumber: 'customer_order_details__element-order-table-item-number-',
+    tableItemName: 'customer_order_details__element-order-table-name-',
+    tableItemQuantity: 'customer_order_details__element-order-table-quantity-',
+    tableItemSubTotal: 'customer_order_details__element-order-table-sub-total-',
+    tableItemTotalPrice: 'customer_order_details__element-order-total-price-',
+    tableTotalPrice: 'customer_order_details__element-order-total-price',
+  };
+
+  const navegationNames = {
+    pageName: 'Meus Pedidos',
   };
 
   return (
-    <OrderDetailsTable testIds={ testIds } />
+    <>
+      <NavBar ids={ testIds } names={ navegationNames } />
+      <OrderDetailsTable testIds={ testIds } />
+    </>
   );
 }
 

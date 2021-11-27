ee923274a80519877010361b2adfda3e1e0e96f8
iago <iago.pferreiravr@gmail.com>
2021-11-27 13:48:39 -0300

    refactor(frontend-orders): Mudando toda a lógica de renderização do componente OrderDetailsTable

--- front-end/src/components/OrderDetailsTable.jsx
+++ front-end/src/components/OrderDetailsTable.jsx
@@ -1,87 +1,90 @@
-// import axios from 'axios';
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext } from 'react';
 import moment from 'moment';
+import PropTypes from 'prop-types';
 import { useParams } from 'react-router-dom';
 import { OrdersContext } from '../contexts/Orders';
 
-function OrderDetailsTable() {
+function OrderDetailsTable({ testIds }) {
+  const {
+    orderId,
+    sellerName,
+    orderDate,
+    orderStatus,
+    buttonDelivery,
+    tableItemNumber,
+    tableItemName,
+    tableItemQuantity,
+    tableItemSubTotal,
+    tableItemTotalPrice,
+    tableTotalPrice,
+  } = testIds;
   const params = useParams();
   const { id } = params;
-  const { orderList: orders } = useContext(OrdersContext);
-  const [values, setValues] = useState('');
-  const { sellerId, totalPrice, saleDate, products } = values;
-  useEffect(() => {
-    if (orders) {
-      const order = orderList.find(
-        (el) => el.id.toString() === id,
-      );
-      setValues(order);
-    }
-  }, []);
+  const { orderList } = useContext(OrdersContext);
+  let order;
 
-  const getSeller = async () => {
-    try {
-      const response = await axios.get('http://localhost:3001/sellers', {
-        headers: {
-          Authorization: token,
-        },
-      });
+  if (orderList) {
+    order = orderList.orders.find(
+      (el) => el.id.toString() === id,
+    );
+  }
 
-      const { name } = response.find((seller) => seller.id === sellerId);
-
-      return name;
-    } catch (err) {
-      console.log(err);
-    }
-  };
-  const fillTable = (() => products.map(
+  const fillTable = (() => order.products && order.products.map(
     ({ id: productId, name, salesProducts: { quantity }, price }, key) => (
       <tr key={ key }>
-        <td data-testid="">
+        <td data-testid={ tableItemNumber + key }>
           { productId }
         </td>
-        <td data-testid="">
+        <td data-testid={ tableItemName + key }>
           { name }
         </td>
-        <td data-testid="">
+        <td data-testid={ tableItemQuantity + key }>
           { quantity }
         </td>
-        <td data-testid="">
-          { price }
+        <td data-testid={ tableItemSubTotal + key }>
+          { Number(price).toLocaleString('pt-BR', {
+            currency: 'BRL',
+            minimumFractionDigits: 2,
+          }) }
         </td>
-        <td data-testid="customer_order_details__element-order-table-sub-total-">
+        <td data-testid={ tableItemTotalPrice + key }>
           { Number(price * quantity).toLocaleString('pt-BR', {
             currency: 'BRL',
             minimumFractionDigits: 2,
           }) }
         </td>
-        {/* <td data-testid=" customer_order_details__element-order-total-price-"> */ }
-        {/* Colocar a vírgula na moeda: https://pt.stackoverflow.com/questions/264503/personalizar-o-tofixed-para-utilizar-v%C3%ADrgula-como-separador-decimal */ }
-        {/* { Number(subTotal).toLocaleString('pt-BR', {
-             currency: 'BRL',
-              minimumFractionDigits: 2,
-            }) }
-          </td> */}
       </tr>
     ),
   )
   );
 
-  if (!orders) {
+  if (!orderList) {
     return 'Loading';
   }
+
+  const renderSaleHeader = () => (
+    <tr>
+      <th data-testid={ orderId }>{ `Pedido: ${id}` }</th>
+      <th data-testid={ sellerName }>{ order.seller.name }</th>
+      <th data-testid={ orderDate }>{ moment(order.saleDate).format('DD/MM/YYYY') }</th>
+      <th data-testid={ orderStatus }>{ order.status }</th>
+      <th>
+        <button
+          type="button"
+          data-testid={ buttonDelivery }
+          disabled
+        >
+          Marcar como entregue
+        </button>
+      </th>
+    </tr>
+  );
 
   return (
     <div>
       <table>
         <thead>
-          <tr>
-            <th>{ `Pedido: ${id}` }</th>
-            <th>{ getSeller() }</th>
-            <th>{ moment(saleDate).format('DD/mm/yyyy') }</th>
-            <th>a</th>
-            <button type="button"> Marcar como entregue </button>
-          </tr>
+          { renderSaleHeader() }
         </thead>
       </table>
       <table>
@@ -98,11 +101,30 @@ function OrderDetailsTable() {
           { fillTable() }
         </tbody>
       </table>
-      <div>
-        { `Total: ${totalPrice}` }
+      <div data-testid={ tableTotalPrice }>
+        { Number(order.totalPrice).toLocaleString('pt-BR', {
+          currency: 'BRL',
+          minimumFractionDigits: 2,
+        }) }
       </div>
     </div>
   );
 }
+
+OrderDetailsTable.propTypes = {
+  testIds: PropTypes.shape({
+    orderId: PropTypes.string,
+    sellerName: PropTypes.string,
+    orderDate: PropTypes.string,
+    orderStatus: PropTypes.string,
+    buttonDelivery: PropTypes.string,
+    tableItemNumber: PropTypes.string,
+    tableItemName: PropTypes.string,
+    tableItemQuantity: PropTypes.string,
+    tableItemSubTotal: PropTypes.string,
+    tableItemTotalPrice: PropTypes.string,
+    tableTotalPrice: PropTypes.string,
+  }).isRequired,
+};
 
 export default OrderDetailsTable;

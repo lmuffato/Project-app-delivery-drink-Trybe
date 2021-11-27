06f10f291ce33d02abc8721087bdc6f0302c7e75
Micaela Alves <micaela.alves93@gmail.com>
2021-11-27 00:57:15 -0300

    refactor - funcao executa o seu verbo

--- back-end/src/controllers/User/findByEmailUser.js
+++ back-end/src/controllers/User/findByEmailUser.js
@@ -1,15 +1,12 @@
 const find = require('../../services/find');
-const { passwordToken } = require('../../services');
 
 module.exports = async (req, res) => {
-  const { email, password } = req.body;
-  console.log(email, password);  
+  const { email } = req.body;
   const [user] = await find(
     'users',
-    { email, password },
+    { email },
   );
-   console.log(user);
   if (!user) return res.status(404).json({ message: 'User not found' });
-  const token = passwordToken(user.id);
-  return res.status(200).json({ token, name: user.name, email: user.email, role: user.role });
+  
+  return res.status(200).json({ name: user.name, email: user.email, role: user.role });
 };

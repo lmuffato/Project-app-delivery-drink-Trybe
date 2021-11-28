ee7bb5a3527dc0730e8d03c7c31a00c5c5034ce8
Stanrley Barbosa <3011stan@gmail.com>
2021-11-28 13:56:13 -0300

    Criaçao de elemento que lista cards de vendas realizados pelo vendedor

---
+++ front-end/src/components/SalesList.jsx
@@ -0,0 +1,52 @@
+import axios from 'axios';
+import React, { useCallback, useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import { useHistory } from 'react-router';
+import SaleCard from './SaleCard';
+
+export default function SalesList(props) {
+  const { token } = props;
+  const url = 'http://localhost:3001';
+  const [sales, setSales] = useState([]);
+  const history = useHistory();
+
+  const getSales = useCallback(
+    async () => {
+      await axios({
+        method: 'get',
+        url: `${url}/sales`,
+        headers: {
+          Authorization: token,
+        },
+      })
+        .then(({ data }) => {
+          setSales(data);
+        })
+        .catch((error) => console.log(error));
+    }, [setSales, token],
+  );
+
+  useEffect(() => {
+    getSales();
+  }, [getSales]);
+
+  return (
+    <div className="w-full flex flex-wrap p-20">
+      {
+        sales.map((sale, index) => (
+          <button
+            key={ index }
+            type="button"
+            onClick={ () => history.push(`/seller/orders/${sale.id}`) }
+          >
+            <SaleCard sale={ sale } key={ index } />
+          </button>
+        ))
+      }
+    </div>
+  );
+}
+
+SalesList.propTypes = {
+  token: PropTypes.string.isRequired,
+};

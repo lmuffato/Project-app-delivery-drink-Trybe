8c4f55a763b0be0a6f40876d8e07e9f8049ce313
Micaela Alves <micaela.alves93@gmail.com>
2021-11-29 03:46:49 -0300

    feat - effect para att valor total

--- front-end/src/provider/ProductsProvider.js
+++ front-end/src/provider/ProductsProvider.js
@@ -6,19 +6,30 @@ import getAllProducts from '../services/getAllProducts';
 export default function ProductsProvider({ children }) {
   const [productsResult, setProductsResult] = useState([]);
   const [isFetching, setIsFetching] = useState(true);
-  const [productsCart, setProductsCart] = useState([{ id: 0, quantity: 0 }]);
+  const [totalCart, setTotalCart] = useState(0);
+  const [productsCart, setProductsCart] = useState([]);
 
   const value = {
     values: {
       productsResult,
       isFetching,
       productsCart,
+      totalCart,
     },
     actions: {
       setProductsResult,
       setProductsCart,
+      setTotalCart,
     },
   };
+
+  useEffect(() => {
+    const valorInicial = 0;
+    const totalAmountCustomer = productsCart
+      .reduce((acc, { total }) => acc + parseFloat(total), valorInicial);
+    localStorage.setItem('totalAmountCustomer', JSON.stringify(totalAmountCustomer));
+    setTotalCart(totalAmountCustomer);
+  }, [productsCart]);
 
   useEffect(() => {
     const fetchAllProducts = () => {

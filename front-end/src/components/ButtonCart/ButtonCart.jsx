e59b6d359a812fa06f289067c33f7b98999f4d8d
Micaela Alves <micaela.alves93@gmail.com>
2021-11-29 03:45:44 -0300

    feat - funcionalidade do botao ver carrinho

--- front-end/src/components/ButtonCart/ButtonCart.jsx
+++ front-end/src/components/ButtonCart/ButtonCart.jsx
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import ProductsContext from '../../context/ProductsContext';
 
 function ButtonCart() {
+  const { values: { totalCart } } = useContext(ProductsContext);
+  const formatedValue = totalCart.toFixed(2).replace('.', ',');
+  const redirect = useNavigate();
+
+  const handleClick = () => {
+    redirect('/customer/checkout');
+  };
+
   return (
-    <button
-      data-testid="customer_products__button-cart"
-      type="button"
-    >
-      <p>Ver Carrinho:</p>
-      <span data-testid="customer_products__checkout-button-value">Valor Total</span>
-    </button>
+    <>
+      <button
+        data-testid="customer_products__button-cart"
+        type="button"
+        onClick={ handleClick }
+      >
+        Ver Carrinho:
+      </button>
+      <span
+        data-testid="customer_products__checkout-bottom-value"
+      >
+        { formatedValue }
+      </span>
+    </>
   );
 }
 

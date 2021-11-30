1bbec622c9cd28fce93cf21cb9f8cb8c796bed44
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-11-29 21:09:04 -0300

    feat: add createAdminUser

---
+++ back-end/src/controllers/User/createAdminUser.js
@@ -0,0 +1,25 @@
+const genHashMd5 = require('md5');
+const insert = require('../../services/insert');
+const find = require('../../services/find');
+const { isUserExists } = require('../../schemas');
+const generateToken = require('../../utils/generateToken');
+
+module.exports = async (req, res) => {
+  const { name, email, password, role } = req.body;
+  const passwordHash = genHashMd5(password);
+  
+  const isExists = await isUserExists({ name, email }, find);
+  
+  if (isExists.message) return res.status(isExists.status).json(isExists.message);
+
+  const createdUser = await insert(
+    'users',
+    { name, email, password: passwordHash, role },
+  );
+
+  const token = await generateToken(createdUser);
+
+  console.log(token);
+
+  res.status(201).json({ token });
+};

e467babfb309216a422fcdc087e3755362c1e08b
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-11-29 21:38:02 -0300

    fix: ErrorBackEnd

--- front-end/src/components/ErrorBackend/index.jsx
+++ front-end/src/components/ErrorBackend/index.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const ErrorBackend = ({ messageError, datatestid }) => (
-  <span data-testid={ datatestid }>
+const ErrorBackend = ({ messageError, dataTestId }) => (
+  <span data-testid={ dataTestId }>
     { messageError }
   </span>
 );
 
 ErrorBackend.propTypes = {
   messageError: PropTypes.string.isRequired,
-  datatestid: PropTypes.string.isRequired,
+  dataTestId: PropTypes.string.isRequired,
 };
 
 export default ErrorBackend;
